89b38bf13c300ab590de620587ec9a6305b553d9
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2005-12-09 17:43:34 +0000

    [maven-scm] copy for tag jetty_6_0_0beta6

---
+++ tags/jetty_6_0_0beta6/modules/maven-plugin/src/main/java/org/mortbay/jetty/plugin/Scanner.java
@@ -0,0 +1,274 @@
+/**
+ * 
+ */
+package org.mortbay.jetty.plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.maven.plugin.logging.Log;
+
+/**
+ * Scanner
+ * 
+ * Scans a list of files and directories on a periodic basis to detect changes.
+ * If a change in any of the watched files is found, then the target LifeCycle
+ * objects are stopped and restarted.
+ * 
+ *  This is used by the Jetty Maven plugin to watch the classes, dependencies 
+ *  and web.xml file of a web application and to restart the webapp if any
+ *  of the above changes.
+ *  
+ * @author janb
+ *
+ */
+public class Scanner extends Thread
+{
+	
+	private int scanInterval;
+	
+	private List roots;
+
+	private Map scanInfo = Collections.EMPTY_MAP;
+	
+	private List listeners;
+	
+	private Log log;
+	
+	
+	public interface Listener
+	{
+		public void changesDetected(Scanner scanner, List changes);
+	}
+	
+	
+	public Scanner ()
+	{	
+		setDaemon(true);
+	}
+	
+	
+	/**
+	 * The files and directory roots to watch. Directories will be
+	 * recursively scanned.
+	 * 
+	 * @return Returns the roots.
+	 */
+	public List getRoots()
+	{
+		return this.roots;
+	}
+
+	/**
+	 * @param roots The roots to set.
+	 */
+	public void setRoots(List roots)
+	{
+		this.roots = roots;
+		//do an initializing scan
+		scanInfo = scan();
+	}
+
+	/**
+	 * 
+	 * @return Returns the scanInterval.
+	 */
+	public int getScanInterval()
+	{
+		return this.scanInterval;
+	}
+
+	/**
+	 * 
+	 * @param scanInterval The scanInterval in seconds to set.
+	 */
+	public void setScanInterval(int scanInterval)
+	{
+		this.scanInterval = scanInterval;
+	}
+	
+
+	/**
+	 * List of Scanner.Listener implementations.
+	 * @return Returns the listeners.
+	 */
+	public List getListeners()
+	{
+		return this.listeners;
+	}
+
+
+	/**
+	 * @param listeners The listeners to set.
+	 */
+	public void setListeners(List listeners)
+	{
+		this.listeners = listeners;
+	}
+	
+    /**
+     * Log file to use for debug/info/error messages.
+     * @param log The Maven plugin Log implementation.
+     */
+    public void setLog (Log log)
+    {
+        this.log = log;       
+    }
+    
+    public Log getLog ()
+    {
+        return this.log;
+    }
+    
+	
+	/**
+	 * Loop every scanInterval seconds until interrupted, checking to see if
+	 * any of the watched files have changed. If they have, stop and restart
+	 * the LifeCycle targets.
+	 *  
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run ()
+	{	
+		// set the sleep interval
+		long sleepMillis = getScanInterval()*1000L;
+		boolean running = true;
+		while (running)
+		{
+			try
+			{
+				//wake up and scan the files
+				Thread.currentThread().sleep(sleepMillis);
+				Map latestScanInfo = scan();
+				
+				List filesWithDifferences = getDifferences(latestScanInfo, scanInfo);
+				
+				if (!filesWithDifferences.isEmpty())
+				{
+					if ((getListeners() != null) && (!getListeners().isEmpty()))
+					{
+						try
+						{
+							getLog().info("Calling scanner listeners ...");
+							
+							for (int i=0; i<getListeners().size();i++)
+								((Scanner.Listener)getListeners().get(i)).changesDetected(this, filesWithDifferences);
+							
+							getLog().info("Listeners completed.");
+						}
+						catch (Exception e)
+						{
+							log.warn("Error doing stop/start", e);
+						}
+					}
+				}				
+				scanInfo = latestScanInfo;
+			}
+			catch (InterruptedException e)
+			{
+				running = false;	
+			}
+		}
+	}
+	
+	
+	
+	
+	/**
+	 * Scan the files and directories.
+	 * 
+	 * @return
+	 */
+	private Map scan ()
+	{
+		getLog().debug("Recursively scanning roots ...");
+		List roots = getRoots();
+		if ((roots == null) || (roots.isEmpty()))
+			return Collections.EMPTY_MAP;
+		
+		HashMap scanInfoMap = new HashMap();	
+		Iterator itor = roots.iterator();
+		while (itor.hasNext())
+		{
+			File f = (File)itor.next();
+			scan (f, scanInfoMap);
+		}
+		
+		if  (getLog().isDebugEnabled())
+		{
+			itor = scanInfo.entrySet().iterator();
+			while (itor.hasNext())
+			{
+				Map.Entry e = (Map.Entry)itor.next();
+				getLog().debug("Scanned "+e.getKey()+" : "+e.getValue());
+			}
+		}
+		
+		getLog().debug("Scan complete.");
+		return scanInfoMap;
+	}
+	
+	
+	/**
+	 * Scan the file, or recurse into it if it is a directory.
+	 * @param f
+	 * @param scanInfoMap
+	 */
+	private void scan (File f, Map scanInfoMap)
+	{
+		try
+		{
+			if (f.isFile())
+			{
+				String name = f.getCanonicalPath();
+				long lastModified = f.lastModified();
+				scanInfoMap.put(name, new Long(lastModified));
+			}
+			else
+			{
+				File[] files = f.listFiles();
+				for (int i=0;i<files.length;i++)
+					scan(files[i], scanInfoMap);
+			}
+		}
+		catch (IOException e)
+		{
+			getLog().error("Error scanning watched files", e);
+		}
+	}
+
+
+	private List getDifferences (Map newScan, Map oldScan)
+	{
+		ArrayList fileNames = new ArrayList();
+		Set oldScanKeys = new HashSet(oldScan.keySet());
+		Iterator itor = newScan.entrySet().iterator();
+		while (itor.hasNext())
+		{
+			Map.Entry entry = (Map.Entry)itor.next();
+			if (!oldScanKeys.contains(entry.getKey()))
+				fileNames.add(entry.getKey());
+			else if (!oldScan.get(entry.getKey()).equals(entry.getValue()))
+			{
+				fileNames.add(entry.getKey());
+				oldScanKeys.remove(entry.getKey());
+			}
+			else
+				oldScanKeys.remove(entry.getKey());
+		}
+		
+		if (!oldScanKeys.isEmpty())
+			fileNames.addAll(oldScanKeys);
+		
+		return fileNames;
+	}
+	
+}
---
+++ tags/jetty_6_0_0beta6/modules/test-webapp/src/main/webapp/js/default.js
@@ -0,0 +1,23 @@
+// Technique borrowed from scriptaculous to do includes.
+
+var DefaultJS = {
+  Version: 'Jetty Test',
+  script: function(libraryName) {
+    document.write('<script type="text/javascript" src="'+libraryName+'"></script>');
+  },
+  load: function() {
+    var scriptTags = document.getElementsByTagName("script");
+    for(var i=0;i<scriptTags.length;i++) {
+      if(scriptTags[i].src && scriptTags[i].src.match(/default\.js$/)) {
+        var path = scriptTags[i].src.replace(/default\.js$/,'');
+        this.script(path + 'prototype.js');
+        this.script(path + 'behaviour.js');
+        this.script(path + 'rico.js');
+        this.script(path + 'scriptaculous.js');
+        break;
+      }
+    }
+  }
+}
+
+DefaultJS.load();
