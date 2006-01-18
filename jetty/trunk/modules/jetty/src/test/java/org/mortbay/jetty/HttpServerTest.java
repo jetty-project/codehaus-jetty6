07141fd2b1a870acf4531dbbddeb176aaf8b99c9
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-01-18 11:41:56 +0000

    Fixed some more fragmented request test cases from Anders. The cached header name was being lost.

--- jetty/trunk/modules/jetty/src/test/java/org/mortbay/jetty/HttpServerTest.java
+++ jetty/trunk/modules/jetty/src/test/java/org/mortbay/jetty/HttpServerTest.java
@@ -1,15 +1,20 @@
 package org.mortbay.jetty;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Server;
 import org.mortbay.jetty.handler.AbstractHandler;
 import org.mortbay.jetty.nio.SelectChannelConnector;
 
@@ -21,7 +26,11 @@ import junit.framework.TestCase;
 public class HttpServerTest
     extends TestCase
 {
-    private static final String REQUEST = "POST / HTTP/1.0\n"
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    /** The request. */
+    private static final String REQUEST1 = "POST / HTTP/1.0\n"
         + "Host: localhost\n"
         + "Content-Type: text/xml\n"
         + "Content-Length: 181\n"
@@ -33,22 +42,69 @@ public class HttpServerTest
         + "</nimbus>";
 
     /** The expected response. */
-    private static final String RESPONSE = "HTTP/1.1 200 OK\n"
+    private static final String RESPONSE1 = "HTTP/1.1 200 OK\n"
         + "Connection: close\n"
         + "Server: Jetty(6.0.x)\n"
         + "\n"
         + "Hello world\n";
 
     // Break the request up into three pieces, splitting the header.
-    private static final String FRAGMENT1 = REQUEST.substring(0, 16);
-    private static final String FRAGMENT2 = REQUEST.substring(16, 34);
-    private static final String FRAGMENT3 = REQUEST.substring(34);
+    private static final String FRAGMENT1 = REQUEST1.substring(0, 16);
+    private static final String FRAGMENT2 = REQUEST1.substring(16, 34);
+    private static final String FRAGMENT3 = REQUEST1.substring(34);
+
+    /** Second test request. */
+    private static final String REQUEST2_HEADER  = 
+          "POST / HTTP/1.0\n"
+        + "Host: localhost\n"
+        + "Content-Type: text/xml\n"
+        + "Content-Length: ";
+    
+        private static final String REQUEST2_CONTENT  = 
+          "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n"
+        + "<nimbus xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
+        + "        xsi:noNamespaceSchemaLocation=\"nimbus.xsd\" version=\"1.0\">\n"
+        + "    <request requestId=\"1\">\n"
+        + "        <getJobDetails>\n"
+        + "            <jobId>73</jobId>\n"
+        + "        </getJobDetails>\n"
+        + "    </request>\n"
+        + "    <request requestId=\"2\">\n"
+        + "        <getProperties>\n"
+        + "            <type>job</type>\n"
+        + "            <jobId>73</jobId>\n"
+        + "        </getProperties>\n"
+        + "    </request>\n"
+        + "</nimbus>";
+        
+        private static final String REQUEST2 =
+            REQUEST2_HEADER+REQUEST2_CONTENT.getBytes().length+"\n\n"+REQUEST2_CONTENT;
+    
+    
+    private static final String RESPONSE2 = "HTTP/1.1 200 OK\n"
+        + "Content-Length: 455\n"
+        + "Server: Jetty(6.0.x)\n"
+        + "\n"
+        + "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n"
+        + "<nimbus xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
+        + "        xsi:noNamespaceSchemaLocation=\"nimbus.xsd\" version=\"1.0\">\n"
+        + "    <request requestId=\"1\">\n"
+        + "        <getJobDetails>\n"
+        + "            <jobId>73</jobId>\n"
+        + "        </getJobDetails>\n"
+        + "    </request>\n"
+        + "    <request requestId=\"2\">\n"
+        + "        <getProperties>\n"
+        + "            <type>job</type>\n"
+        + "            <jobId>73</jobId>\n"
+        + "        </getProperties>\n"
+        + "    </request>\n"
+        + "</nimbus>\n";
 
     // Useful constants
-    private static final long   ONE_SECOND = 1000L;
-    private static final long   TEN_SECONDS = 10000L;
-    private static final int    PORT       = 8123;
-    private static final String HOST       = "localhost";
+    private static final long   PAUSE = 10L;
+    private static final int    PORT  = 8080;
+    private static final String HOST  = "localhost";
 
     //~ Methods ----------------------------------------------------------------
 
@@ -58,25 +114,25 @@ public class HttpServerTest
      * @throws    Exception
      * @throws    InterruptedException
      */
-    public void testRequest()
-                     throws Exception, InterruptedException
+    public void testRequest1_jetty()
+                            throws Exception, InterruptedException
     {
-        // TODO does not work in maven
-        /*
-        Server       server = startServer();
+        Server       server = startServer(new HelloWorldHandler());
         Socket       client = new Socket(HOST, PORT);
         OutputStream os     = client.getOutputStream();
 
-        os.write(REQUEST.getBytes());
+        os.write(REQUEST1.getBytes());
         os.flush();
 
+        // Read the response.
         String response = readResponse(client);
 
+        // Shut down
         client.close();
         server.stop();
 
-        assertEquals("response", RESPONSE, response);
-        */
+        // Check the response
+        assertEquals("response", RESPONSE1, response);
     }
 
     /**
@@ -85,36 +141,199 @@ public class HttpServerTest
      * @throws    Exception
      * @throws    InterruptedException
      */
-    public void testRequestFragments()
-                              throws Exception, InterruptedException
+    public void testRequest1Fragments_jetty()
+                                     throws Exception, InterruptedException
     {
-        // TODO does not work in maven
-        /*
-        Server server = startServer();
-        Socket client = null;
+        Server server   = startServer(new HelloWorldHandler());
+        String response;
 
-        client = new Socket(HOST, PORT);
+        try {
+            Socket       client = new Socket(HOST, PORT);
+            OutputStream os     = client.getOutputStream();
 
-        OutputStream os = client.getOutputStream();
+            // Write a fragment, flush, sleep, write the next fragment, etc.
+            os.write(FRAGMENT1.getBytes());
+            os.flush();
+            Thread.sleep(PAUSE);
+            os.write(FRAGMENT2.getBytes());
+            os.flush();
+            Thread.sleep(PAUSE);
+            os.write(FRAGMENT3.getBytes());
+            os.flush();
 
-        os.write(FRAGMENT1.getBytes());
-        os.flush();
-        Thread.sleep(ONE_SECOND);
-        os.write(FRAGMENT2.getBytes());
-        os.flush();
-        Thread.sleep(ONE_SECOND);
-        os.write(FRAGMENT3.getBytes());
-        os.flush();
+            // Read the response
+            response = readResponse(client);
 
-        String response = readResponse(client);
+            // Shut down
+            client.close();
+        }
+        finally {
+            server.stop();
+        }
 
-        Thread.sleep(ONE_SECOND);
-        
-        client.close();
-        server.stop();
+        // Check the response
+        assertEquals("response", RESPONSE1, response);
+    }
 
-        assertEquals("response", RESPONSE, response);
-        */
+    public void testRequest2_jetty()
+                            throws Exception
+    {
+        byte[] bytes  = REQUEST2.getBytes();
+        Server server = startServer(new EchoHandler());
+
+        try {
+            for (int i = 0; i < 100; i++) {
+                Socket       client = client = new Socket(HOST, PORT);
+                OutputStream os     = client.getOutputStream();
+
+                os.write(bytes);
+                os.flush();
+
+                // Read the response
+                String response = readResponse(client);
+                client.close();
+
+                // Check the response
+                assertEquals("response "+i, RESPONSE2, response);
+            }
+        }
+        finally {
+            // Shut down
+            server.stop();
+        }
+    }
+
+    /**
+     * @throws    Exception
+     */
+    public void testRequest2Fragments_jetty()
+                                     throws Exception
+    {
+        Random    random     = new Random(System.currentTimeMillis());
+        byte[]    bytes      = REQUEST2.getBytes();
+        final int pointCount = 2;
+        Server    server     = startServer(new EchoHandler());
+
+        try {
+            for (int i = 0; i < 100; i++) {
+                
+                int[]        points  = new int[pointCount];
+                StringBuffer message = new StringBuffer();
+
+                message.append("iteration #" + (i + 1));
+
+                // Pick fragment points at random
+                for (int j = 0; j < points.length; ++j) {
+                    points[j] = random.nextInt(bytes.length);
+                    //System.err.println("points["+j+"]="+points[j]);
+                }
+
+                // Sort the list
+                Arrays.sort(points);
+
+                Socket       client = client = new Socket(HOST, PORT);
+                OutputStream os     = client.getOutputStream();
+
+                writeFragments(bytes, points, message, os);
+
+                // Read the response
+                String response = readResponse(client);
+                //System.err.println("\nresponse:\n"+response);
+
+                // Close the client
+                client.close();
+
+                // Check the response
+                assertEquals("response for "+i+" " + message.toString(), RESPONSE2,
+                             response);
+            }
+        }
+        finally {
+            // Shut down
+            server.stop();
+        }
+    }
+
+    public void testRequest2Iterate_jetty()
+                                   throws Exception
+    {
+        byte[] bytes  = REQUEST2.getBytes();
+        Server server = startServer(new EchoHandler());
+
+        try {
+            for (int i = 0; i < bytes.length; i++) {
+                int[]        points  = new int[] {i};
+                StringBuffer message = new StringBuffer();
+
+                message.append("iteration #" + (i + 1));
+
+                // Sort the list
+                Arrays.sort(points);
+
+                Socket       client = client = new Socket(HOST, PORT);
+                OutputStream os     = client.getOutputStream();
+
+                writeFragments(bytes, points, message, os);
+
+                // Read the response
+                String response = readResponse(client);
+
+                // Close the client
+                client.close();
+
+                // Check the response
+                assertEquals("response for "+i+" " + message.toString(), RESPONSE2,
+                             response);
+            }
+        }
+        finally {
+            // Shut down
+            server.stop();
+        }
+    }
+
+    /**
+     * After several iterations, I generated some known bad fragment points.
+     *
+     * @throws    Exception
+     */
+    public void testRequest2KnownBad_jetty()
+                                    throws Exception
+    {
+        byte[]  bytes     = REQUEST2.getBytes();
+        int[][] badPoints = new int[][] {
+                {70}, // beginning here, drops last line of request
+                {71}, // no response at all
+                {72}, // again starts drops last line of request
+                {74}, // again, no response at all
+            };
+        Server  server    = startServer(new EchoHandler());
+
+        try {
+            for (int i = 0; i < badPoints.length; ++i) {
+                Socket       client  = client = new Socket(HOST, PORT);
+                OutputStream os      = client.getOutputStream();
+                StringBuffer message = new StringBuffer();
+
+                message.append("iteration #" + (i + 1));
+                writeFragments(bytes, badPoints[i], message, os);
+
+                // Read the response
+                String response = readResponse(client);
+
+                // Close the client
+                client.close();
+
+                // Check the response
+                // TODO - change to equals when code gets fixed
+                assertNotSame("response for " + message.toString(), RESPONSE2,
+                              response);
+            }
+        }
+        finally {
+            // Shut down
+            server.stop();
+        }
     }
 
     /**
@@ -136,7 +355,7 @@ public class HttpServerTest
                                                               .getInputStream()));
 
             StringBuffer sb   = new StringBuffer();
-            String        line;
+            String       line;
 
             while ((line = br.readLine()) != null) {
                 sb.append(line);
@@ -153,35 +372,97 @@ public class HttpServerTest
     }
 
     /**
-     * Create and start the server.
+     * Create the server.
      *
-     * @return    Newly created, started server.
+     * @param     handler
+     *
+     * @return    Newly created server, ready to start.
      *
      * @throws    Exception
      */
-    private static Server startServer()
+    private static Server startServer(Handler handler)
                                throws Exception
     {
         Server                 server    = new Server();
         SelectChannelConnector connector = new SelectChannelConnector();
 
         server.setConnectors(new Connector[] {connector});
-        server.setHandler(new HelloWorldHandler());
+        server.setHandler(handler);
         server.start();
 
         return server;
     }
 
+    private void writeFragments(byte[] bytes, int[] points,
+                                StringBuffer message, OutputStream os)
+                         throws IOException, InterruptedException
+    {
+        int last = 0;
+
+        // Write out the fragments
+        for (int j = 0; j < points.length; ++j) {
+            int point = points[j];
+
+            //String dump = new String(bytes,last,point-last);
+            //System.err.println("\nFragment "+j+"='"+dump+"'");
+            os.write(bytes, last, point - last);
+            last = point;
+            os.flush();
+            Thread.sleep(PAUSE);
+
+            // Update the log message
+            message.append(" point #" + (j + 1) + ": " + point);
+        }
+
+        // Write the last fragment
+        //String dump = new String(bytes,last,bytes.length-last);
+        //System.err.println("\nFragment N='"+dump+"'");
+        os.write(bytes, last, bytes.length - last);
+        os.flush();
+        Thread.sleep(PAUSE);
+    }
+
     //~ Inner Classes ----------------------------------------------------------
+
+    private static class EchoHandler
+        extends AbstractHandler
+    {
+
+        //~ Methods ------------------------------------------------------------
+
+        public boolean handle(String target, HttpServletRequest request,
+                              HttpServletResponse response, int dispatch)
+                       throws IOException, ServletException
+        {
+            PrintWriter    writer = response.getWriter();
+            BufferedReader reader = request.getReader();
+            int            count  = 0;
+            String         line;
+
+            while ((line = reader.readLine()) != null) {
+                writer.println(line);
+                count += line.length();
+            }
+
+            if (count==0)
+                throw new IllegalStateException("received input");
+
+            return true;
+        }
+    }
+
     private static class HelloWorldHandler
         extends AbstractHandler
     {
+
         //~ Methods ------------------------------------------------------------
+
         public boolean handle(String target, HttpServletRequest request,
                               HttpServletResponse response, int dispatch)
                        throws IOException, ServletException
         {
             response.getOutputStream().print("Hello world");
+
             return true;
         }
     }
