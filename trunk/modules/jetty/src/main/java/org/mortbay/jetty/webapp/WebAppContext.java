9b7d833041b1eb4cced7732bb8c6d2c468ce13ff
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-02-19 15:09:58 +0000

    adding jndi java:comp/env support

--- trunk/modules/jetty/src/main/java/org/mortbay/jetty/webapp/WebAppContext.java
+++ trunk/modules/jetty/src/main/java/org/mortbay/jetty/webapp/WebAppContext.java
@@ -1,6 +1,6 @@
 //========================================================================
 //$Id: WebAppContext.java,v 1.5 2005/11/16 22:02:45 gregwilkins Exp $
-//Copyright 2004-2005 Mort Bay Consulting Pty. Ltd.
+//Copyright 2004-2006 Mort Bay Consulting Pty. Ltd.
 //------------------------------------------------------------------------
 //Licensed under the Apache License, Version 2.0 (the "License");
 //you may not use this file except in compliance with the License.
@@ -83,7 +83,7 @@ public class WebAppContext extends ContextHandler
     private ServletHandler _servletHandler;
     private SessionHandler _sessionHandler;
     private String[] _systemClasses = new String[]{"java.","javax.servlet.","javax.xml.","org.mortbay.","org.xml.","org.w3c."};
-    private String[] _serverClasses = new String[]{"org.mortbay.", "org.slf4j."};
+    private String[] _serverClasses = new String[]{"org.mortbay.", "-org.mortbay.naming.","-org.mortbay.util.", "org.slf4j."};
     private File _tmpDir;
     private String _war;
     
