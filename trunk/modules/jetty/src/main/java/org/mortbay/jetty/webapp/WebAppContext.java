e8bfb90f5aaddb8a9a453c9fbb37da0b89016d9f
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-03-15 14:55:11 +0000

    fixed default parent classloading

--- trunk/modules/jetty/src/main/java/org/mortbay/jetty/webapp/WebAppContext.java
+++ trunk/modules/jetty/src/main/java/org/mortbay/jetty/webapp/WebAppContext.java
@@ -81,7 +81,7 @@ public class WebAppContext extends ContextHandler
     private String _defaultsDescriptor=WEB_DEFAULTS_XML;
     private boolean _distributable=false;
     private boolean _extractWAR=true;
-    private boolean _parentLoaderPriority= true;
+    private boolean _parentLoaderPriority= false;
     private PermissionCollection _permissions;
     private SecurityHandler _securityHandler;
     private ServletHandler _servletHandler;
