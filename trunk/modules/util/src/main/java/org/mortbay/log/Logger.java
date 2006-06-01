70ad4f94078d33a6667ffdde78a4a5a04669b473
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-06-01 13:35:02 +0000

    doco

--- trunk/modules/util/src/main/java/org/mortbay/log/Logger.java
+++ trunk/modules/util/src/main/java/org/mortbay/log/Logger.java
@@ -15,10 +15,21 @@
 
 package org.mortbay.log;
 
+/** Logging Facade
+ * A simple logging facade that is intended simply to capture the style 
+ * of logging as used by Jetty.
+ *
+ */
 public interface Logger
 {
     public boolean isDebugEnabled();
+
+    /** Mutator used to turn debug on programatically.
+     * Implementations operation in which case an appropriate
+     * warning message shall be generated.
+     */
     public void setDebugEnabled(boolean enabled);
+
     public void info(String msg,Object arg0, Object arg1);
     public void debug(String msg,Throwable th);
     public void debug(String msg,Object arg0, Object arg1);
