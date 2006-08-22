93bbb6828adf91a62567fdc5aae8ba66654ca53f
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-08-22 11:07:59 +0000

    grizzly progress

--- jetty/trunk/extras/grizzly/src/main/java/org/mortbay/jetty/grizzly/JettyProcessorTask.java
+++ jetty/trunk/extras/grizzly/src/main/java/org/mortbay/jetty/grizzly/JettyProcessorTask.java
@@ -1,3 +1,18 @@
+///========================================================================
+// License and copyright status is a work in progress.
+// Parts Copyright 2006 Mort Bay Consulting Pty. Ltd.
+// Parts Copyright 2006 Sun Micro system and perhaps CDDL.
+//------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//========================================================================*
 /*
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
@@ -21,288 +36,204 @@
  * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
  */
 
+
 package org.mortbay.jetty.grizzly;
 
-
-import com.sun.enterprise.web.connector.grizzly.ByteBufferInputStream;
-import com.sun.enterprise.web.connector.grizzly.Handler;
-import com.sun.enterprise.web.connector.grizzly.ProcessorTask;
-import com.sun.enterprise.web.connector.grizzly.TaskBase;
-import com.sun.enterprise.web.connector.grizzly.TaskEvent;
-import com.sun.enterprise.web.connector.grizzly.OutputWriter;
-import com.sun.enterprise.web.connector.grizzly.SelectorFactory;
-import org.mortbay.jetty.grizzly.GrizzlyConnector.GrizzlyEndPoint;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.nio.channels.ByteChannel;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.SocketChannel;
+
+import com.sun.enterprise.web.connector.grizzly.Handler;
+import com.sun.enterprise.web.connector.grizzly.ProcessorTask;
+import com.sun.enterprise.web.connector.grizzly.TaskBase;
+import com.sun.enterprise.web.connector.grizzly.TaskEvent;
 
 /**
- *
+ * 
  * @author Jeanfrancois Arcand
  */
-public class JettyProcessorTask extends TaskBase implements ProcessorTask {
-    
+public class JettyProcessorTask extends TaskBase implements ProcessorTask
+{
     private Handler handler;
-    
-    private ByteBufferChannel bbChannel;
-    
-    private boolean keepAlive = true;
-    
-    
-    public JettyProcessorTask() {
+
+    public JettyProcessorTask()
+    {
+        System.err.println("new JettyProcessorTask "+this);
     }
 
-    public void doTask() throws IOException {
-        
+    public void doTask() throws IOException
+    {
+        System.err.println(this+" doTask ");
     }
 
-    public void taskEvent(TaskEvent event) {
+    public void taskEvent(TaskEvent event)
+    {
+        System.err.println(this+" taskEvent "+event);
     }
 
-    public void initialize() {
-        bbChannel = new ByteBufferChannel();
+    public void initialize()
+    {
     }
 
     /**
      * The Default ReadTask will invoke that method.
      */
-    public boolean process(InputStream input, OutputStream output) 
-        throws Exception {
+    public boolean process(InputStream input, OutputStream output) throws Exception
+    {
+        System.err.println(this+" process "+input);
+        JettySelectorThread selThread=(JettySelectorThread)selectorThread;
+        GrizzlyConnector grizzlyConnector=selThread.getGrizzlyConnector();
 
-        JettySelectorThread selThread = (JettySelectorThread)selectorThread;
-        GrizzlyConnector grizzlyConnector = selThread.getGrizzlyConnector();
+        // TODO This is wrong? where does the output go?
+        if (output==null)
+            output=System.out;
         
-        bbChannel.setByteBuffer( ((ByteBufferInputStream)input).getByteBuffer());
-        bbChannel.setSocketChannel( (SocketChannel) key.channel());
-        GrizzlyEndPoint gep = new GrizzlyEndPoint(grizzlyConnector,bbChannel);
+        // TODO How do we find the existing endpoint?
+        GrizzlyEndPoint endpoint=null;
         
-        // We are already using a Grizzly WorkerThread, so no need to 
+        if (endpoint==null)
+        {
+            endpoint=new GrizzlyEndPoint(grizzlyConnector,input,output,this);
+        }
+        else
+        {
+            // TODO maybe just check they have not changed?
+            if (endpoint.getInputStream()!=input)
+                throw new IllegalStateException();
+            endpoint.setOutputStream(output);
+        }
+
+        // We are already using a Grizzly WorkerThread, so no need to
         // invoke Jetty Thread Pool
-        gep.run();
-        
+        endpoint.handle();
+
         // How to find the keepAlive flag?
-        
-        return keepAlive;
+        return true;
     }
 
-    
-    /**
-     * Wrapper to pass to Jetty. Here we are copying bytes from
-     * one ByteBuffer to the other, which is bad.
-     * XXX FIX this coying
-     */
-    private class ByteBufferChannel implements ByteChannel{
-        
-        private ByteBuffer byteBuffer;
-        
-        private SocketChannel socketChannel;
-        
-        
-        public ByteBufferChannel(){
-        }
-        
-        
-        protected void setByteBuffer(ByteBuffer byteBuffer){
-            this.byteBuffer = byteBuffer;
-        }
-        
-        
-        protected void setSocketChannel(SocketChannel socketChannel){
-            this.socketChannel = socketChannel;
-        }
-        
-        
-        public int read(ByteBuffer dst) throws IOException {
-            int pos = dst.position();
-            dst.put(byteBuffer);
-            
-            // Now let see if we can use a temporary Selector to read more bytes
-            // Make this configurable ???
-            if ( dst.remaining() > 0)
-                readAllBytes(dst);    
-            return dst.position() - pos;
-        }
-        
-        
-        /**
-         * Use the temporary <code>Selector</code> to try to loads as much as we
-         * can available bytes before delegating the request processing to 
-         * <code>AsyncWeb</code>
-         */
-        private void readAllBytes(java.nio.ByteBuffer byteBuffer) throws IOException{
-            int count = 1;
-            int byteRead = 0;
-            Selector readSelector = null;
-            SelectionKey tmpKey = null;
 
-            try{
-                SocketChannel socketChannel = (SocketChannel)key.channel();
-                while (count > 0){
-                    count = socketChannel.read(byteBuffer);
-                    if ( count > 0 )
-                        byteRead += count;
-                }            
-
-                if ( byteRead == 0 ){
-                    readSelector = SelectorFactory.getSelector();
-
-                    if ( readSelector == null ){
-                        return;
-                    }
-                    count = 1;
-                    tmpKey = socketChannel
-                            .register(readSelector,SelectionKey.OP_READ);               
-                    tmpKey.interestOps(tmpKey.interestOps() | SelectionKey.OP_READ);
-                    int code = readSelector.selectNow();
-                    tmpKey.interestOps(
-                        tmpKey.interestOps() & (~SelectionKey.OP_READ));
-
-                    if ( code == 0 ){
-                        return;
-                    }
-
-                    while (count > 0){
-                        count = socketChannel.read(byteBuffer);
-                        if ( count > 0 )
-                            byteRead += count;                 
-                    }
-                }
-            } finally {
-                if (tmpKey != null)
-                    tmpKey.cancel();
-
-                if ( readSelector != null){
-                    // Bug 6403933
-                    try{
-                        readSelector.selectNow();
-                    } catch (IOException ex){
-                        ;
-                    }
-                    SelectorFactory.returnSelector(readSelector);
-                }
-            }
-        }
-    
-        
-        public boolean isOpen() {
-            return true;
-        }
-
-        public void close() throws IOException {
-        }
-
-        
-        public int write(ByteBuffer src) throws IOException {
-            int len = byteBuffer.flip().limit();
-            OutputWriter.flushChannel(socketChannel,src);
-            return len;
-        }
-        
-        
-        public void recycle(){
-            byteBuffer = null;
-            socketChannel = null;
-        }
-        
+    public void recycle()
+    {
+        System.err.println("recycle");
     }
-    
-    
-    public void recycle(){
-        bbChannel.recycle();
-    }    
-    
-    // ---------------------------------------------------- Not Used for now ---//
-    
-    
-    public int getBufferSize() {
+
+    // ---------------------------------------------------- Not Used for now
+    // ---//
+
+    public int getBufferSize()
+    {
         return -1;
     }
 
-    public boolean getDropConnection() {
+    public boolean getDropConnection()
+    {
         return false;
     }
 
-    public int getMaxPostSize() {
+    public int getMaxPostSize()
+    {
         return -1;
     }
 
-    public void invokeAdapter() {
-    }
-    
-    
-    public void setBufferSize(int requestBufferSize) {
+    public void invokeAdapter()
+    {
     }
 
-    public void setDropConnection(boolean dropConnection) {
+    public void setBufferSize(int requestBufferSize)
+    {
     }
 
-    public void setHandler(Handler handler) {
-        this.handler = handler;
+    public void setDropConnection(boolean dropConnection)
+    {
     }
 
-    public Handler getHandler() {
+    public void setHandler(Handler handler)
+    {
+        this.handler=handler;
+    }
+
+    public Handler getHandler()
+    {
         return handler;
     }
 
-    public void setMaxHttpHeaderSize(int maxHttpHeaderSize) {
+    public void setMaxHttpHeaderSize(int maxHttpHeaderSize)
+    {
     }
 
-    public void setMaxPostSize(int mps) {
+    public void setMaxPostSize(int mps)
+    {
     }
 
-    public void setSocket(Socket socket) {
+    public void setSocket(Socket socket)
+    {
     }
 
-    public void setTimeout(int timeouts) {
+    public void setTimeout(int timeouts)
+    {
     }
 
-    public void terminateProcess() {
+    public void terminateProcess()
+    {
     }
 
-    public String getRequestURI() {
-        return null; 
+    public String getRequestURI()
+    {
+        return null;
     }
 
-    public long getWorkerThreadID() {
+    public long getWorkerThreadID()
+    {
         return -1;
     }
 
-    public boolean isKeepAlive() {
-        return keepAlive;
+    public boolean isKeepAlive()
+    {
+        // TODO
+        return true;
     }
 
-    public boolean isError() {
+    public boolean isError()
+    {
         return false;
     }
 
     // --------------------------------------------------- Grizzly ARP ------//
-    
-    
-    public void parseRequest() throws Exception {
+
+    public void parseRequest() throws Exception
+    {
+        System.err.println("pareRequest()");
     }
 
-    public boolean parseRequest(InputStream input, OutputStream output, boolean keptAlive) throws Exception {
+    public boolean parseRequest(InputStream input, OutputStream output, boolean keptAlive) throws Exception
+    {
+        System.err.println("pareRequest(in,out,ka)");
         return true;
     }
 
-    public void postProcess() throws Exception {
+    public void postProcess() throws Exception
+    {
+        System.err.println("postProcess()");
     }
 
-    public void postProcess(InputStream input, OutputStream output) throws Exception {
+    public void postProcess(InputStream input, OutputStream output) throws Exception
+    {
+        System.err.println("postProcess(in,out)");
     }
 
-    public void postResponse() throws Exception {
+    public void postResponse() throws Exception
+    {
+        System.err.println("(postResponse)");
     }
 
-    public void preProcess() throws Exception {
+    public void preProcess() throws Exception
+    {
+        System.err.println("preProcess()");
     }
 
-    public void preProcess(InputStream input, OutputStream output) throws Exception {
-    }   
+    public void preProcess(InputStream input, OutputStream output) throws Exception
+    {
+        System.err.println("preProcess(in,out)");
+    }
 }
