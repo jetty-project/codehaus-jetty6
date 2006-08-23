390985bf0351bbf30735048e0ec27ada0a4d3b3a
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-08-23 13:37:01 +0000

    moved parsing to a ReadTask

--- extras/grizzly/src/main/java/org/mortbay/jetty/grizzly/GrizzlyConnector.java
+++ extras/grizzly/src/main/java/org/mortbay/jetty/grizzly/GrizzlyConnector.java
@@ -1,7 +1,6 @@
 //========================================================================
-// License and copyright status is a work in progress.
 // Parts Copyright 2006 Mort Bay Consulting Pty. Ltd.
-// Parts Copyright 2006 Sun Micro system and perhaps CDDL.
+// Parts Copyright 2006 Jeanfrancois Arcand
 //------------------------------------------------------------------------
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,29 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//========================================================================*
-/*
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License).  You may not use this file except in
- * compliance with the License.
- *
- * You can obtain a copy of the license at
- * https://glassfish.dev.java.net/public/CDDLv1.0.html or
- * glassfish/bootstrap/legal/CDDLv1.0.txt.
- * See the License for the specific language governing
- * permissions and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at glassfish/bootstrap/legal/CDDLv1.0.txt.
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * you own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
- */
+//========================================================================
+
+
 
 package org.mortbay.jetty.grizzly;
 
