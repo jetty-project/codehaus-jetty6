cad6b85de4489a6fbb2d6346d738adff1366afca
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-08-24 10:34:50 +0000

    clean up code

---
+++ jetty/trunk/extras/cometd/src/main/java/org/mortbay/cometd/JSONDataFilter.java
@@ -0,0 +1,12 @@
+package org.mortbay.cometd;
+
+public class JSONDataFilter implements DataFilter
+{
+
+    public Object filter(Object data)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
