65734231b61ca2031098bcbac7446824f2a9fe2e
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-09-18 13:10:39 +0000

    refactored session lifecycle methods and added x context dispatch tests

--- trunk/modules/jetty/src/main/java/org/mortbay/jetty/SessionIdManager.java
+++ trunk/modules/jetty/src/main/java/org/mortbay/jetty/SessionIdManager.java
@@ -12,11 +12,34 @@ import org.mortbay.component.LifeCycle;
  */
 public interface SessionIdManager extends LifeCycle
 {
+    /**
+     * @param id
+     * @return True if the session ID is in use by at least one context.
+     */
     public boolean idInUse(String id);
     
+    /**
+     * Add a session to the list of known sessions for a given ID.
+     * @param session The session
+     */
     public void addSession(HttpSession session);
     
+    /**
+     * Remove session from the list of known sessions for a given ID.
+     * @param session
+     */
+    public void removeSession(HttpSession session);
+    
+    /**
+     * Call {@link HttpSession#invalidate()} on all known sessions for the given id.
+     * @param id
+     */
     public void invalidateAll(String id);
     
+    /**
+     * @param request
+     * @param created
+     * @return
+     */
     public String newSessionId(HttpServletRequest request,long created);
 }
