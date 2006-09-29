86ffcd0b85fd9e07ce2fd48d5c6bd618ca538b48
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-09-29 14:19:47 +0000

    Implemented DataFilter configuration

--- extras/cometd/src/main/java/org/mortbay/cometd/filter/NoScriptsFilter.java
+++ extras/cometd/src/main/java/org/mortbay/cometd/filter/NoScriptsFilter.java
@@ -3,12 +3,12 @@ package org.mortbay.cometd.filter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.mortbay.cometd.JSONDataFilter;import org.mortbay.util.StringUtil;
-;
+import org.mortbay.util.StringUtil;
+
 
 public class NoScriptsFilter extends JSONDataFilter
 {
-    private static Pattern __script=Pattern.compile("<\\s*script");
+    private static Pattern __script=Pattern.compile("<\\s*[Ss][Cc][Rr][Ii][Pp][Tt]");
 
     protected Object filterString(String string)
     {
