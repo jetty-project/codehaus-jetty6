cf431490ceb306d3b4ac6337f364af5acdd76386
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-09-30 08:33:19 +0000

    added setuid server for zimbra

---
+++ trunk/extras/setuid/src/main/java/org/mortbay/setuid/SetUID.java
@@ -0,0 +1,15 @@
+
+package org.mortbay.setuid;
+
+public class SetUID
+{
+    public static final int OK = 0;
+    public static final int ERROR = -1;
+
+    public static native int setuid(int uid);
+
+    static 
+    {
+    	System.loadLibrary("setuid");
+    }
+}
