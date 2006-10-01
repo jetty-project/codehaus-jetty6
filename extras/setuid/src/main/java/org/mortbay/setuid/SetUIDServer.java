85075885c9045a7b2ab3681f50b8395f3a1107ee
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-10-01 13:59:27 +0000

    ooops adding setUid server

---
+++ extras/setuid/src/main/java/org/mortbay/setuid/SetUIDServer.java
@@ -0,0 +1,39 @@
+package org.mortbay.setuid;
+
+import org.mortbay.jetty.Server;
+import org.mortbay.log.Log;
+
+/**
+ * This extension of {@link Server} will make a JNI call to set the unix UID
+ * after the server has been started.
+ * This can be used to start the server as root so that priviledged ports may
+ * be accessed and then switch to a non-root user for security.
+ * 
+ * @author gregw
+ *
+ */
+public class SetUIDServer extends Server
+{
+    int _uid=0;
+
+    public int getUid()
+    {
+        return _uid;
+    }
+
+    public void setUid(int uid)
+    {
+        _uid=uid;
+    }
+    
+    protected void doStart() throws Exception
+    {
+        super.doStart();
+        if (_uid!=0)
+        {
+            Log.info("Setting UID="+_uid);
+            SetUID.setuid(_uid);
+        }
+    }
+    
+}
