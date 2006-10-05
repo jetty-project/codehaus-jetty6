cfa04a947e174c2ff3206a27b185baa524eb1841
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-10-05 12:41:58 +0000

    JETTY-113 applied to getQueryString

--- modules/jetty/src/main/java/org/mortbay/jetty/HttpURI.java
+++ modules/jetty/src/main/java/org/mortbay/jetty/HttpURI.java
@@ -420,7 +420,7 @@ public class HttpURI
     {
         if (_query==_fragment)
             return null;
-        return StringUtil.toString(_raw,_query+1,_fragment-_query-1,encoding);
+        return StringUtil.toString(_raw,_query+1,_fragment-_query-1,encoding==null?URIUtil.__CHARSET:encoding);
     }
     
     public String getFragment()
