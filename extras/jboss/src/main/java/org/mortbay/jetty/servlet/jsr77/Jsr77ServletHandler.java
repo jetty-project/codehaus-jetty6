ccfb0eeb18ffbc82d245b59a6c77adab01f7edd6
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-11-13 09:03:42 +0000

    ensure jsr77servletholder is constructed

--- extras/jboss/src/main/java/org/mortbay/jetty/servlet/jsr77/Jsr77ServletHandler.java
+++ extras/jboss/src/main/java/org/mortbay/jetty/servlet/jsr77/Jsr77ServletHandler.java
@@ -4,9 +4,13 @@ import org.mortbay.jetty.servlet.ServletHolder;
 
 public class Jsr77ServletHandler extends ServletHandler 
 {
+    
+    public ServletHolder newServletHolder ()
+    {
+        return new Jsr77ServletHolder();
+    }
 	public ServletHolder newServletHolder(Class servlet) 
 	{
-        System.err.println("ADDING A NEW JSR77ServletHolder");
 		return new Jsr77ServletHolder(servlet);
 	}
 }
