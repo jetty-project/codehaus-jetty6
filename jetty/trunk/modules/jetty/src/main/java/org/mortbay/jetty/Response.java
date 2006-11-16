e0c7369804d7a8dbb86dc174f8f2e78a20a0dd58
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-11-16 18:31:27 +0000

    simplified resource cache and default servlet

--- jetty/trunk/modules/jetty/src/main/java/org/mortbay/jetty/Response.java
+++ jetty/trunk/modules/jetty/src/main/java/org/mortbay/jetty/Response.java
@@ -960,6 +960,12 @@ public class Response implements HttpServletResponse
     }
 
     /* ------------------------------------------------------------ */
+    public HttpFields getHttpFields()
+    {
+        return _connection.getResponseFields();
+    }
+    
+    /* ------------------------------------------------------------ */
     public String toString()
     {
         return "HTTP/1.1 "+_status+" "+ (_reason==null?"":_reason) +System.getProperty("line.separator")+
@@ -975,5 +981,6 @@ public class Response implements HttpServletResponse
         {
         }
     }
+
 
 }
