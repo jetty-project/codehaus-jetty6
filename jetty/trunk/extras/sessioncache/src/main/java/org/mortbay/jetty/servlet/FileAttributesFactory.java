6f94f6dd5b797cf201f9a2e7682356d74db83097
ngonzalez <ngonzalez@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-12-15 09:46:22 +0000

    the cache now contains elements that have the session's clusterid as key and the attributeMap as value. Moved the _sessionCache to the SessionManager from the Session since the context will have a single instance of the cache just like it has 1 sessionManager. the cache is now a selfpopulatngcache. It returns a null default value for now (TODO). Also removed the overridden methods in session and just did the underlying cache functions in the FileSessionAttributeMap implementation. Also changed the instantiation of _cacheManager in CacheSessionIdManager so that a default cache can be added and the directory path for the diskstore can be configured.

--- jetty/trunk/extras/sessioncache/src/main/java/org/mortbay/jetty/servlet/FileAttributesFactory.java
+++ jetty/trunk/extras/sessioncache/src/main/java/org/mortbay/jetty/servlet/FileAttributesFactory.java
@@ -1,215 +1,154 @@
 package org.mortbay.jetty.servlet;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import javax.servlet.http.HttpSession;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
 
 import org.mortbay.jetty.servlet.CacheSessionManager.AttributesFactory;
-import org.mortbay.log.Log;
 
 public class FileAttributesFactory implements AttributesFactory
 {
     // TODO replace this with ehcache disk store.
     
-    private File _sessionAttributeDir;
+    private Ehcache _cache;
 
-    public FileAttributesFactory(String dir)
+    public Map create(String id, Ehcache cache)
     {
-        _sessionAttributeDir = new File(dir);
-        if (_sessionAttributeDir.exists()) _sessionAttributeDir.delete();
-        _sessionAttributeDir.mkdir();        
-    }
-
-    
-    public FileAttributesFactory()
-    {
-        this(System.getProperty("java.io.tmpdir","."));
-    }
-
-
-    public Map create(String id, String context)
-    {
+        _cache = cache;
         // TODO Auto-generated method stub
-        return new FileSessionAttributeMap(id, context);
+        return new FileSessionAttributeMap(id);
     }
 
     class FileSessionAttributeMap implements Map
     {
-        private HashMap _sessionAttributes;
         private String _clusterId;
-        private String _context;
-        private File _contextSubDir;
-
-        public FileSessionAttributeMap(String clusterId, String context)
+        public FileSessionAttributeMap(String clusterId)
         {
             _clusterId = clusterId;
-            _context = context;
-            _sessionAttributes = new HashMap();
-            _contextSubDir = new File(_sessionAttributeDir+"/"+_context);
-            
-            if (!_contextSubDir.exists())
-                _contextSubDir.mkdir();
-            else if(!_contextSubDir.isDirectory())
-                throw new IllegalStateException("context sub directory is not a directory");
-            
-            writeMap();
+            putAttributeMap(new HashMap());
+            _cache.flush();
         }
-
+        
+        private Map getAttributeMap()
+        {
+            Element element = _cache.get(_clusterId);
+            return (Map)element.getObjectValue();
+        }
+        
+        private void putAttributeMap(Map attributeMap)
+        {
+            _cache.put(new Element(_clusterId,attributeMap));
+        }
+        
         public void clear()
         {
-            if (_sessionAttributes != null)
-            _sessionAttributes.clear();
-            writeMap();
-
+            Map attributeMap = getAttributeMap();
+            if (attributeMap != null)
+            attributeMap.clear();
+            putAttributeMap(attributeMap);
         }
 
         public boolean containsKey(Object obj)
         {
-            readMap();
-            if (_sessionAttributes != null)
-                return _sessionAttributes.containsKey(obj);
+            Map attributeMap = getAttributeMap();
+            if (attributeMap != null)
+                return attributeMap.containsKey(obj);
             return false;
         }
 
         public boolean containsValue(Object obj)
         {
-            readMap();
-            if (_sessionAttributes != null)
-                return _sessionAttributes.containsValue(obj);
+            Map attributeMap =getAttributeMap();
+            if (attributeMap != null)
+                return attributeMap.containsValue(obj);
             return false;
         }
 
         public Set entrySet()
         {
-            readMap();
-            if (_sessionAttributes != null)
-                _sessionAttributes.entrySet();
+            Map attributeMap = getAttributeMap();
+            if (attributeMap != null)
+                attributeMap.entrySet();
             return null;
         }
 
         public Object get(Object obj)
         {
-            readMap();
-            if (_sessionAttributes != null)
-                return _sessionAttributes.get(obj);
+            Map attributeMap = getAttributeMap();
+            if (attributeMap != null)
+                return attributeMap.get(obj);
             return null;
         }
 
         public boolean isEmpty()
         {
-            readMap();
-            if (_sessionAttributes != null)
-                _sessionAttributes.isEmpty();
+            Map attributeMap = getAttributeMap();
+            if (attributeMap != null)
+                attributeMap.isEmpty();
             return true;
         }
 
         public Set keySet()
         {
-            readMap();
-            if (_sessionAttributes != null)
-                return _sessionAttributes.keySet();
+            Map attributeMap = getAttributeMap();
+            if (attributeMap != null)
+                return attributeMap.keySet();
             return null;
         }
 
         public Object put(Object key, Object value)
         {
-            readMap();
+            Map attributeMap = getAttributeMap();
             Object obj = null;
-            if (_sessionAttributes != null)
+            if (attributeMap != null)
             {
-                obj = _sessionAttributes.put(key, value);
-                writeMap();
+                obj = attributeMap.put(key, value);
             }
+            putAttributeMap(attributeMap);
             return obj;
         }
 
         public void putAll(Map map)
         {
-            readMap();
-            if (_sessionAttributes != null)
+            Map attributeMap = getAttributeMap();
+            if (attributeMap != null)
             {
-                _sessionAttributes.putAll(map);
-                writeMap();
+                attributeMap.putAll(map);
             }
+            putAttributeMap(attributeMap);
         }
 
         public Object remove(Object obj)
         {
-            readMap();
+            Map attributeMap = getAttributeMap();
             Object object = null;
-            if (_sessionAttributes != null)
+            if (attributeMap != null)
             {
-                object = _sessionAttributes.remove(obj);
-                writeMap();
+                object = attributeMap.remove(obj);
             }
+            putAttributeMap(attributeMap);
             return object;
         }
 
         public int size()
         {
-            readMap();
-            if (_sessionAttributes != null)
-                return _sessionAttributes.size();
+            Map attributeMap = getAttributeMap();
+            if (attributeMap != null)
+                return attributeMap.size();
             return 0;
         }
 
         public Collection values()
         {
-            readMap();
-            if (_sessionAttributes != null)
-                return _sessionAttributes.values();
+            Map attributeMap = getAttributeMap();
+            if (attributeMap != null)
+                return attributeMap.values();
             return null;
         }
-        
-        private void writeMap()
-        {
-            FileOutputStream out = null;
-            ObjectOutputStream oos = null;
-            
-            try
-            {
-                out = new FileOutputStream(_contextSubDir.getAbsolutePath()+"/"+_clusterId);
-                oos = new ObjectOutputStream(out);
-                oos.writeObject(_sessionAttributes);
-                oos.close();
-            }
-            catch(IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-        
-        public void readMap()
-        {
-            File sessionAttributeFile = new File(_contextSubDir.getAbsolutePath()+"/"+_clusterId);
-            if (sessionAttributeFile.exists())
-            {
-                try
-                {
-                    FileInputStream fileIn = new FileInputStream(sessionAttributeFile);
-                    ObjectInputStream in = new ObjectInputStream(fileIn);
-                    _sessionAttributes = (HashMap)in.readObject();
-                    in.close();
-                }
-                catch (IOException e)
-                {
-                    Log.debug(e);
-                }
-                catch (ClassNotFoundException e)
-                {
-                    Log.debug(e);
-                }
-            }
-        }
-
     }
 }
