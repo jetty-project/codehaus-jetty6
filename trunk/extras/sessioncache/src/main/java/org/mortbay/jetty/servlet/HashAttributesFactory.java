40ab6a8b3b62e48ca69476535337e364d1b4653b
ngonzalez <ngonzalez@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2006-12-15 09:46:22 +0000

    the cache now contains elements that have the session's clusterid as key and the attributeMap as value. Moved the _sessionCache to the SessionManager from the Session since the context will have a single instance of the cache just like it has 1 sessionManager. the cache is now a selfpopulatngcache. It returns a null default value for now (TODO). Also removed the overridden methods in session and just did the underlying cache functions in the FileSessionAttributeMap implementation. Also changed the instantiation of _cacheManager in CacheSessionIdManager so that a default cache can be added and the directory path for the diskstore can be configured.

--- trunk/extras/sessioncache/src/main/java/org/mortbay/jetty/servlet/HashAttributesFactory.java
+++ trunk/extras/sessioncache/src/main/java/org/mortbay/jetty/servlet/HashAttributesFactory.java
@@ -3,12 +3,14 @@ package org.mortbay.jetty.servlet;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.sf.ehcache.Ehcache;
+
 import org.mortbay.jetty.servlet.CacheSessionManager.AttributesFactory;
 
 public class HashAttributesFactory implements AttributesFactory
 {
 
-    public Map create(String id, String context)
+    public Map create(String id, Ehcache cach)
     {
         // TODO Auto-generated method stub
         return new HashMap(3);
