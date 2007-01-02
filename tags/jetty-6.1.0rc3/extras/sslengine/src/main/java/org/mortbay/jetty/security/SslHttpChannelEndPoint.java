158d088247c30a0cd274d05bce90a016fe080ca6
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2007-01-02 17:53:39 +0000

    release

---
+++ tags/jetty-6.1.0rc3/extras/sslengine/src/main/java/org/mortbay/jetty/security/SslHttpChannelEndPoint.java
@@ -0,0 +1,426 @@
+package org.mortbay.jetty.security;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLEngineResult;
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSession;
+
+import org.mortbay.io.Buffer;
+import org.mortbay.io.nio.NIOBuffer;
+import org.mortbay.io.nio.SelectorManager;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.mortbay.log.Log;
+
+/* ------------------------------------------------------------ */
+/**
+ * SslHttpChannelEndPoint.
+ * 
+ * @author Nik Gonzalez <ngonzalez@exist.com>
+ * @author Greg Wilkins <gregw@mortbay.com>
+ */
+public class SslHttpChannelEndPoint extends SelectChannelConnector.ConnectorEndPoint implements Runnable
+{
+    private static ByteBuffer[] __NO_BUFFERS={};
+    private static ByteBuffer __EMPTY=ByteBuffer.allocate(0);
+    private static SSLEngineResult _result;
+    
+    private final SSLEngine _engine;
+    private final ByteBuffer _inBuffer;
+    private final NIOBuffer _inNIOBuffer;
+    private final ByteBuffer _outBuffer;
+    private final NIOBuffer _outNIOBuffer;
+
+    private ByteBuffer _reuseBuffer;    
+    private final ByteBuffer[] _outBuffers=new ByteBuffer[3];
+
+    // ssl
+    private final SSLSession _session;
+
+    /* ------------------------------------------------------------ */
+    public SslHttpChannelEndPoint(SocketChannel channel, SelectorManager.SelectSet selectSet, SelectionKey key, SSLEngine engine)
+            throws SSLException, IOException
+    {
+        super(channel,selectSet,key);
+
+        
+        // ssl
+        _engine=engine;
+        _engine.setUseClientMode(false);
+        _session=engine.getSession();
+
+        _outNIOBuffer=new NIOBuffer(_session.getPacketBufferSize(),true);
+        _outBuffer=_outNIOBuffer.getByteBuffer();
+        _inNIOBuffer=new NIOBuffer(_session.getPacketBufferSize(),true);
+        _inBuffer=_inNIOBuffer.getByteBuffer();
+
+    }
+
+    /* ------------------------------------------------------------ */
+    public void close() throws IOException
+    {
+        _engine.closeOutbound();
+        
+        try
+        {   
+            loop: while (_inBuffer.remaining()>0)
+            {
+                switch(_engine.getHandshakeStatus())
+                {
+                    case FINISHED:
+                    case NOT_HANDSHAKING:
+                        break loop;
+                        
+                    case NEED_UNWRAP:
+                        if(!fill(__EMPTY))
+                            break loop; // TODO may need to reschedule?
+                        break;
+                        
+                    case NEED_TASK:
+                    {
+                        Runnable task;
+                        while ((task=_engine.getDelegatedTask())!=null)
+                        {
+                            task.run();
+                        }
+                        break;
+                    }
+                        
+                    case NEED_WRAP:
+                    {
+                        flush();
+                        
+                        SSLEngineResult result=null;
+                        try
+                        {
+                            _outBuffer.position(_outNIOBuffer.putIndex());
+                            result=_engine.wrap(__NO_BUFFERS,_outBuffer);
+                        }
+                        finally
+                        {
+                            _outBuffer.position(0);
+                            _outNIOBuffer.setGetIndex(0);
+                            _outNIOBuffer.setPutIndex(result.bytesProduced());
+                        }
+                        
+                        flush();
+                        
+                        break;
+                    }
+                }
+            }
+            
+        }
+        finally
+        {
+            super.close();
+        }
+        
+        
+    }
+
+    /* ------------------------------------------------------------ */
+    /* 
+     */
+    public int fill(Buffer buffer) throws IOException
+    {
+        ByteBuffer bbuf=extractInputBuffer(buffer);
+        int size=buffer.length();
+		 
+        try
+        {
+            fill(bbuf);
+            
+            loop: while (_inBuffer.remaining()>0)
+            {
+                switch(_engine.getHandshakeStatus())
+                {
+                    case FINISHED:
+                    case NOT_HANDSHAKING:
+                        break loop;
+                        
+                    case NEED_UNWRAP:
+                        if(!fill(bbuf))
+                            break loop;
+                        break;
+                        
+                    case NEED_TASK:
+                    {
+                        Runnable task;
+                        while ((task=_engine.getDelegatedTask())!=null)
+                        {
+                            task.run();
+                        }
+                        break;
+                    }
+                        
+                    case NEED_WRAP:
+                    {
+                        flush();
+                        
+                        SSLEngineResult result=null;
+                        try
+                        {
+                            _outBuffer.position(_outNIOBuffer.putIndex());
+                            result=_engine.wrap(__NO_BUFFERS,_outBuffer);
+                        }
+                        finally
+                        {
+                            _outBuffer.position(0);
+                            _outNIOBuffer.setGetIndex(0);
+                            _outNIOBuffer.setPutIndex(result.bytesProduced());
+                        }
+                    
+                        flush();
+                        
+                        break;
+                    }
+                }
+            }
+            
+        }
+        finally
+        {
+            buffer.setPutIndex(bbuf.position());
+            bbuf.position(0);
+        }
+        
+        return buffer.length()-size;
+    }
+
+    /* ------------------------------------------------------------ */
+    public int flush(Buffer buffer) throws IOException
+    {
+        return flush(buffer,null,null);
+    }
+
+
+    /* ------------------------------------------------------------ */
+    /*     
+     */
+    public int flush(Buffer header, Buffer buffer, Buffer trailer) throws IOException
+    {
+        if (_outNIOBuffer.length()>0)
+        {
+            flush();
+            if (_outNIOBuffer.length()>0)
+                return 0;
+        }
+        
+        _outBuffers[0]=extractOutputBuffer(header);
+        _outBuffers[1]=extractOutputBuffer(buffer);
+        _outBuffers[2]=extractOutputBuffer(trailer);
+        
+        try
+        {
+            _outNIOBuffer.clear();
+            _outBuffer.position(0);
+            _outBuffer.limit(_outBuffer.capacity());
+            _result=_engine.wrap(_outBuffers,_outBuffer);
+        }
+        finally
+        {
+            _outBuffer.position(0);
+            _outNIOBuffer.setGetIndex(0);
+            _outNIOBuffer.setPutIndex(_result.bytesProduced());
+            
+            int consumed=_result.bytesConsumed();
+            if (consumed>0 && header!=null)
+            {
+                int len=consumed<header.length()?consumed:header.length();
+                header.skip(len);
+                consumed-=len;
+                _outBuffers[0].position(0);
+                _outBuffers[0].limit(_outBuffers[0].capacity());
+            }
+            if (consumed>0 && buffer!=null)
+            {
+                int len=consumed<buffer.length()?consumed:buffer.length();
+                buffer.skip(len);
+                consumed-=len;
+                _outBuffers[1].position(0);
+                _outBuffers[1].limit(_outBuffers[1].capacity());
+            }
+            if (consumed>0 && trailer!=null)
+            {
+                int len=consumed<trailer.length()?consumed:trailer.length();
+                trailer.skip(len);
+                consumed-=len;
+                _outBuffers[1].position(0);
+                _outBuffers[1].limit(_outBuffers[1].capacity());
+            }
+            assert consumed==0;
+        }
+    
+        flush();
+        
+        return _result.bytesConsumed();
+    }
+
+    
+    /* ------------------------------------------------------------ */
+    public void flush() throws IOException
+    {
+        while (_outNIOBuffer.length()>0)
+        {
+            int flushed=super.flush(_outNIOBuffer);
+            if (flushed==0)
+            {
+                Thread.yield();
+                flushed=super.flush(_outNIOBuffer);
+                if (flushed==0)
+                    return;
+            }
+        }
+    }
+
+    /* ------------------------------------------------------------ */
+    private ByteBuffer extractInputBuffer(Buffer buffer)
+    {
+        assert buffer instanceof NIOBuffer;
+        NIOBuffer nbuf=(NIOBuffer)buffer;
+        ByteBuffer bbuf=nbuf.getByteBuffer();
+        bbuf.position(buffer.putIndex());
+        return bbuf;
+    }
+    
+    /* ------------------------------------------------------------ */
+    private ByteBuffer extractOutputBuffer(Buffer buffer)
+    {
+        if(buffer==null)
+            return __EMPTY;
+        
+        ByteBuffer src=null;
+        NIOBuffer nBuf=null;
+
+        if (buffer.buffer() instanceof NIOBuffer)
+        {
+            nBuf=(NIOBuffer)buffer.buffer();
+            src=nBuf.getByteBuffer();
+        }
+        else
+        {
+        	if (_reuseBuffer == null)
+        	{
+        		_reuseBuffer = ByteBuffer.allocateDirect(_session.getPacketBufferSize());
+        	}
+            _reuseBuffer.put(buffer.asArray());
+            src = _reuseBuffer;
+        }
+
+        if (src!=null)
+        {
+            src.position(buffer.getIndex());
+            src.limit(buffer.putIndex());
+        }
+
+        return src;
+    }
+
+    /* ------------------------------------------------------------ */
+    private boolean fill(ByteBuffer buffer) throws IOException
+    {
+        int in_len=0;
+        
+        if (!_inNIOBuffer.hasContent() || (_result != null && _result.getStatus() == SSLEngineResult.Status.BUFFER_UNDERFLOW))
+        {
+            _inNIOBuffer.clear();
+            while (_inNIOBuffer.space()>0)
+            {
+                int len=super.fill(_inNIOBuffer);
+                if (len<=0)
+                    break;
+                in_len+=len;
+            }
+        }
+
+        if (_inNIOBuffer.length()==0)
+            return false;
+        
+        try
+        {
+            _inBuffer.position(_inNIOBuffer.getIndex());
+            _inBuffer.limit(_inNIOBuffer.putIndex());
+            _result=_engine.unwrap(_inBuffer,buffer);
+            if (_result != null)
+            {
+            	if (_result.getStatus() == SSLEngineResult.Status.OK)	
+            		_inNIOBuffer.skip(_result.bytesConsumed());
+            	else if (_result.getStatus() == SSLEngineResult.Status.CLOSED)
+            		throw new IOException("sslEngine closed");
+            }
+            
+        }
+        finally
+        {
+            _inBuffer.position(0);
+            _inBuffer.limit(_inBuffer.capacity());
+        }
+        
+        switch(_result.getStatus())
+        {
+            case OK:
+            case CLOSED:
+                break;
+            case BUFFER_UNDERFLOW:
+            	break;
+            case BUFFER_OVERFLOW:
+            	buffer.clear();
+            	break;
+            default:
+                Log.warn("unwrap "+_result);
+                throw new IOException(_result.toString());
+        }
+        
+        return (_result.bytesProduced()+_result.bytesConsumed())>0;
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * Updates selection key. Adds operations types to the selection key as needed. No operations
+     * are removed as this is only done during dispatch. This method records the new key and
+     * schedules a call to syncKey to do the keyChange
+     */
+    protected void updateKey()
+    {
+        synchronized (this)
+        {
+            int ops = _key == null ? 0 : _key.interestOps();
+            _interestOps = ops | ((!_dispatched || _readBlocked) ? SelectionKey.OP_READ : 0) | (_writable && !_writeBlocked && !isBufferingOutput() ? 0 : SelectionKey.OP_WRITE);
+            _writable = true; // Once writable is in ops, only removed with dispatch.
+
+            if (_interestOps != ops)
+            {
+                _selectSet.addChange(this);
+                _selectSet.wakeup();
+            }
+        }
+    }
+
+    /* ------------------------------------------------------------ */
+    public boolean isBufferingInput()
+    {
+        return _inNIOBuffer.hasContent();
+    }
+
+    /* ------------------------------------------------------------ */
+    public boolean isBufferingOutput()
+    {
+        return _outNIOBuffer.hasContent();
+    }
+
+    /* ------------------------------------------------------------ */
+    public boolean isBufferred()
+    {
+        return true;
+    }
+
+    public SSLEngine getSSLEngine()
+    {
+        return _engine;
+    }
+}
---
+++ tags/jetty-6.1.0rc3/modules/jetty/src/main/java/org/mortbay/jetty/webapp/TagLibConfiguration.java
@@ -0,0 +1,281 @@
+//========================================================================
+//$Id: TagLibConfiguration.java,v 1.4 2005/08/13 00:01:27 gregwilkins Exp $
+//Copyright 2004 Mort Bay Consulting Pty. Ltd.
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+
+package org.mortbay.jetty.webapp;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.mortbay.log.Log;
+import org.mortbay.resource.Resource;
+import org.mortbay.util.Loader;
+import org.mortbay.util.TypeUtil;
+import org.mortbay.xml.XmlParser;
+
+/* ------------------------------------------------------------ */
+/** TagLibConfiguration.
+ * 
+ * The class searches for TLD descriptors found in web.xml, in WEB-INF/*.tld files of the web app
+ * or *.tld files withing jars found in WEB-INF/lib of the webapp.   Any listeners defined in these
+ * tld's are added to the context.
+ * 
+ * &lt;bile&gt;This is total rubbish special case for JSPs! If there was a general use-case for web app
+ * frameworks to register listeners directly, then a generic mechanism could have been added to the servlet
+ * spec.  Instead some special purpose JSP support is required that breaks all sorts of encapsualtion rules as
+ * the servlet container must go searching for and then parsing the descriptors for one particular framework.
+ * It only appears to be used by JSF, which is being developed by the same developer who implemented this
+ * feature in the first place!
+ * &lt;/bile&gt;
+ * 
+ * @author gregw
+ *
+ */
+public class TagLibConfiguration implements Configuration
+{
+    WebAppContext _context;
+    String[] _serverTagLibClasses=
+    { 
+        "org.apache.jasper.servlet.JspServlet", //find tlds bundled with jasper (jstl)
+        "com.sun.faces.config.ConfigureListener", //find tlds from sun's jsf impl
+        "org.apache.myfaces.webapp.MyFacesServlet" //find tlds from myfaces impl
+    };
+    
+    public void setWebAppContext(WebAppContext context)
+    {
+        _context=context;
+    }
+
+    public WebAppContext getWebAppContext()
+    {
+        return _context;
+    }
+
+    public void configureClassLoader() throws Exception
+    {
+    }
+
+    /* ------------------------------------------------------------ */
+    /* 
+     * @see org.mortbay.jetty.servlet.WebAppContext.Configuration#configureDefaults()
+     */
+    public void configureDefaults() throws Exception
+    {
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * @return List of {@link Resource}s for jar files in which to search for TLDs
+     * @throws IOException 
+     * @throws MalformedURLException 
+     */
+    protected List getJarResourceList() throws MalformedURLException, IOException
+    {
+        List list = new ArrayList();
+
+        Resource web_inf=_context.getWebInf();
+        if (web_inf!=null && web_inf.exists())
+        {
+            Resource lib=web_inf.addPath("lib/");
+            if (lib.exists() && lib.isDirectory())
+            {
+                String[] contents = lib.list();
+                for (int i=0;i<contents.length;i++)
+                {
+                    if (contents[i]!=null && contents[i].toLowerCase().endsWith(".jar"))
+                        list.add(lib.addPath(contents[i]));
+                }
+            }
+        }
+        
+        list.addAll(getServerJarResourceList());
+        
+        Log.debug("TLD search {}",list);
+        return list;
+        
+    }
+    
+    protected List getServerJarResourceList () throws MalformedURLException, IOException
+    {
+        List list = new ArrayList();
+        
+        for (int i=0;_serverTagLibClasses!=null && i<_serverTagLibClasses.length;i++)
+        {
+            URL jar = TypeUtil.jarFor(_serverTagLibClasses[i]);
+            if (jar!=null)
+                list.add(Resource.newResource(jar));       
+        }
+        return list;
+    }
+
+    /* ------------------------------------------------------------ */
+    private void findTLDs(Set tlds,Resource dir) throws MalformedURLException, IOException
+    {
+        String[] meta_contents=dir.list();          
+        for (int j=0;j<meta_contents.length;j++)
+        {
+            Resource r=dir.addPath(meta_contents[j]);
+            if (r.isDirectory())
+                findTLDs(tlds,r);
+            else if (meta_contents[j].toLowerCase().endsWith(".tld"))
+                tlds.add(r);
+        }
+    }
+    
+    /* ------------------------------------------------------------ */
+    /* 
+     * @see org.mortbay.jetty.servlet.WebAppContext.Configuration#configureWebApp()
+     */
+    public void configureWebApp() throws Exception
+    {
+        Set tlds = new HashSet();
+        
+        // Find tld's from web.xml
+        // When the XMLConfigurator (or other configurator) parsed the web.xml,
+        // It should have created aliases for all TLDs.  So search resources aliases
+        // for aliases ending in tld
+        if (_context.getResourceAliases()!=null && 
+            _context.getBaseResource()!=null && 
+            _context.getBaseResource().exists())
+        {
+            Iterator iter=_context.getResourceAliases().values().iterator();
+            while(iter.hasNext())
+            {
+                String location = (String)iter.next();
+                if (location!=null && location.toLowerCase().endsWith(".tld"))
+                {
+                    if (!location.startsWith("/"))
+                        location="/WEB-INF/"+location;
+                    Resource l=_context.getBaseResource().addPath(location);
+                    tlds.add(l);
+                }
+            }
+        }
+        
+        // Look for any tlds in WEB-INF directly.
+        Resource web_inf = _context.getWebInf();
+        if (web_inf!=null)
+        {
+            String[] contents = web_inf.list();
+            for (int i=0;i<contents.length;i++)
+            {
+                if (contents[i]!=null && contents[i].toLowerCase().endsWith(".tld"))
+                {
+                    Resource l=_context.getWebInf().addPath(contents[i]);
+                    tlds.add(l);
+                }
+                
+            }
+        }
+        
+        // Look for tlds in any jars
+        List jars = getJarResourceList();
+        for (int i=0;i<jars.size();i++)
+        {
+            Resource jar=(Resource)jars.get(i);
+            Resource meta=Resource.newResource("jar:"+jar.getURL()+"!/META-INF/",false);
+            if (meta.exists())
+                findTLDs(tlds,meta);
+        }
+        
+        // Create a TLD parser
+        XmlParser parser = new XmlParser(false);
+        parser.redirectEntity("web-jsptaglib_1_1.dtd",Loader.getResource(TagLibConfiguration.class,"javax/servlet/jsp/resources/web-jsptaglibrary_1_1.dtd", false));
+        parser.redirectEntity("web-jsptaglib_1_2.dtd",Loader.getResource(TagLibConfiguration.class,"javax/servlet/jsp/resources/web-jsptaglibrary_1_2.dtd", false));
+        parser.redirectEntity("web-jsptaglib_2_0.xsd",Loader.getResource(TagLibConfiguration.class,"javax/servlet/jsp/resources/web-jsptaglibrary_2_0.xsd", false));
+        parser.redirectEntity("web-jsptaglibrary_1_1.dtd",Loader.getResource(TagLibConfiguration.class,"javax/servlet/jsp/resources/web-jsptaglibrary_1_1.dtd", false));
+        parser.redirectEntity("web-jsptaglibrary_1_2.dtd",Loader.getResource(TagLibConfiguration.class,"javax/servlet/jsp/resources/web-jsptaglibrary_1_2.dtd", false));
+        parser.redirectEntity("web-jsptaglibrary_2_0.xsd",Loader.getResource(TagLibConfiguration.class,"javax/servlet/jsp/resources/web-jsptaglibrary_2_0.xsd", false));
+        parser.setXpath("/taglib/listener/listener-class");
+        // Parse all the discovered TLDs
+        Iterator iter = tlds.iterator();
+        while (iter.hasNext())
+        {
+            try
+            {
+                Resource tld = (Resource)iter.next();
+                if (Log.isDebugEnabled()) Log.debug("TLD="+tld);
+                
+                XmlParser.Node root = parser.parse(tld.getURL().toString());
+
+		if (root==null)
+		{
+		    Log.warn("No TLD root in {}",tld);
+		    continue;
+		}
+                
+                for (int i=0;i<root.size();i++)
+                {
+                    Object o=root.get(i);
+                    if (o instanceof XmlParser.Node)
+                    {
+                        XmlParser.Node node = (XmlParser.Node)o;
+                        if ("listener".equals(node.getTag()))
+                        {
+                            String className=node.getString("listener-class",false,true);
+                            if (Log.isDebugEnabled()) Log.debug("listener="+className);
+                            
+                            try
+                            {
+                                Class listenerClass=getWebAppContext().loadClass(className);
+                                EventListener l=(EventListener)listenerClass.newInstance();
+                                _context.addEventListener(l);
+                            }
+                            catch(Exception e)
+                            {
+                                Log.warn("Could not instantiate listener "+className,e);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Log.warn(e);
+            }
+        }
+    }
+
+
+    public void deconfigureWebApp() throws Exception
+    {
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * @return
+     */
+    public String[] getServerTagLibClasses()
+    {
+        return _serverTagLibClasses;
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * @param serverTagLibClasses An array of class names. The jars that these 
+     * classes are loaded from will be examined for TLD files (even if these jars 
+     * are on the server or system classpath).
+     */
+    public void setServerTagLibClasses(String[] serverTagLibClasses)
+    {
+        _serverTagLibClasses=serverTagLibClasses;
+    }
+}
