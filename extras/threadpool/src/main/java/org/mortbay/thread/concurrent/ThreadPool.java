9bbe09d9f212b61ff8585aea2e342958be715580
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2007-01-04 08:53:58 +0000

    Select queue type by queue size

--- extras/threadpool/src/main/java/org/mortbay/thread/concurrent/ThreadPool.java
+++ extras/threadpool/src/main/java/org/mortbay/thread/concurrent/ThreadPool.java
@@ -14,10 +14,12 @@
 
 package org.mortbay.thread.concurrent;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -44,6 +46,20 @@ public class ThreadPool extends ThreadPoolExecutor implements org.mortbay.thread
     public ThreadPool()
     {
         super(32,256,60,TimeUnit.SECONDS,new LinkedBlockingQueue<Runnable>());
+    }
+    
+    /* ------------------------------------------------------------ */
+    /** Default constructor.
+     * Core size is 32, max pool size is 256, pool thread timeout after 60 seconds
+     * @param queueSize if -1, an unbounded {@link LinkedBlockingQueue} is used, if 0 then a
+     * {@link SynchronousQueue} is used, other a {@link ArrayBlockingQueue} of the given size is used.
+     */
+    public ThreadPool(int queueSize)
+    {
+        super(32,256,60,TimeUnit.SECONDS,
+                queueSize<0?new LinkedBlockingQueue<Runnable>()
+                        : (queueSize==0?new SynchronousQueue<Runnable>()
+                                :new ArrayBlockingQueue<Runnable>(queueSize)));
     }
 
     /* ------------------------------------------------------------ */
