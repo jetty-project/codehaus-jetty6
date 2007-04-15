09e5b983ee3464a34d8e3fde2ff0fb4a21fd9558
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2007-04-15 22:39:22 +0000

    log only printable usernames

--- trunk/modules/util/src/main/java/org/mortbay/util/StringUtil.java
+++ trunk/modules/util/src/main/java/org/mortbay/util/StringUtil.java
@@ -347,7 +347,7 @@ public class StringUtil
         if (name==null)
             return null;
         StringBuffer buf = new StringBuffer(name.length());
-        for (int i=0;i<name.length())
+        for (int i=0;i<name.length();i++)
         {
             char c=name.charAt(i);
             if (!Character.isISOControl(c))
