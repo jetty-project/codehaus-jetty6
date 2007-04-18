9e91f139b3daa71461de87e7a50fa0fa69507858
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2007-04-18 08:50:28 +0000

    JETTY-302

--- jetty/trunk/modules/jetty/src/test/java/org/mortbay/jetty/ResponseTest.java
+++ jetty/trunk/modules/jetty/src/test/java/org/mortbay/jetty/ResponseTest.java
@@ -16,12 +16,18 @@
 package org.mortbay.jetty;
 
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSessionContext;
 
+import org.mortbay.jetty.handler.AbstractHandler;
 import org.mortbay.jetty.handler.ContextHandler;
 import org.mortbay.jetty.servlet.AbstractSessionManager;
 import org.mortbay.jetty.servlet.HashSessionIdManager;
@@ -38,14 +44,16 @@ import junit.framework.TestCase;
  */
 public class ResponseTest extends TestCase
 {
-    Server server = new Server();
-    LocalConnector connector = new LocalConnector();
+    Server _server = new Server();
+    LocalConnector _connector = new LocalConnector();
+    RequestHandler _handler = new RequestHandler();
     
     public ResponseTest(String arg0)
     {
         super(arg0);
-        server.setConnectors(new Connector[]{connector});
-        server.setHandler(new DumpHandler());
+        _server.setConnectors(new Connector[]{_connector});
+        
+        _server.setHandler(_handler);
     }
 
     public static void main(String[] args)
@@ -60,7 +68,7 @@ public class ResponseTest extends TestCase
     {
         super.setUp();
         
-        server.start();
+        _server.start();
     }
 
     /*
@@ -69,383 +77,79 @@ public class ResponseTest extends TestCase
     protected void tearDown() throws Exception
     {
         super.tearDown();
-        server.stop();
+        _server.stop();
     }
     
     
-    public void testContentType()
+    public void testContentTypeEncoding()
     	throws Exception
     {
-
-        HttpConnection connection = new HttpConnection(connector,connector.endp,connector.server);
-        Response response = connection.getResponse();
+        final ArrayList results = new ArrayList();
+        _handler._checker = new RequestChecker()
+        {
+            public boolean check(HttpServletRequest request)
+            {
+                results.add(request.getContentType());
+                results.add(request.getCharacterEncoding());
+                return true;
+            }  
+        };
         
-        assertEquals(null,response.getContentType());
+        _connector.getResponses(
+                "GET / HTTP/1.1\n"+
+                "Host: whatever\n"+
+                "Content-Type: text/test\n"+
+                "\n"+
+               
+                "GET / HTTP/1.1\n"+
+                "Host: whatever\n"+
+                "Content-Type: text/html;charset=utf8\n"+
+                "\n"+
+                
+                "GET / HTTP/1.1\n"+
+                "Host: whatever\n"+
+                "Content-Type: text/html; charset=\"utf8\"\n"+
+                "\n"+
+                
+                "GET / HTTP/1.1\n"+
+                "Host: whatever\n"+
+                "Content-Type: text/html; other=foo ; blah=\"charset=wrong;\" ; charset =   \" x=z; \"   ; more=values \n"+
+                "\n"
+                );
         
-        response.setContentType("foo/bar");
-        assertEquals("foo/bar",response.getContentType());
-        response.getWriter();
-        assertEquals("foo/bar; charset=ISO-8859-1",response.getContentType());
-        response.setContentType("foo2/bar2");
-        assertEquals("foo2/bar2; charset=ISO-8859-1",response.getContentType());
-        response.setHeader("name","foo");
-        Enumeration en=response.getHeaders("name");
-        assertEquals("foo",en.nextElement());
-        assertFalse(en.hasMoreElements());
-        response.addHeader("name","bar");
-        en=response.getHeaders("name");
-        assertEquals("foo",en.nextElement());
-        assertEquals("bar",en.nextElement());
-        assertFalse(en.hasMoreElements());
+        int i=0;
+        assertEquals("text/test",results.get(i++));
+        assertEquals(null,results.get(i++));
         
-        response.recycle();
+        assertEquals("text/html;charset=utf8",results.get(i++));
+        assertEquals("utf8",results.get(i++));
         
-        response.setContentType("text/html");
-        assertEquals("text/html",response.getContentType());
-        response.getWriter();
-        assertEquals("text/html; charset=iso-8859-1",response.getContentType());
-        response.setContentType("foo2/bar2");
-        assertEquals("foo2/bar2; charset=ISO-8859-1",response.getContentType());
-
-        response.recycle();
-    }
-
-
-    public void testLocale()
-        throws Exception
-    {
-
-        HttpConnection connection = new HttpConnection(connector,connector.endp,connector.server);
-        Request request = connection.getRequest();
-        Response response = connection.getResponse();
-        ContextHandler context = new ContextHandler();
-        context.addLocaleEncoding(Locale.ENGLISH.toString(),"ISO-8859-1");
-        context.addLocaleEncoding(Locale.ITALIAN.toString(),"ISO-8859-2");
-        request.setContext(context.getServletContext());
-
-        response.setLocale(java.util.Locale.ITALIAN);
-        assertEquals(null,response.getContentType());
-        response.setContentType("text/plain");
-        assertEquals("text/plain; charset=ISO-8859-2",response.getContentType());
-
-        response.recycle();
-        response.setContentType("text/plain");
-        response.setCharacterEncoding("utf-8");
-        response.setLocale(java.util.Locale.ITALIAN);
-        assertEquals("text/plain; charset=utf-8",response.getContentType());
-        assertTrue(response.toString().indexOf("charset=utf-8")>0);
-    }
-    
-    public void testContentTypeCharacterEncoding()
-        throws Exception
-    {
-        HttpConnection connection = new HttpConnection(connector,connector.endp,connector.server);
+        assertEquals("text/html; charset=\"utf8\"",results.get(i++));
+        assertEquals("utf8",results.get(i++));
         
-        Request request = connection.getRequest();
-        Response response = connection.getResponse();
+        assertTrue(((String)results.get(i++)).startsWith("text/html"));
+        assertEquals(" x=z; ",results.get(i++));
         
-        
-        response.setContentType("foo/bar");
-        response.setCharacterEncoding("utf-8");
-        assertEquals("foo/bar; charset=utf-8",response.getContentType());
-        response.getWriter();
-        assertEquals("foo/bar; charset=utf-8",response.getContentType());
-        response.setContentType("foo2/bar2");
-        assertEquals("foo2/bar2; charset=utf-8",response.getContentType());
-        response.setCharacterEncoding("ISO-8859-1");
-        assertEquals("foo2/bar2; charset=utf-8",response.getContentType());
-
-        response.recycle();
-        
-        response.setContentType("text/html");
-        response.setCharacterEncoding("utf-8");
-        assertEquals("text/html; charset=utf-8",response.getContentType());
-        response.getWriter();
-        assertEquals("text/html; charset=utf-8",response.getContentType());
-        response.setContentType("text/xml");
-        assertEquals("text/xml; charset=utf-8",response.getContentType());
-        response.setCharacterEncoding("ISO-8859-1");
-        assertEquals("text/xml; charset=utf-8",response.getContentType());
         
     }
     
-    public void testCharacterEncodingContentType()
-    throws Exception
+    interface RequestChecker
     {
-        Response response = new Response(new HttpConnection(connector,connector.endp,connector.server));
-        
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("foo/bar");
-        assertEquals("foo/bar; charset=utf-8",response.getContentType());
-        response.getWriter();
-        assertEquals("foo/bar; charset=utf-8",response.getContentType());
-        response.setContentType("foo2/bar2");
-        assertEquals("foo2/bar2; charset=utf-8",response.getContentType());
-        response.setCharacterEncoding("ISO-8859-1");
-        assertEquals("foo2/bar2; charset=utf-8",response.getContentType());
-        
-        response.recycle();
-        
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("text/html");
-        assertEquals("text/html; charset=utf-8",response.getContentType());
-        response.getWriter();
-        assertEquals("text/html; charset=utf-8",response.getContentType());
-        response.setContentType("text/xml");
-        assertEquals("text/xml; charset=utf-8",response.getContentType());
-        response.setCharacterEncoding("iso-8859-1");
-        assertEquals("text/xml; charset=utf-8",response.getContentType());
-        
-    }
-
-    public void testContentTypeWithCharacterEncoding()
-        throws Exception
-    {
-        Response response = new Response(new HttpConnection(connector,connector.endp,connector.server));
-        
-        response.setCharacterEncoding("utf16");
-        response.setContentType("foo/bar; charset=utf-8");
-        assertEquals("foo/bar; charset=utf-8",response.getContentType());
-        response.getWriter();
-        assertEquals("foo/bar; charset=utf-8",response.getContentType());
-        response.setContentType("foo2/bar2");
-        assertEquals("foo2/bar2; charset=utf-8",response.getContentType());
-        response.setCharacterEncoding("ISO-8859-1");
-        assertEquals("foo2/bar2; charset=utf-8",response.getContentType());
-
-        response.recycle();
-
-        response.setCharacterEncoding("utf16");
-        response.setContentType("text/html; charset=utf-8");
-        assertEquals("text/html; charset=utf-8",response.getContentType());
-        response.getWriter();
-        assertEquals("text/html; charset=utf-8",response.getContentType());
-        response.setContentType("text/xml");
-        assertEquals("text/xml; charset=utf-8",response.getContentType());
-        response.setCharacterEncoding("iso-8859-1");
-        assertEquals("text/xml; charset=utf-8",response.getContentType());
-        
+        boolean check(HttpServletRequest request);
     }
     
-    public void testContentTypeWithOther()
-    throws Exception
+    class RequestHandler extends AbstractHandler
     {
-        Response response = new Response(new HttpConnection(connector,connector.endp,connector.server));
+        RequestChecker _checker;
         
-        response.setContentType("foo/bar; other=xyz");
-        assertEquals("foo/bar; other=xyz",response.getContentType());
-        response.getWriter();
-        assertEquals("foo/bar; other=xyz charset=ISO-8859-1",response.getContentType());
-        response.setContentType("foo2/bar2");
-        assertEquals("foo2/bar2; charset=ISO-8859-1",response.getContentType());
-        
-        response.recycle();
-        
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("text/html; other=xyz");
-        assertEquals("text/html; other=xyz charset=utf-8",response.getContentType());
-        response.getWriter();
-        assertEquals("text/html; other=xyz charset=utf-8",response.getContentType());
-        response.setContentType("text/xml");
-        assertEquals("text/xml; charset=utf-8",response.getContentType());       
-    }
-    
-
-    public void testContentTypeWithCharacterEncodingAndOther()
-        throws Exception
-    {
-        Response response = new Response(new HttpConnection(connector,connector.endp,connector.server));
-
-        response.setCharacterEncoding("utf16");
-        response.setContentType("foo/bar; charset=utf-8 other=xyz");
-        assertEquals("foo/bar; charset=utf-8 other=xyz",response.getContentType());
-        response.getWriter();
-        assertEquals("foo/bar; charset=utf-8 other=xyz",response.getContentType());
-
-        response.recycle();
-
-        response.setCharacterEncoding("utf16");
-        response.setContentType("text/html; other=xyz charset=utf-8");
-        assertEquals("text/html; other=xyz charset=utf-8",response.getContentType());
-        response.getWriter();
-        assertEquals("text/html; other=xyz charset=utf-8",response.getContentType());
-
-        response.recycle();
-        
-        response.setCharacterEncoding("utf16");
-        response.setContentType("foo/bar; other=pq charset=utf-8 other=xyz");
-        assertEquals("foo/bar; other=pq charset=utf-8 other=xyz",response.getContentType());
-        response.getWriter();
-        assertEquals("foo/bar; other=pq charset=utf-8 other=xyz",response.getContentType());
-
-    }
-    
-    public void testStatusCodes() throws Exception
-    {
-        Response response=newResponse();
-
-        response.sendError(404);
-        assertEquals(404, response.getStatus());
-        assertEquals(null, response.getReason());
-        
-        response=newResponse();
-        
-        response.sendError(500, "Database Error");
-        assertEquals(500, response.getStatus());
-        assertEquals("Database Error", response.getReason());
-
-        response=newResponse();
-        
-        response.setStatus(200);
-        assertEquals(200, response.getStatus());
-        assertEquals(null, response.getReason());
-        
-        response=newResponse();
-        
-        response.sendError(406, "Super Nanny");
-        assertEquals(406, response.getStatus());
-        assertEquals("Super Nanny", response.getReason());
-    }
-    
-    public void testEncodeRedirect()
-        throws Exception
-    {
-        HttpConnection connection=new HttpConnection(connector,connector.endp,connector.server);
-        Response response = new Response(connection);
-        Request request = connection.getRequest();
-        
-        assertEquals("http://host:port/path/info;param?query=0&more=1#target",response.encodeRedirectUrl("http://host:port/path/info;param?query=0&more=1#target"));
-       
-        request.setRequestedSessionId("12345");
-        request.setRequestedSessionIdFromCookie(false);
-        AbstractSessionManager manager=new HashSessionManager();
-        manager.setIdManager(new HashSessionIdManager());
-        request.setSessionManager(manager);
-        request.setSession(new TestSession(manager,"12345"));
-        
-        assertEquals("http://host:port/path/info;param;jsessionid=12345?query=0&more=1#target",response.encodeRedirectUrl("http://host:port/path/info;param?query=0&more=1#target"));
-              
+        public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch) throws IOException, ServletException
+        {
+            ((Request)request).setHandled(true);
+            if (_checker!=null && _checker.check(request))
+                response.setStatus(200);
+            else
+                response.sendError(500);   
+        }   
     }
 
-    public void testSetBufferSize ()
-    throws Exception
-    {
-        Response response = new Response(new HttpConnection(connector,connector.endp,connector.server));
-        response.setBufferSize(20*1024);
-        response.getWriter().print("hello");
-        try
-        {
-            response.setBufferSize(21*1024);
-            fail("Expected IllegalStateException on Request.setBufferSize");
-        }
-        catch (Exception e)
-        {
-            assertTrue(e instanceof IllegalStateException);
-        }
-    }
-    
-    private Response newResponse()
-    {
-        HttpConnection connection=new HttpConnection(connector,connector.endp,connector.server);
-        connection.getGenerator().reset(false);
-        HttpConnection.setCurrentConnection(connection);
-        Response response = connection.getResponse();
-        connection.getRequest().setRequestURI("/test");
-        return response;
-    }
-    
-    class TestSession extends AbstractSessionManager.Session
-    {
-        public TestSession(AbstractSessionManager abstractSessionManager, String id)
-        {
-            abstractSessionManager.super(System.currentTimeMillis(), id);
-        }
-
-        public Object getAttribute(String name) 
-        {
-            return null;
-        }
-
-        public Enumeration getAttributeNames()
-        {
-
-            return null;
-        }
-
-        public long getCreationTime()
-        {
-
-            return 0;
-        }
-
-        public String getId()
-        {
-            return "12345";
-        }
-
-        public long getLastAccessedTime()
-        {
-            return 0;
-        }
-
-        public int getMaxInactiveInterval()
-        {
-            return 0;
-        }
-
-        public ServletContext getServletContext()
-        {
-            return null;
-        }
-
-        public HttpSessionContext getSessionContext()
-        {
-            return null;
-        }
-
-        public Object getValue(String name)
-        {
-            return null;
-        }
-
-        public String[] getValueNames()
-        {
-            return null;
-        }
-
-        public void invalidate()
-        {
-        }
-
-        public boolean isNew()
-        {
-            return false;
-        }
-
-        public void putValue(String name, Object value)
-        {
-        }
-
-        public void removeAttribute(String name)
-        {
-        }
-
-        public void removeValue(String name)
-        {   
-        }
-
-        public void setAttribute(String name, Object value)
-        {
-        }
-
-        public void setMaxInactiveInterval(int interval)
-        {
-        }
-
-        protected Map newAttributeMap()
-        {
-            // TODO Auto-generated method stub
-            return null;
-        }
-    }
 }
