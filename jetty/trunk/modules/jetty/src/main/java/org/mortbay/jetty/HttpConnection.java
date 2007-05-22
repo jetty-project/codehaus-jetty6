810897a6e088dc98fe0946931ca4a344a665e31e
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2007-05-22 07:41:04 +0000

    fixed the fix for jsp closed

--- jetty/trunk/modules/jetty/src/main/java/org/mortbay/jetty/HttpConnection.java
+++ jetty/trunk/modules/jetty/src/main/java/org/mortbay/jetty/HttpConnection.java
@@ -320,7 +320,7 @@ public class HttpConnection implements Connection
                 {
                     try
                     {
-                        _out.close();
+                        out.close();
                     }
                     catch(IOException e)
                     {
