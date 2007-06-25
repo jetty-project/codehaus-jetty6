08fee74268ba89108757f5aa8f6fd3220b620e5c
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2007-06-25 02:33:48 +0000

    option to only open connectors before setuid

--- jetty/trunk/extras/setuid/src/main/java/org/mortbay/setuid/SetUIDServer.java
+++ jetty/trunk/extras/setuid/src/main/java/org/mortbay/setuid/SetUIDServer.java
@@ -1,17 +1,24 @@
 package org.mortbay.setuid;
 
+import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
 import org.mortbay.log.Log;
 
 /**
- * This extension of {@link Server} will make a JNI call to set the unix UID
- * after the server has been started.
- * This can be used to start the server as root so that priviledged ports may
- * be accessed and then switch to a non-root user for security.
+ * This extension of {@link Server} will make a JNI call to set the unix UID.
  *
+ * This can be used to start the server as root so that privileged ports may
+ * be accessed and then switch to a non-root user for security.
+ * Depending on the value of {@link #setStartServerAsPrivileged(boolean)}, either the
+ * server will be started and then the UID set; or the {@link Server#getConnectors()} will be 
+ * opened with a call to {@link Connector#open()}, the UID set and then the server is started.
+ * The later is the default and avoids any webapplication code being run as a privileged user,
+ * but will not work if the application code also needs to open privileged ports.
+ *
+ *<p>
  * The configured umask is set before the server is started and the configured
  * uid is set after the server is started.
- * 
+ * </p>
  * @author gregw
  *
  */
@@ -19,6 +26,7 @@ public class SetUIDServer extends Server
 {
     int _uid=0;
     int _umask=0;
+    boolean _startServerAsPrivileged;
 
 
     public int getUmask ()
@@ -49,12 +57,47 @@ public class SetUIDServer extends Server
             Log.info("Setting umask=0"+Integer.toString(_umask,8));
             SetUID.setumask(_umask);
         }
-        super.doStart();
-        if (_uid!=0)
+        
+        if (_startServerAsPrivileged)
         {
-            Log.info("Setting UID="+_uid);
-            SetUID.setuid(_uid);
+            super.doStart();
+            if (_uid!=0)
+            {
+                Log.info("Setting UID="+_uid);
+                SetUID.setuid(_uid);
+            }
         }
+        else
+        {
+            Connector[] connectors = getConnectors();
+            for (int i=0;connectors!=null && i<connectors.length;i++)
+                connectors[i].open();
+            if (_uid!=0)
+            {
+                Log.info("Setting UID="+_uid);
+                SetUID.setuid(_uid);
+            }
+            super.doStart();
+        }
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * @return the startServerAsPrivileged 
+     */
+    public boolean isStartServerAsPrivileged()
+    {
+        return _startServerAsPrivileged;
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * @see {@link Connector#open()}
+     * @param startServerAsPrivileged if true, the server is started and then the process UID is switched. If false, the connectors are opened, the UID is switched and then the server is started.
+     */
+    public void setStartServerAsPrivileged(boolean startContextsAsPrivileged)
+    {
+        _startServerAsPrivileged=startContextsAsPrivileged;
     }
     
 }
