191069da23b05b8a44da967c4c79e73cf59395c8
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2007-08-09 00:12:44 +0000

    utf-8 for bayeux client

--- trunk/modules/jetty/src/main/java/org/mortbay/io/ByteArrayBuffer.java
+++ trunk/modules/jetty/src/main/java/org/mortbay/io/ByteArrayBuffer.java
@@ -17,6 +17,7 @@ package org.mortbay.io;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 
 /* ------------------------------------------------------------------------------- */
 /**
@@ -64,6 +65,16 @@ public class ByteArrayBuffer extends AbstractBuffer
     {
         super(READWRITE,NON_VOLATILE);
         _bytes = Portable.getBytes(value);
+        setGetIndex(0);
+        setPutIndex(_bytes.length);
+        _access=IMMUTABLE;
+        _string = value;
+    }
+
+    public ByteArrayBuffer(String value,String encoding) throws UnsupportedEncodingException
+    {
+        super(READWRITE,NON_VOLATILE);
+        _bytes = value.getBytes(encoding);
         setGetIndex(0);
         setPutIndex(_bytes.length);
         _access=IMMUTABLE;
