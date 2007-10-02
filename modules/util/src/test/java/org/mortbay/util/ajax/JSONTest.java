bda3955d0209663a4a5b0a2a5b124a2d8285ecbb
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2007-10-02 08:57:38 +0000

    JSON convertable

--- modules/util/src/test/java/org/mortbay/util/ajax/JSONTest.java
+++ modules/util/src/test/java/org/mortbay/util/ajax/JSONTest.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.mortbay.util.ajax.JSON;
+import org.mortbay.util.ajax.JSON.Output;
 
 import junit.framework.TestCase;
 
@@ -15,6 +16,17 @@ public class JSONTest extends TestCase
         HashMap obj6 = new HashMap();
         HashMap obj7 = new HashMap();
         
+        Woggle w0 = new Woggle();
+        Woggle w1 = new Woggle();
+        
+        w0.name="woggle0";
+        w0.nested=w1;
+        w0.number=100;
+        w1.name="woggle1";
+        w1.nested=null;
+        w1.number=101;
+       
+        
         map.put("n1",null);
         map.put("n2",new Integer(2));
         map.put("n3",new Double(-0.00000000003));
@@ -24,11 +36,12 @@ public class JSONTest extends TestCase
         map.put("n7",obj7);
         map.put("n8",new int[]{1,2,3,4});
         map.put("n9",new JSON.Literal("[{},  [],  {}]"));
+        map.put("w0",w0);
         
         obj7.put("x","value");
         
         String s = JSON.toString(map);
-        // System.err.println(s);
+        System.err.println(s);
         assertTrue(s.indexOf("\"n1\":null")>=0);
         assertTrue(s.indexOf("\"n2\":2")>=0);
         assertTrue(s.indexOf("\"n3\":-3.0E-11")>=0);
@@ -38,7 +51,7 @@ public class JSONTest extends TestCase
         assertTrue(s.indexOf("\"n7\":{\"x\":\"value\"}")>=0);
         assertTrue(s.indexOf("\"n8\":[1,2,3,4]")>=0);
         assertTrue(s.indexOf("\"n9\":[{},  [],  {}]")>=0);
-        
+        assertTrue(s.indexOf("\"w0\":{\"class\":\"org.mortbay.util.ajax.JSONTest$Woggle\",\"name\":\"woggle0\",\"nested\":{\"class\":\"org.mortbay.util.ajax.JSONTest$Woggle\",\"name\":\"woggle1\",\"nested\":null,\"number\":101},\"number\":100}")>=0);
     }
     
     public void testParse()
@@ -54,13 +67,16 @@ public class JSONTest extends TestCase
         "\"empty\" : {}  ," +
         "\"map\" : {\"a\":-1.0e2}  ," +
         "\"array\" : [\"a\",-1.0e2,[],null,true,false]  ," +
+        "\"w0\":{\"class\":\"org.mortbay.util.ajax.JSONTest$Woggle\",\"name\":\"woggle0\",\"nested\":{\"class\":\"org.mortbay.util.ajax.JSONTest$Woggle\",\"name\":\"woggle1\",\"nested\":null,\"number\":101},\"number\":100}" +
         "}";
         
         Map map = (Map)JSON.parse(test);
-        // System.err.println(JSON.toString(map));
+        System.err.println(map);
         assertEquals(new Long(100),map.get("onehundred"));
         assertEquals("fred",map.get("name"));
         assertTrue(map.get("array").getClass().isArray());
+        assertTrue(map.get("w0") instanceof Woggle);
+        assertTrue(((Woggle)map.get("w0")).nested instanceof Woggle);
         
         test="{\"data\":{\"source\":\"15831407eqdaawf7\",\"widgetId\":\"Magnet_8\"},\"channel\":\"/magnets/moveStart\",\"connectionId\":null,\"clientId\":\"15831407eqdaawf7\"}";
         map = (Map)JSON.parse(test);
@@ -85,6 +101,39 @@ public class JSONTest extends TestCase
         o = JSON.parse(test,true);
         assertTrue(o instanceof Map);
         assertEquals("fred",((Map)o).get("name"));
+        
+    }
+    
+    
+    public static class Woggle implements JSON.Convertable
+    {
+        String name;
+        Woggle nested;
+        int number;
+        
+        public Woggle()
+        {
+        }
+        
+        public void fromJSON(Map object)
+        {
+            name=(String)object.get("name");
+            nested=(Woggle)object.get("nested");
+            number=((Number)object.get("number")).intValue();
+        }
+
+        public void toJSON(Output out)
+        {
+            out.addClass(Woggle.class);
+            out.add("name",name);
+            out.add("nested",nested);
+            out.add("number",number);
+        }
+        
+        public String toString()
+        {
+            return name+"<<"+nested+">>"+number;
+        }
         
     }
 }
