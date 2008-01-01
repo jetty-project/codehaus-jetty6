01aec963de23e9ef742b198909cb6c28f59c62e1
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-01-01 08:01:47 +0000

    increase default size

--- modules/jetty/src/main/java/org/mortbay/jetty/AbstractBuffers.java
+++ modules/jetty/src/main/java/org/mortbay/jetty/AbstractBuffers.java
@@ -18,7 +18,7 @@ public abstract class AbstractBuffers extends AbstractLifeCycle implements Buffe
     
     private int _headerBufferSize=4*1024;
     private int _requestBufferSize=8*1024;
-    private int _responseBufferSize=24*1024;
+    private int _responseBufferSize=32*1024;
 
     // Use and array of buffers to avoid contention
     private transient ArrayList _headerBuffers=new ArrayList();
