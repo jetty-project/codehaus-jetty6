e2a08726334f93431205c56f303f759e6c248330
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-02-03 15:43:29 +0000

    JETTY-399 Upgrade to gwt 1.4

--- extras/gwt/src/main/java/org/mortbay/gwt/AsyncRemoteServiceServlet.java
+++ extras/gwt/src/main/java/org/mortbay/gwt/AsyncRemoteServiceServlet.java
@@ -1,59 +1,75 @@
+/*
+ * Copyright 2006 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.mortbay.gwt;
 
-import java.io.IOException;
-import java.lang.reflect.Method;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.google.gwt.user.server.rpc.OpenRemoteServiceServlet;
+import com.google.gwt.user.server.rpc.UnexpectedException;
 
-public class AsyncRemoteServiceServlet extends OpenRemoteServiceServlet
-{
-    public static final String PAYLOAD= "com.google.gwt.payload";
-    
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * Jetty Continuation support for GWT RPC.
+ * 
+ * @author Craig Day (craig@alderaan.com.au)
+ */
+public class AsyncRemoteServiceServlet extends OpenRemoteServiceServlet {
+
+    public static final String PAYLOAD = "com.google.gwt.payload";
+
+    private static final String JETTY_RETRY_REQUEST_EXCEPTION = "org.mortbay.jetty.RetryRequest";
+
     /* ------------------------------------------------------------ */
     /* (non-Javadoc)
      * @see com.google.gwt.user.server.rpc.OpenRemoteServiceServlet#readPayloadAsUtf8(javax.servlet.http.HttpServletRequest)
      */
-    protected String readPayloadAsUtf8(HttpServletRequest request) throws IOException, ServletException
-    {
-        String payload=(String)request.getAttribute(PAYLOAD);
-        if (payload==null)
-        {
-            payload=super.readPayloadAsUtf8(request);
-            request.setAttribute(PAYLOAD,payload);
+    protected String readPayloadAsUtf8(HttpServletRequest request) throws IOException, ServletException {
+        String payload = (String) request.getAttribute(PAYLOAD);
+        if (payload == null) {
+            payload = super.readPayloadAsUtf8(request);
+            request.setAttribute(PAYLOAD, payload);
         }
         return payload;
     }
 
-    /* ------------------------------------------------------------ */
-    /* (non-Javadoc)
-     * @see com.google.gwt.user.server.rpc.OpenRemoteServiceServlet#respondWithFailure(javax.servlet.http.HttpServletResponse, java.lang.Throwable)
+
+    /**
+     * Overridden to really throw Jetty RetryRequest Exception (as opposed to sending failure to client).
+     *
+     * @param caught the exception
      */
-    protected void respondWithFailure(HttpServletResponse response, Throwable caught)
-    {
-    	throwIfRetyRequest(caught);        
-        super.respondWithFailure(response,caught);
+    protected void doUnexpectedFailure(Throwable caught) {
+        throwIfRetryRequest(caught);
+        super.doUnexpectedFailure(caught);
     }
-    
-    protected void handleException( String responsePayload, Throwable caught )
-    {
-    	throwIfRetyRequest(caught);     
-    	super.handleException( responsePayload, caught );
-    }
-    
-    protected void throwIfRetyRequest( Throwable caught )
-    {
-        if (caught instanceof RuntimeException && "org.mortbay.jetty.RetryRequest".equals(caught.getClass().getName()))
-        {
-            throw (RuntimeException) caught;
+
+    /**
+     * Throws the Jetty RetryRequest if found.
+     *
+     * @param caught the exception
+     */
+    protected void throwIfRetryRequest(Throwable caught) {
+        if (caught instanceof UnexpectedException) {
+            caught = caught.getCause();
+        }
+        if (caught instanceof ThreadDeath && JETTY_RETRY_REQUEST_EXCEPTION.equals(caught.getClass().getName())) {
+            throw (Error) caught;
         }
     }
 
 
-
 }
-
