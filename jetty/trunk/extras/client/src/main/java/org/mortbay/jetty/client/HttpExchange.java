486592ff4608ceb8d6eaec88a894e5e394f7ab5c
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-02-12 22:51:57 +0000

    better javadoc

--- jetty/trunk/extras/client/src/main/java/org/mortbay/jetty/client/HttpExchange.java
+++ jetty/trunk/extras/client/src/main/java/org/mortbay/jetty/client/HttpExchange.java
@@ -143,7 +143,7 @@ public class HttpExchange
     
     /* ------------------------------------------------------------ */
     /**
-     * @param url
+     * @param url Including protocol, host and port
      */
     public void setURL(String url)
     {
@@ -450,6 +450,12 @@ public class HttpExchange
     /* ------------------------------------------------------------ */
     /* ------------------------------------------------------------ */
     /* ------------------------------------------------------------ */
+    /**
+     * An exchange that caches response status and fields for later use.
+     * 
+     * @author gregw
+     *
+     */
     public static class CachedExchange extends HttpExchange
     {
         int _responseStatus;
@@ -495,6 +501,10 @@ public class HttpExchange
     /* ------------------------------------------------------------ */
     /* ------------------------------------------------------------ */
     /* ------------------------------------------------------------ */
+    /**
+     * A CachedExchange that retains all content for later use.
+     *
+     */
     public static class ContentExchange extends CachedExchange
     {
         int _contentLength=1024;
