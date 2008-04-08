19421edfd50fe22e06b6e00fbb8e8af172f4bc55
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-04-08 04:44:24 +0000

    JETTY-546 added classpath support

--- modules/server/jetty-util/src/main/java/org/mortbay/log/Log.java
+++ modules/server/jetty-util/src/main/java/org/mortbay/log/Log.java
@@ -40,68 +40,80 @@ public class Log
     public final static String IGNORED_FMT= "IGNORED: {}";
     public final static String NOT_IMPLEMENTED= "NOT IMPLEMENTED ";
     
-    private static String logClass=System.getProperty("org.mortbay.log.class","org.mortbay.log.Slf4jLog");
-    private static boolean verbose = System.getProperty("VERBOSE",null)!=null;
-    private static boolean ignored = System.getProperty("IGNORED",null)!=null;
-    private static Logger log;
+    private static String __logClass=System.getProperty("org.mortbay.log.class","org.mortbay.log.Slf4jLog");
+    private static boolean __verbose = System.getProperty("VERBOSE",null)!=null;
+    private static boolean __ignored = System.getProperty("IGNORED",null)!=null;
+    private static Logger __log;
    
     static
+    {
+        initialize();
+    }
+    
+    public static void initialize()
     {
         Class log_class=null;
         try
         {
-            log_class=Loader.loadClass(Log.class, logClass);
-            log=(Logger) log_class.newInstance();
+            log_class=Loader.loadClass(Log.class, __logClass);
+            if (__log==null || !__log.getClass().equals(log_class))
+            {
+                __log=(Logger) log_class.newInstance();
+                __log.info("Logging to {} via {}",__log,log_class.getName());
+            }
         }
         catch(Exception e)
         {
-            log_class=StdErrLog.class;
-            log=new StdErrLog();
-            if(verbose)
-                e.printStackTrace();
+            if (__log==null)
+            {
+                log_class=StdErrLog.class;
+                __log=new StdErrLog();
+                __log.info("Logging to {} via {}",__log,log_class.getName());
+                if(__verbose)
+                    e.printStackTrace();
+            }
         }
         
-        log.info("Logging to {} via {}",log,log_class.getName());
     }
     
     public static void setLog(Logger log)
     {
-        Log.log=log;
+        Log.__log=log;
     }
     
     public static Logger getLog()
     {
-        return log;
+        return __log;
     }
     
     
     public static void debug(Throwable th)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.debug(EXCEPTION,th);
+        __log.debug(EXCEPTION,th);
         unwind(th);
     }
 
     public static void debug(String msg)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.debug(msg,null,null);
+        __log.debug(msg,null,null);
     }
     
     public static void debug(String msg,Object arg)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.debug(msg,arg,null);
+        __log.debug(msg,arg,null);
     }
     
     public static void debug(String msg,Object arg0, Object arg1)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.debug(msg,arg0,arg1);
+        __log.debug(msg,arg0,arg1);
     }
     
     /* ------------------------------------------------------------ */
@@ -111,82 +123,82 @@ public class Log
      */
     public static void ignore(Throwable th)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        if (ignored)
+        if (__ignored)
         {
-            log.warn(IGNORED,th);
+            __log.warn(IGNORED,th);
             unwind(th);
         }
-        if (verbose)
+        if (__verbose)
         {
-            log.warn(IGNORED,th);
+            __log.warn(IGNORED,th);
             unwind(th);
         }
     }
     
     public static void info(String msg)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.info(msg,null,null);
+        __log.info(msg,null,null);
     }
     
     public static void info(String msg,Object arg)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.info(msg,arg,null);
+        __log.info(msg,arg,null);
     }
     
     public static void info(String msg,Object arg0, Object arg1)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.info(msg,arg0,arg1);
+        __log.info(msg,arg0,arg1);
     }
     
     public static boolean isDebugEnabled()
     {
-        if (log==null)
+        if (__log==null)
             return false;
-        return log.isDebugEnabled();
+        return __log.isDebugEnabled();
     }
     
     public static void warn(String msg)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.warn(msg,null,null);
+        __log.warn(msg,null,null);
     }
     
     public static void warn(String msg,Object arg)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.warn(msg,arg,null);        
+        __log.warn(msg,arg,null);        
     }
     
     public static void warn(String msg,Object arg0, Object arg1)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.warn(msg,arg0,arg1);        
+        __log.warn(msg,arg0,arg1);        
     }
     
     public static void warn(String msg, Throwable th)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.warn(msg,th);
+        __log.warn(msg,th);
         unwind(th);
     }
 
     public static void warn(Throwable th)
     {
-        if (log==null)
+        if (__log==null)
             return;
-        log.warn(EXCEPTION,th);
+        __log.warn(EXCEPTION,th);
         unwind(th);
     }
 
@@ -195,11 +207,11 @@ public class Log
      */
     public static Logger getLogger(String name)
     {
-        if (log==null)
-            return log;
+        if (__log==null)
+            return __log;
         if (name==null)
-          return log;
-        return log.getLogger(name);
+          return __log;
+        return __log.getLogger(name);
     }
 
     private static void unwind(Throwable th)
