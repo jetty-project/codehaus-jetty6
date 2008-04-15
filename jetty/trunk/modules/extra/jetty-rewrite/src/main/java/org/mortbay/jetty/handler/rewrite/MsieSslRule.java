b21d46fc569e63af1a97561e97c4ca64d52b5413
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-04-15 23:54:48 +0000

    better check for bad IE

--- jetty/trunk/modules/extra/jetty-rewrite/src/main/java/org/mortbay/jetty/handler/rewrite/MsieSslRule.java
+++ jetty/trunk/modules/extra/jetty-rewrite/src/main/java/org/mortbay/jetty/handler/rewrite/MsieSslRule.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.mortbay.jetty.HttpHeaderValues;
 import org.mortbay.jetty.HttpHeaders;
+import org.mortbay.util.StringMap;
 
 /**
  * MSIE SSL Rule
@@ -17,12 +18,21 @@ import org.mortbay.jetty.HttpHeaders;
  */
 public class MsieSslRule extends Rule
 {
-    private static final int IEv5 = 53;
-    private static final int IEv6 = 54;
+    private static final int IEv5 = '5';
+    private static final int IEv6 = '6';
+    private static StringMap __IE6_BadOS = new StringMap();
+    {
+        __IE6_BadOS.put("NT 5.0",Boolean.TRUE);
+        __IE6_BadOS.put("NT 4.0",Boolean.TRUE);
+        __IE6_BadOS.put("98",Boolean.TRUE);
+        __IE6_BadOS.put("98; Win 9x 4.90",Boolean.TRUE);
+        __IE6_BadOS.put("95",Boolean.TRUE);
+        __IE6_BadOS.put("CE",Boolean.TRUE);
+    }
     
     public MsieSslRule()
     {
-        _handling = true;
+        _handling = false;
         _terminating = false;
     }
     
@@ -40,13 +50,24 @@ public class MsieSslRule extends Rule
                     // Get Internet Explorer Version
                     int ieVersion = user_agent.charAt(msie+5);
                     
-                    // Check if OS is Windows 2000. 
-                    boolean isWin2k = user_agent.indexOf("Windows NT 5",msie)>0;
-                    
-                    if ( ieVersion<=IEv5 || (ieVersion==IEv6 && isWin2k))
+                    if ( ieVersion<=IEv5)
                     {
                         response.setHeader(HttpHeaders.CONNECTION, HttpHeaderValues.CLOSE);
                         return target;
+                    }
+
+                    if (ieVersion==IEv6)
+                    {
+                        int windows = user_agent.indexOf("Windows",msie+5);
+                        if (windows>0)
+                        {
+                            int end=user_agent.indexOf(')',windows+8);
+                            if(end<0 || __IE6_BadOS.getEntry(user_agent,windows+8,end-windows-8)!=null)
+                            {
+                                response.setHeader(HttpHeaders.CONNECTION, HttpHeaderValues.CLOSE);
+                                return target;
+                            }
+                        }
                     }
                 }
             }
