c42cae715bbbf594ab50a9eea9f81f073601f1ef
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-05-29 09:07:02 +0000

    JETTY-587

---
+++ modules/server/jetty/src/main/java/org/mortbay/jetty/servlet/AbstractSessionIdManager.java
@@ -0,0 +1,141 @@
+package org.mortbay.jetty.servlet;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.mortbay.component.AbstractLifeCycle;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.SessionIdManager;
+import org.mortbay.log.Log;
+
+public abstract class AbstractSessionIdManager extends AbstractLifeCycle implements SessionIdManager
+{
+    private final static String __NEW_SESSION_ID="org.mortbay.jetty.newSessionId";  
+    protected final static String SESSION_ID_RANDOM_ALGORITHM = "SHA1PRNG";
+    protected final static String SESSION_ID_RANDOM_ALGORITHM_ALT = "IBMSecureRandom";
+    
+    protected Random _random;
+    protected boolean _weakRandom;
+    protected String _workerName;
+    protected Server _server;
+    
+    
+    public AbstractSessionIdManager(Server server)
+    {
+        _server=server;
+    }
+    
+    
+    public AbstractSessionIdManager(Server server, Random random)
+    {
+        _random=random;
+        _server=server;
+    }
+
+    public String getWorkerName()
+    {
+        return _workerName;
+    }
+    
+    public void setWorkerName (String name)
+    {
+        _workerName=name;
+    }
+
+    /* ------------------------------------------------------------ */
+    public Random getRandom()
+    {
+        return _random;
+    }
+
+    /* ------------------------------------------------------------ */
+    public void setRandom(Random random)
+    {
+        _random=random;
+        _weakRandom=false;
+    }
+    /** 
+     * Create a new session id if necessary.
+     * 
+     * @see org.mortbay.jetty.SessionIdManager#newSessionId(javax.servlet.http.HttpServletRequest, long)
+     */
+    public String newSessionId(HttpServletRequest request, long created)
+    {
+        synchronized (this)
+        {
+            // A requested session ID can only be used if it is in use already.
+            String requested_id=request.getRequestedSessionId();
+            if (requested_id!=null&&idInUse(requested_id))
+                return requested_id;
+
+            // Else reuse any new session ID already defined for this request.
+            String new_id=(String)request.getAttribute(__NEW_SESSION_ID);
+            if (new_id!=null&&idInUse(new_id))
+                return new_id;
+
+            
+            
+            // pick a new unique ID!
+            String id=null;
+            while (id==null||id.length()==0||idInUse(id))
+            {
+                long r=_weakRandom
+                ?(hashCode()^Runtime.getRuntime().freeMemory()^_random.nextInt()^(((long)request.hashCode())<<32))
+                :_random.nextLong();
+                r^=created;
+                if (request!=null && request.getRemoteAddr()!=null)
+                    r^=request.getRemoteAddr().hashCode();
+                if (r<0)
+                    r=-r;
+                id=Long.toString(r,36);
+            }
+
+            request.setAttribute(__NEW_SESSION_ID,id);
+            return id;
+        }
+    }
+
+    
+    public void doStart()
+    {
+       initRandom();
+    }
+
+    
+    
+    
+    /**
+     * Set up a random number generator for the sessionids.
+     * 
+     * By preference, use a SecureRandom but allow to be injected.
+     */
+    public void initRandom ()
+    {
+        if (_random==null)
+        {
+            try
+            {
+                _random=SecureRandom.getInstance(SESSION_ID_RANDOM_ALGORITHM);
+            }
+            catch (NoSuchAlgorithmException e)
+            {
+                try
+                {
+                    _random=SecureRandom.getInstance(SESSION_ID_RANDOM_ALGORITHM_ALT);
+                    _weakRandom=false;
+                }
+                catch (NoSuchAlgorithmException e_alt)
+                {
+                    Log.warn("Could not generate SecureRandom for session-id randomness",e);
+                    _random=new Random();
+                    _weakRandom=true;
+                }
+            }
+        }
+        _random.setSeed(_random.nextLong()^System.currentTimeMillis()^hashCode()^Runtime.getRuntime().freeMemory()); 
+    }
+}
---
+++ modules/server/jetty/src/main/java/org/mortbay/jetty/servlet/JDBCSessionIdManager.java
@@ -0,0 +1,316 @@
+package org.mortbay.jetty.servlet;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.servlet.JDBCSessionManager.Session;
+import org.mortbay.jetty.webapp.WebAppContext;
+import org.mortbay.log.Log;
+
+
+/**
+ * JDBCSessionIdManager
+ *
+ * SessionIdManager implementation that uses a database to store in-use session ids, 
+ * to support distributed sessions.
+ * 
+ */
+public class JDBCSessionIdManager extends AbstractSessionIdManager
+{    
+    private static final String __createString = "create table JettySessionIds (id varchar(60), primary key(id))";
+    private static final String __insertString = "insert into JettySessionIds (id)  values (?)";
+    private static final String __deleteString = "delete from JettySessionIds where id = ?";
+    private static final String __queryString = "select * from JettySessionIds where id = ?";
+    protected HashSet<String> _sessionIds = new HashSet();
+    protected String _driverClassName;
+    protected String _connectionUrl;
+
+    public JDBCSessionIdManager(Server server)
+    {
+        super(server);
+    }
+    
+    public JDBCSessionIdManager(Server server, Random random)
+    {
+       super(server, random);
+    }
+
+    /**
+     * Configure jdbc connection information
+     * 
+     * @param driverClassName
+     * @param connectionUrl
+     */
+    public void setDriverInfo (String driverClassName, String connectionUrl)
+    {
+        _driverClassName=driverClassName;
+        _connectionUrl=connectionUrl;
+    }
+    
+    public String getDriverClassName()
+    {
+        return _driverClassName;
+    }
+    
+    public String getConnectionUrl ()
+    {
+        return _connectionUrl;
+    }
+   
+    public void addSession(HttpSession session)
+    {
+        if (session == null)
+            return;
+        
+        synchronized (_sessionIds)
+        {
+            String id = ((JDBCSessionManager.Session)session).getClusterId();            
+            try
+            {
+                insert(id);
+                _sessionIds.add(id);
+            }
+            catch (Exception e)
+            {
+                Log.warn("Problem storing session id="+id, e);
+            }
+        }
+    }
+    
+    public void removeSession(HttpSession session)
+    {
+        if (session == null)
+            return;
+        
+        removeSession(((JDBCSessionManager.Session)session).getClusterId());
+    }
+    
+    
+    
+    public void removeSession (String id)
+    {
+
+        if (id == null)
+            return;
+        
+        synchronized (_sessionIds)
+        {  
+            if (Log.isDebugEnabled())
+                Log.debug("Removing session id="+id);
+            try
+            {               
+                _sessionIds.remove(id);
+                delete(id);
+            }
+            catch (Exception e)
+            {
+                Log.warn("Problem removing session id="+id, e);
+            }
+        }
+        
+    }
+    
+
+    /** 
+     * Get the session id without any node identifier suffix.
+     * 
+     * @see org.mortbay.jetty.SessionIdManager#getClusterId(java.lang.String)
+     */
+    public String getClusterId(String nodeId)
+    {
+        int dot=nodeId.lastIndexOf('.');
+        return (dot>0)?nodeId.substring(0,dot):nodeId;
+    }
+    
+
+    /** 
+     * Get the session id, including this node's id as a suffix.
+     * 
+     * @see org.mortbay.jetty.SessionIdManager#getNodeId(java.lang.String, javax.servlet.http.HttpServletRequest)
+     */
+    public String getNodeId(String clusterId, HttpServletRequest request)
+    {
+        if (_workerName!=null)
+            return clusterId+'.'+_workerName;
+
+        return clusterId;
+    }
+
+
+    public boolean idInUse(String id)
+    {
+        if (id == null)
+            return false;
+        
+        synchronized (_sessionIds)
+        {
+            if (_sessionIds.contains(id))
+                return true; //optimisation - if this session is one we've been managing, we can check locally
+
+            //otherwise, we need to go to the database to check
+            try
+            {
+                return exists(id);
+            }
+            catch (Exception e)
+            {
+                Log.warn("Problem checking inUse for id="+id, e);
+                return false;
+            }
+        }
+    }
+
+    /** 
+     * Invalidate the session matching the id on all contexts.
+     * 
+     * @see org.mortbay.jetty.SessionIdManager#invalidateAll(java.lang.String)
+     */
+    public void invalidateAll(String id)
+    {            
+        //take the id out of the list of known sessionids for this node
+        removeSession(id);
+        
+        synchronized (_sessionIds)
+        {
+            //tell all contexts that may have a session object with this id to
+            //get rid of them
+            Handler[] contexts = _server.getChildHandlersByClass(WebAppContext.class);
+            for (int i=0; contexts!=null && i<contexts.length; i++)
+            {
+                AbstractSessionManager.Session session = ((AbstractSessionManager)((WebAppContext)contexts[i]).getSessionHandler().getSessionManager()).getSession(id);
+                if (session !=null)
+                    session.invalidate();
+            }
+        }
+    }
+
+
+    public void doStart()
+    {
+        try
+        {
+            prepareTables();        
+            super.doStart();
+        }
+        catch (Exception e)
+        {
+            Log.warn("Problem initialising JettySessionIds table", e);
+        }
+    }
+    /**
+     * Set up the table in the database
+     * @throws SQLException
+     */
+    private void prepareTables()
+    throws Exception
+    {
+        Class.forName(_driverClassName);
+        Connection connection = null;
+        try
+        {
+            connection = getConnection();
+            String tableName = "JettySessionIds";
+            DatabaseMetaData metaData = connection.getMetaData();
+            if (metaData.storesLowerCaseIdentifiers())
+                tableName = tableName.toLowerCase();
+            if (metaData.storesUpperCaseIdentifiers())
+                tableName = tableName.toUpperCase();
+            System.err.println("Transformed table name to "+tableName);
+            ResultSet result = metaData.getTables(null, null, tableName, null);
+            if (!result.next())
+            {
+                //table does not exist, so create it
+                connection.createStatement().executeUpdate(__createString);
+            }
+        }
+        finally
+        {
+            if (connection != null)
+                connection.close();
+        }
+    }
+    
+    private void insert (String id)
+    throws SQLException 
+    {
+        Connection connection = null;
+        try
+        {
+            connection = getConnection();
+            connection.setAutoCommit(true);
+            PreparedStatement statement = connection.prepareStatement(__insertString);
+            statement.setString(1, id);
+            statement.executeUpdate();
+        }
+        finally
+        {
+            if (connection != null)
+                connection.close();
+        }
+    }
+    
+    private void delete (String id)
+    throws SQLException
+    {
+        Connection connection = null;
+        try
+        {
+            connection = getConnection();
+            connection.setAutoCommit(true);
+            PreparedStatement statement = connection.prepareStatement(__deleteString);
+            statement.setString(1, id);
+            statement.executeUpdate();
+        }
+        finally
+        {
+            if (connection != null)
+                connection.close();
+        }
+    }
+    
+    
+    private boolean exists (String id)
+    throws SQLException
+    {
+        Connection connection = null;
+        try
+        {
+            connection = getConnection();
+            connection.setAutoCommit(true);
+            PreparedStatement statement = connection.prepareStatement(__queryString);
+            statement.setString(1, id);
+            ResultSet result = statement.executeQuery();
+            if (result.next())
+                return true;
+            else
+                return false;
+        }
+        finally
+        {
+            if (connection != null)
+                connection.close();
+        }
+    }
+    
+    /**
+     * Get a connection from the driver.
+     * @return
+     * @throws SQLException
+     */
+    private Connection getConnection ()
+    throws SQLException
+    {
+        return DriverManager.getConnection(_connectionUrl);
+    }
+}
