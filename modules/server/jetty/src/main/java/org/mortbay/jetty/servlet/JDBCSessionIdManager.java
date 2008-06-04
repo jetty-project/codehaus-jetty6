49b9d091f979a1eda00b1a00b8ae5f5fbd4f8f77
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-06-04 01:50:49 +0000

    JETTY-587 Take out debug printlns

--- modules/server/jetty/src/main/java/org/mortbay/jetty/servlet/JDBCSessionIdManager.java
+++ modules/server/jetty/src/main/java/org/mortbay/jetty/servlet/JDBCSessionIdManager.java
@@ -1,3 +1,17 @@
+// ========================================================================
+// Copyright 2008 Mort Bay Consulting Pty. Ltd.
+// ------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ========================================================================
+
 package org.mortbay.jetty.servlet;
 
 import java.sql.Connection;
@@ -8,7 +22,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.Timer;
@@ -19,7 +32,6 @@ import javax.servlet.http.HttpSession;
 
 import org.mortbay.jetty.Handler;
 import org.mortbay.jetty.Server;
-import org.mortbay.jetty.servlet.JDBCSessionManager.Session;
 import org.mortbay.jetty.webapp.WebAppContext;
 import org.mortbay.log.Log;
 
