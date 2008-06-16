b5cec745c58a85b0c25e54ff7fd12ff46513586d
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-06-16 14:21:39 +0000

    [JETTY-609] adding digest authentication support, contributed by david yu

--- modules/extra/jetty-client/src/main/java/org/mortbay/jetty/client/security/DigestAuthentication.java
+++ modules/extra/jetty-client/src/main/java/org/mortbay/jetty/client/security/DigestAuthentication.java
@@ -14,34 +14,122 @@ package org.mortbay.jetty.client.security;
 //limitations under the License.
 //========================================================================
 
-import org.mortbay.jetty.client.HttpExchange;
-import org.mortbay.jetty.HttpHeaders;
-import org.mortbay.jetty.security.B64Code;
-import org.mortbay.util.StringUtil;
-
-
 import java.io.IOException;
-import java.util.Iterator;
+import java.security.MessageDigest;
 import java.util.Map;
+
+import org.mortbay.jetty.HttpHeaders;
+import org.mortbay.jetty.client.HttpExchange;
+import org.mortbay.util.StringUtil;
+import org.mortbay.util.TypeUtil;
 
 public class DigestAuthentication implements Authentication
 {
+    
+    private static final String NC = "00000001";
+    
     public String getAuthType()
     {
-        return "digest";
+        return "Digest";
     }
 
-    public void setCredentials( HttpExchange exchange, SecurityRealm realm, Map details ) throws IOException
-    {
-        String authenticationString = getAuthType() + " " + B64Code.encode( realm.getPrincipal() + ":" + realm.getCredentials(), StringUtil.__ISO_8859_1);
+    public void setCredentials( HttpExchange exchange, SecurityRealm securityRealm, Map details ) 
+    throws IOException
+    {        
+        StringBuilder buffer = new StringBuilder().append(getAuthType());
         
-        for (Iterator i = details.keySet().iterator(); i.hasNext(); )
-        {
-        	String key = (String)i.next();
-        	System.out.println("Digest Detail: " + key + " " + (String)details.get(key));
-        }
+        buffer.append(" ").append("username").append('=').append('"').append(securityRealm.getPrincipal()).append('"');
         
+        buffer.append(", ").append("realm").append('=').append('"').append(String.valueOf(details.get("realm"))).append('"');
         
-        exchange.setRequestHeader( HttpHeaders.AUTHORIZATION, authenticationString);
+        buffer.append(", ").append("nonce").append('=').append('"').append(String.valueOf(details.get("nonce"))).append('"');
+        
+        buffer.append(", ").append("uri").append('=').append('"').append(exchange.getURI()).append('"');
+        
+        buffer.append(", ").append("algorithm").append('=').append(String.valueOf(details.get("algorithm")));
+        
+        String cnonce = newCnonce(exchange, securityRealm, details);
+        
+        buffer.append(", ").append("response").append('=').append('"').append(newResponse(cnonce, 
+                exchange, securityRealm, details)).append('"');
+        
+        buffer.append(", ").append("qop").append('=').append(String.valueOf(details.get("qop")));
+        
+
+        buffer.append(", ").append("nc").append('=').append(NC);
+        
+        buffer.append(", ").append("cnonce").append('=').append('"').append(cnonce).append('"');
+        
+        exchange.setRequestHeader( HttpHeaders.AUTHORIZATION, 
+                new String(buffer.toString().getBytes(StringUtil.__ISO_8859_1)));
     }
+    
+    protected String newResponse(String cnonce, HttpExchange exchange, SecurityRealm securityRealm, Map details)
+    {        
+        try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            
+            // calc A1 digest
+            md.update(securityRealm.getPrincipal().getBytes(StringUtil.__ISO_8859_1));
+            md.update((byte)':');
+            md.update(String.valueOf(details.get("realm")).getBytes(StringUtil.__ISO_8859_1));
+            md.update((byte)':');
+            md.update(securityRealm.getCredentials().getBytes(StringUtil.__ISO_8859_1));
+            byte[] ha1 = md.digest();
+            // calc A2 digest
+            md.reset();
+            md.update(exchange.getMethod().getBytes(StringUtil.__ISO_8859_1));
+            md.update((byte)':');
+            md.update(exchange.getURI().getBytes(StringUtil.__ISO_8859_1));
+            byte[] ha2=md.digest();
+            
+            md.update(TypeUtil.toString(ha1,16).getBytes(StringUtil.__ISO_8859_1));
+            md.update((byte)':');
+            md.update(String.valueOf(details.get("nonce")).getBytes(StringUtil.__ISO_8859_1));
+            md.update((byte)':');
+            md.update(NC.getBytes(StringUtil.__ISO_8859_1));
+            md.update((byte)':');
+            md.update(cnonce.getBytes(StringUtil.__ISO_8859_1));
+            md.update((byte)':');
+            md.update(String.valueOf(details.get("qop")).getBytes(StringUtil.__ISO_8859_1));
+            md.update((byte)':');
+            md.update(TypeUtil.toString(ha2,16).getBytes(StringUtil.__ISO_8859_1));
+            byte[] digest=md.digest();
+            
+            // check digest
+            return encode(digest);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }        
+    }
+    
+    protected String newCnonce(HttpExchange exchange, SecurityRealm securityRealm, Map details)
+    {
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] b= md.digest(String.valueOf(System.currentTimeMillis()).getBytes(StringUtil.__ISO_8859_1));            
+            return encode(b);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    private static String encode(byte[] data)
+    {
+        StringBuilder buffer = new StringBuilder();
+        for (int i=0; i<data.length; i++) 
+        {
+            buffer.append(Integer.toHexString((data[i] & 0xf0) >>> 4));
+            buffer.append(Integer.toHexString(data[i] & 0x0f));
+        }
+        return buffer.toString();
+    }
+
 }
