b4e52e6a4f31e3cd20073b640f6c516f584ee1e5
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-06-18 19:53:59 +0000

    realm resolver update

--- jetty/trunk/modules/extra/jetty-client/src/main/java/org/mortbay/jetty/client/security/SecurityRealmResolver.java
+++ jetty/trunk/modules/extra/jetty-client/src/main/java/org/mortbay/jetty/client/security/SecurityRealmResolver.java
@@ -6,9 +6,7 @@ import org.mortbay.jetty.client.HttpDestination;
 
 public interface SecurityRealmResolver
 {
-
     public void addSecurityRealm( SecurityRealm realm );
      
-    public SecurityRealm getRealm( String realmName, HttpDestination destination, String path ) throws IOException;
-    
+    public SecurityRealm getRealm( String realmName, HttpDestination destination, String path ) throws IOException;   
 }
