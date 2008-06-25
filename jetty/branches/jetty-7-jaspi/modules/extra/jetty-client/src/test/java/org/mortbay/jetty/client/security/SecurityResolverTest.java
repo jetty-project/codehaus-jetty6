2f0335873f89caebe648754ea1c22c78a04695a6
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-06-25 00:37:24 +0000

    jaspi experimentation

---
+++ jetty/branches/jetty-7-jaspi/modules/extra/jetty-client/src/test/java/org/mortbay/jetty/client/security/SecurityResolverTest.java
@@ -0,0 +1,32 @@
+package org.mortbay.jetty.client.security;
+
+import junit.framework.TestCase;
+
+public class SecurityResolverTest extends TestCase
+{
+    public void testNothing()
+    {
+        
+    }
+    /* TODO
+
+    public void testCredentialParsing() throws Exception
+    {
+        SecurityListener resolver = new SecurityListener();
+        Buffer value = new ByteArrayBuffer("basic a=b".getBytes());
+        
+        assertEquals( "basic", resolver.scrapeAuthenticationType( value.toString() ) );
+        assertEquals( 1, resolver.scrapeAuthenticationDetails( value.toString() ).size() );
+
+        value = new ByteArrayBuffer("digest a=boo, c=\"doo\" , egg=foo".getBytes());
+        
+        assertEquals( "digest", resolver.scrapeAuthenticationType( value.toString() ) );
+        Map<String,String> testMap = resolver.scrapeAuthenticationDetails( value.toString() );
+        assertEquals( 3, testMap.size() );
+        assertEquals( "boo", testMap.get("a") );
+        assertEquals( "doo", testMap.get("c") );
+        assertEquals( "foo", testMap.get("egg") );
+    }
+    
+    */
+}
