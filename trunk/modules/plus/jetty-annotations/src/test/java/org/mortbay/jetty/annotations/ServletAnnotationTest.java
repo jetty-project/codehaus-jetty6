76132316bf0212ced20a8c64b1aaaaf28fcbd5b3
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-07-21 13:51:02 +0000

    JETTY-612 temporarily add src for servlet spec 3.0 annotations; add processing of those annotations

---
+++ trunk/modules/plus/jetty-annotations/src/test/java/org/mortbay/jetty/annotations/ServletAnnotationTest.java
@@ -0,0 +1,91 @@
+// ========================================================================
+//$Id$
+//Copyright 2008 Mort Bay Consulting Pty. Ltd.
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+package org.mortbay.jetty.annotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+import org.mortbay.jetty.plus.annotation.InjectionCollection;
+import org.mortbay.jetty.plus.annotation.LifeCycleCallbackCollection;
+import org.mortbay.jetty.plus.annotation.RunAsCollection;
+import org.mortbay.jetty.servlet.FilterHolder;
+import org.mortbay.jetty.servlet.FilterMapping;
+import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.jetty.servlet.ServletMapping;
+
+import junit.framework.TestCase;
+
+public class ServletAnnotationTest extends TestCase
+{ 
+   
+    
+    public void testAnnotations() throws Exception
+    {
+        List servlets = new ArrayList();
+        List filters = new ArrayList();
+        List listeners = new ArrayList();
+        List servletMappings = new ArrayList();
+        List filterMappings = new ArrayList();
+        
+        List classes = new ArrayList();
+        classes.add("org.mortbay.jetty.annotations.ClassC");
+       
+        AnnotationFinder finder = new AnnotationFinder();
+        finder.find (classes, 
+                new ClassNameResolver()
+        {
+
+            public boolean isExcluded(String name)
+            {
+                return false;
+            }
+
+            public boolean shouldOverride(String name)
+            {
+                return false;
+            }
+            
+        });
+        
+        AnnotationProcessor processor = new AnnotationProcessor (finder, new RunAsCollection(), new InjectionCollection(), new LifeCycleCallbackCollection(), 
+                servlets, filters, listeners, servletMappings, filterMappings);
+        processor.process();
+        assertEquals(1, servlets.size());
+        ServletHolder sholder = (ServletHolder)servlets.get(0);
+        assertEquals("CServlet", sholder.getName());
+        Map sinitparams = sholder.getInitParameters();
+        assertEquals(1, sinitparams.size());
+        assertTrue(sinitparams.containsKey("x"));
+        assertTrue(sinitparams.containsValue("y"));
+        assertEquals(1, filters.size());
+        FilterHolder fholder = (FilterHolder)filters.get(0);
+        Map finitparams = fholder.getInitParameters();
+        assertEquals(1, finitparams.size());
+        assertTrue(finitparams.containsKey("a"));
+        assertTrue(finitparams.containsValue("99"));
+        assertEquals(1, servletMappings.size());
+        ServletMapping smap = (ServletMapping)servletMappings.get(0);
+        assertEquals("CServlet", smap.getServletName());
+        assertEquals(2, smap.getPathSpecs().length);
+        assertEquals(1, filterMappings.size());
+        FilterMapping fmap = (FilterMapping)filterMappings.get(0);
+        assertEquals("CFilter", fmap.getFilterName());
+        assertEquals(1, fmap.getPathSpecs().length);
+        
+    }
+
+}
