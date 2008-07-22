72dd1d26e1f7c11e26678c08449479f4d4edd6c1
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-07-22 13:40:59 +0000

    JETTY-612 improve pojo annotation test webapp and add pojo filter and pojo servlet

--- trunk/modules/examples/jetty-annotation-example-webapp/src/main/java/com/acme/PojoAnnotationTest.java
+++ trunk/modules/examples/jetty-annotation-example-webapp/src/main/java/com/acme/PojoAnnotationTest.java
@@ -1,3 +1,17 @@
+//========================================================================
+//$Id$
+//Copyright 2004-2005 Mort Bay Consulting Pty. Ltd.
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
 package com.acme;
 import java.io.IOException;
 
@@ -56,14 +70,13 @@ public class PojoAnnotationTest
         result = request.isUserInRole("special");
         response.getWriter().println("<p>RunAs userIsInRole special = "+result+"</p>");
         response.getWriter().println("<p>PostConstruct called "+postConstruct+"</p>");
-        response.getWriter().println("<p>PreDestroy called "+!preDestroy+"</p>");
+        response.getWriter().println("<p>PreDestroy not called "+!preDestroy+"</p>");
        
-        /*
-         * TODO Make a pojo Filter
+        response.getWriter().println("<p>ContextInitialized called "+request.getServletContext().getAttribute("contextInitialized"));
+
         HttpSession session = request.getSession(true);
-        Boolean filterResult = (Boolean)session.getAttribute("filterResult");
+        Boolean filterResult = (Boolean)session.getAttribute("action");
         result = (filterResult != null && filterResult.booleanValue());
-        response.getWriter().println("<p>Filter was called "+result+"</p>");
-        */
+        response.getWriter().println("<p>Filter was called "+result+"</p>");     
     }
 }
