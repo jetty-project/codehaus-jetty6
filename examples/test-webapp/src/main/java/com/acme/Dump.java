7b156cc466dcff7af3b763c36fdee57b7eeeeebd
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-07-24 03:10:13 +0000

    JETTY-319

--- examples/test-webapp/src/main/java/com/acme/Dump.java
+++ examples/test-webapp/src/main/java/com/acme/Dump.java
@@ -47,10 +47,18 @@ import org.mortbay.util.ajax.ContinuationSupport;
  */
 public class Dump extends HttpServlet
 {
+    static boolean fixed;
     /* ------------------------------------------------------------ */
     public void init(ServletConfig config) throws ServletException
     {
     	super.init(config);
+    	
+    	if (config.getInitParameter("unavailable")!=null && !fixed)
+    	{
+    	    
+    	    fixed=true;
+    	    throw new UnavailableException("Unavailable test",Integer.parseInt(config.getInitParameter("unavailable")));
+    	}
     }
 
     /* ------------------------------------------------------------ */
