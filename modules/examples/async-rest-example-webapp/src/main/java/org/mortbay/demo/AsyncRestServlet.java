25c358bfb2b6516fd4a6da5b57bf6b9c8466000c
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-07-29 10:57:14 +0000

    update

--- modules/examples/async-rest-example-webapp/src/main/java/org/mortbay/demo/AsyncRestServlet.java
+++ modules/examples/async-rest-example-webapp/src/main/java/org/mortbay/demo/AsyncRestServlet.java
@@ -2,99 +2,113 @@ package org.mortbay.demo;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.mortbay.jetty.client.ContentExchange;
 import org.mortbay.jetty.client.HttpClient;
-import org.mortbay.util.ajax.JSON;
 
 /**
  * Servlet implementation class AsyncRESTServlet
  */
 public class AsyncRestServlet extends HttpServlet
 {
-    public static final String ITEMS_PARAM="items";
-    HttpClient _client;
-    
-    
-    public void init() throws ServletException
+    private final static String __DEFAULT_APPID = "Webtide81-adf4-4f0a-ad58-d91e41bbe85";
+
+    final static String ITEMS_PARAM = "items";
+    final static String APPID_PARAM = "appid";
+
+    final static String CLIENT_ATTR = "org.mortbay.demo.client";
+    final static String DURATION_ATTR = "org.mortbay.demo.duration";
+
+    private HttpClient _client;
+    private String _appid;
+
+    public void init(ServletConfig servletConfig) throws ServletException
     {
+        if (servletConfig.getInitParameter(APPID_PARAM) == null)
+            _appid = __DEFAULT_APPID;
+        else
+            _appid = servletConfig.getInitParameter(APPID_PARAM);
+
         _client = new HttpClient();
         _client.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL);
-        
+
         try
         {
             _client.start();
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             throw new ServletException(e);
         }
     }
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        final ServletRequest request = (ServletRequest)req; 
-        
-        if (request.getAttribute("items")==null)
-        {
-            System.err.println("Search EBAY "+request.getParameter(ITEMS_PARAM));
+        long start = System.currentTimeMillis();
 
-            // Create an async RESTFUL request to ebay
-            ContentExchange exchange = new ContentExchange()
-            {
-                protected void onResponseComplete() throws IOException
-                {
-                    super.onResponseComplete();
-                    Map query = (Map)JSON.parse(this.getResponseContent());
-                    Object[] itemsArray = (Object[])query.get("Item");
-                    
-                    if (itemsArray==null)
-                    {
-                        request.setAttribute("message","No Items Found!");
-                    }
-                    else
-                    {
-                        List items = new ArrayList();
-                        for (Object o : (Object[])query.get("Item"))
-                            items.add((Map)o);
-                        request.setAttribute("items",items);
-                        request.setAttribute("message","&nbsp;");
-                    }
-                    request.resume();
-                }           
-            };
-            
+        if (request.isInitial())
+        {
+            System.err.println("Search EBAY " + request.getParameter(ITEMS_PARAM));
+
             // Suspend and send the restful request
             request.suspend();
             request.getServletResponse().disable();
-            exchange.setMethod("GET");
-            exchange.setURL("http://open.api.ebay.com/shopping?MaxEntries=20&appid=Webtide81-adf4-4f0a-ad58-d91e41bbe85&version=573&siteid=0&callname=FindItems&responseencoding=JSON&QueryKeywords=" +request.getParameter(ITEMS_PARAM));
-            request.setAttribute("items",Collections.emptyList());
-            _client.send(exchange);
-            return;
+
+            EbayFindItemAsync searches = new EbayFindItemAsync(_client, request, _appid);
+
+            StringTokenizer strtok = new StringTokenizer(request.getParameter(ITEMS_PARAM), ",");
+            while (strtok.hasMoreTokens())
+            {
+                searches.search(strtok.nextToken());
+            }
+
+            request.setAttribute(CLIENT_ATTR, searches);
+            request.setAttribute(DURATION_ATTR, new Long(System.currentTimeMillis() - start));
         }
-        
-        List list=(List)request.getAttribute("items");
-        resp.setContentType("text/html");
-        PrintWriter out=resp.getWriter();
-        for (Object o : list)
+        else
         {
-            Map m = (java.util.Map)o;
-            out.println("Item: "+m.get("ItemID")+"&nbsp;"+m.get("Title")+"<br/>");
+            // resumed request: either we got all the results, or we timed out
+            EbayFindItemAsync aggregator = (EbayFindItemAsync) request.getAttribute(CLIENT_ATTR);
+            List<Map<String, String>> results = aggregator.getPayload();
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<html><head><style type='text/css'>img:hover {height:75px}</style></head><body><small>");
+
+            for (Map<String, String> m : results)
+            {
+                out.print("<a href=\""+m.get("ViewItemURLForNaturalSearch")+"\">");
+                if (m.containsKey("GalleryURL"))
+                    out.print("<img border='1px' height='20px' src=\""+m.get("GalleryURL")+"\"/>&nbsp;");
+                
+                out.print(m.get("Title"));
+                out.println("</a><br/>");
+            }
+
+            out.println("<hr />");
+            long now = System.currentTimeMillis();
+            out.print("Total Time: ");
+            out.print(now - aggregator.getStartTime());
+            out.println("ms<br/>");
+            out.print("Thread held: ");
+
+            long duration = (Long) request.getAttribute(DURATION_ATTR);
+            long duration2 = now - start;
+            out.print(duration + duration2);
+            out.println("ms (" + duration + " initial + " + duration2 + " resume )");
+            out.println("</small></body></html>");
+            out.close();
         }
     }
 
-    protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         doGet(request, response);
     }
---
+++ modules/examples/async-rest-example-webapp/src/main/java/org/mortbay/demo/EbayFindItemAsync.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package org.mortbay.demo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletRequest;
+
+import org.mortbay.jetty.client.ContentExchange;
+import org.mortbay.jetty.client.HttpClient;
+import org.mortbay.util.ajax.JSON;
+
+/**
+ * @author athena
+ * 
+ */
+public class EbayFindItemAsync
+{
+    private ServletRequest _request;
+    private HttpClient _client;
+    private String _appid;
+    private List<Map<String, String>> _results = new ArrayList<Map<String, String>>();
+
+    private long _startTime;
+    private int _todo;
+
+    public EbayFindItemAsync(HttpClient client, ServletRequest request, String appid)
+    {
+        _startTime = System.currentTimeMillis();
+        _request = request;
+        _client = client;
+        _appid = appid;
+    }
+
+    public void search(String item) throws IOException
+    {
+        final String itemName = item;
+        // Create an async RESTFUL request to ebay
+        ContentExchange exchange = new ContentExchange()
+        {
+            protected void onResponseComplete() throws IOException
+            {
+                super.onResponseComplete();
+                Map query = (Map) JSON.parse(this.getResponseContent());
+                Object[] itemsArray = (Object[]) query.get("Item");
+
+                if (itemsArray == null)
+                {
+                    Map<String, String> m = new HashMap<String, String>();
+                    m.put("ItemID", "\"" + itemName + "\"");
+                    m.put("Title", " not found!");
+                    _results.add(m);
+                }
+                else
+                {
+                    for (Object o : (Object[]) query.get("Item"))
+                        _results.add((Map) o);
+                }
+
+                synchronized (this)
+                {
+                    _todo--;
+                    if (_todo == 0)
+                        _request.resume();
+                }
+            }
+        };
+
+        exchange.setMethod("GET");
+        exchange.setURL("http://open.api.ebay.com/shopping?MaxEntries=5&appid=" + _appid + "&version=573&siteid=0&callname=FindItems&responseencoding=JSON&QueryKeywords=" + item);
+
+        synchronized (this)
+        {
+            _todo++;
+        }
+
+        _client.send(exchange);
+    }
+
+    public List<Map<String, String>> getPayload()
+    {
+        return _results;
+    }
+
+    public long getStartTime()
+    {
+        return _startTime;
+    }
+}
