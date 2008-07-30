63672bc5aaabfc8ef1ecd8879a3b4b003dfb17da
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-07-30 15:01:18 +0000

    tweaks

--- modules/examples/async-rest-example-webapp/src/main/java/org/mortbay/demo/AsyncRestServlet.java
+++ modules/examples/async-rest-example-webapp/src/main/java/org/mortbay/demo/AsyncRestServlet.java
@@ -2,6 +2,7 @@ package org.mortbay.demo;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -26,6 +27,7 @@ public class AsyncRestServlet extends HttpServlet
 
     final static String CLIENT_ATTR = "org.mortbay.demo.client";
     final static String DURATION_ATTR = "org.mortbay.demo.duration";
+    final static String START_ATTR = "org.mortbay.demo.start";
 
     private HttpClient _client;
     private String _appid;
@@ -52,60 +54,61 @@ public class AsyncRestServlet extends HttpServlet
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        long start = System.currentTimeMillis();
+        Long start=System.currentTimeMillis();
 
-        if (request.isInitial())
+        if (request.isInitial() || request.getAttribute(CLIENT_ATTR)==null)
         {
             System.err.println("Search EBAY " + request.getParameter(ITEMS_PARAM));
 
             // Suspend and send the restful request
             request.suspend();
-            request.getServletResponse().disable();
 
             EbayFindItemAsync searches = new EbayFindItemAsync(_client, request, _appid);
 
             StringTokenizer strtok = new StringTokenizer(request.getParameter(ITEMS_PARAM), ",");
             while (strtok.hasMoreTokens())
             {
-                searches.search(strtok.nextToken());
+                searches.search(URLEncoder.encode(strtok.nextToken(), "UTF-8") );
             }
 
             request.setAttribute(CLIENT_ATTR, searches);
+            request.setAttribute(START_ATTR, start);
             request.setAttribute(DURATION_ATTR, new Long(System.currentTimeMillis() - start));
+            return;
         }
-        else
+
+        // resumed request: either we got all the results, or we timed out
+        EbayFindItemAsync aggregator = (EbayFindItemAsync) request.getAttribute(CLIENT_ATTR);
+        List<Map<String, String>> results = aggregator.getPayload();
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><style type='text/css'>img:hover {height:75px}</style></head><body><small>");
+
+        for (Map<String, String> m : results)
         {
-            // resumed request: either we got all the results, or we timed out
-            EbayFindItemAsync aggregator = (EbayFindItemAsync) request.getAttribute(CLIENT_ATTR);
-            List<Map<String, String>> results = aggregator.getPayload();
-            response.setContentType("text/html");
-            PrintWriter out = response.getWriter();
-            out.println("<html><head><style type='text/css'>img:hover {height:75px}</style></head><body><small>");
+            out.print("<a href=\""+m.get("ViewItemURLForNaturalSearch")+"\">");
+            if (m.containsKey("GalleryURL"))
+                out.print("<img border='1px' height='20px' src=\""+m.get("GalleryURL")+"\"/>&nbsp;");
 
-            for (Map<String, String> m : results)
-            {
-                out.print("<a href=\""+m.get("ViewItemURLForNaturalSearch")+"\">");
-                if (m.containsKey("GalleryURL"))
-                    out.print("<img border='1px' height='20px' src=\""+m.get("GalleryURL")+"\"/>&nbsp;");
-                
-                out.print(m.get("Title"));
-                out.println("</a><br/>");
-            }
-
-            out.println("<hr />");
-            long now = System.currentTimeMillis();
-            out.print("Total Time: ");
-            out.print(now - aggregator.getStartTime());
-            out.println("ms<br/>");
-            out.print("Thread held: ");
-
-            long duration = (Long) request.getAttribute(DURATION_ATTR);
-            long duration2 = now - start;
-            out.print(duration + duration2);
-            out.println("ms (" + duration + " initial + " + duration2 + " resume )");
-            out.println("</small></body></html>");
-            out.close();
+            out.print(m.get("Title"));
+            out.println("</a><br/>");
         }
+
+        out.println("<hr />");
+        long duration = (Long) request.getAttribute(DURATION_ATTR);
+        long start0 = (Long) request.getAttribute(START_ATTR);
+
+        long now = System.currentTimeMillis();
+        out.print("Total Time: ");
+        out.print(now - start0);
+        out.println("ms<br/>");
+        out.print("Thread held: ");
+
+        long duration2 = now - start;
+        out.print(duration + duration2);
+        out.println("ms (" + duration + " initial + " + duration2 + " resume )");
+        out.println("</small></body></html>");
+        out.close();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
