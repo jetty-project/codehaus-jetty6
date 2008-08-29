2e01a40717f6df6fd4007f5351b938850646cb6b
dyu <dyu@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-08-29 08:45:32 +0000

    JETTY-680 Can't configure the ResourceCollection with maven

--- modules/jetty/src/main/java/org/mortbay/resource/ResourceCollection.java
+++ modules/jetty/src/main/java/org/mortbay/resource/ResourceCollection.java
@@ -158,6 +158,16 @@ public class ResourceCollection extends Resource
     /* ------------------------------------------------------------ */
     /**
      * 
+     * @param csvResources Comma separated values
+     */
+    public void setResourcesAsCSV(String csvResources)
+    {
+        setResources(csvResources);
+    }
+    
+    /* ------------------------------------------------------------ */
+    /**
+     * 
      * @return the resource array
      */
     public Resource[] getResources()
