4d35cc3d9347c1d05c5cc42f7af8249f2e55c0a6
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-09-04 00:03:14 +0000

    JETTY-699

--- modules/extra/cometd/client/bin/run.sh
+++ modules/extra/cometd/client/bin/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+cd `dirname $0`/..
 java -Xmx2048M -cp target/classes:target/test-classes/:../bayeux/target/classes/:../../../../lib/cometd/cometd-api-1.0-SNAPSHOT.jar:../../../../lib/ext/jetty-client-7.0-SNAPSHOT.jar:../../../../lib/jetty-util-7.0-SNAPSHOT.jar:../../../../lib/jetty-7.0-SNAPSHOT.jar:../../../../lib/ssl/jetty-ssl-7.0-SNAPSHOT.jar:../../../../lib/servlet-api-3.0.pre1.jar org.mortbay.cometd.client.BayeuxLoadGenerator
