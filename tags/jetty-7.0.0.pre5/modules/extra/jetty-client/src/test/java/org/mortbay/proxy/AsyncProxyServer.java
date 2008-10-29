8fe02889d5e100cd34adf0a8f771f99ec9a17f84
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-10-29 15:36:15 +0000

    [maven-release-plugin]  copy for tag jetty-7.0.0.pre5

---
+++ tags/jetty-7.0.0.pre5/modules/extra/jetty-client/src/test/java/org/mortbay/proxy/AsyncProxyServer.java
@@ -0,0 +1,29 @@
+package org.mortbay.proxy;
+
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.mortbay.jetty.servlet.FilterHolder;
+import org.mortbay.jetty.servlet.ServletHandler;
+import org.mortbay.jetty.servlet.ServletHolder;
+
+public class AsyncProxyServer
+{
+    public static void main(String[] args)
+        throws Exception
+    {
+        Server server = new Server();
+        Connector connector=new SelectChannelConnector();
+        connector.setPort(8888);
+        server.setConnectors(new Connector[]{connector});
+        
+        ServletHandler handler=new ServletHandler();
+        server.setHandler(handler);
+        
+        // FilterHolder gzip = handler.addFilterWithMapping("org.mortbay.servlet.GzipFilter","/*",0);
+        ServletHolder proxy = handler.addServletWithMapping("org.mortbay.proxy.AsyncProxyServlet","/");
+        
+        server.start();
+        server.join();
+    }
+}
