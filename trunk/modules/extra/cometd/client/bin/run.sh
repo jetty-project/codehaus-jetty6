cd0403c3e9b8c74ed4ab76df862ff38d3bb377e3
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-10-30 01:11:04 +0000

    updated

--- trunk/modules/extra/cometd/client/bin/run.sh
+++ trunk/modules/extra/cometd/client/bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd `dirname $0`/..
 java -Xmx2048M -cp \
-target/classes:target/test-classes/:../bayeux/target/classes/:../../../../lib/cometd/cometd-api-1.0-SNAPSHOT.jar:../../../../lib/ext/jetty-client-7.0-SNAPSHOT.jar:../../../../lib/jetty-util-7.0-SNAPSHOT.jar:../../../../lib/jetty-7.0-SNAPSHOT.jar:../../../../lib/jetty-security-7.0-SNAPSHOT.jar:../../../../lib/jetty-ssl-7.0-SNAPSHOT.jar:../../../../lib/jetty-webapp-7.0-SNAPSHOT.jar:../../../../lib/jetty-xml-7.0-SNAPSHOT.jar:../../../../lib/servlet-api-3.0.pre1.jar \
+target/classes:target/test-classes/:../bayeux/target/classes/:../../../../lib/cometd/cometd-api-1.0.beta4.jar:../../../../lib/ext/jetty-client-7.0-SNAPSHOT.jar:../../../../lib/jetty-util-7.0-SNAPSHOT.jar:../../../../lib/jetty-7.0-SNAPSHOT.jar:../../../../lib/jetty-security-7.0-SNAPSHOT.jar:../../../../lib/jetty-ssl-7.0-SNAPSHOT.jar:../../../../lib/jetty-webapp-7.0-SNAPSHOT.jar:../../../../lib/jetty-xml-7.0-SNAPSHOT.jar:../../../../lib/servlet-api-3.0.pre4.jar \
 org.mortbay.cometd.client.BayeuxLoadGenerator
