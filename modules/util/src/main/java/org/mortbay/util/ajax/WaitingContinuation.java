219ae17b2417df475494d6cb20e10a2aacfd3482
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-11-13 00:41:15 +0000

    JETTY-790

--- modules/util/src/main/java/org/mortbay/util/ajax/WaitingContinuation.java
+++ modules/util/src/main/java/org/mortbay/util/ajax/WaitingContinuation.java
@@ -123,7 +123,7 @@ public class WaitingContinuation implements org.mortbay.util.ajax.Continuation
 
     public void setMutex(Object mutex)
     {
-        if (!_new && _mutex!=this)
+        if (_pending && mutex!=_mutex)
             throw new IllegalStateException();
         _mutex = mutex==null ? this : mutex; 
     }
