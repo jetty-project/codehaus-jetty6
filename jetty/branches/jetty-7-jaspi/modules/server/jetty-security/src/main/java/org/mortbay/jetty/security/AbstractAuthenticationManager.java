5c000031cf449c3190726533f033ddd6842a69b3
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-11-25 18:11:08 +0000

    Doh! Forgot to check in the classes

---
+++ jetty/branches/jetty-7-jaspi/modules/server/jetty-security/src/main/java/org/mortbay/jetty/security/AbstractAuthenticationManager.java
@@ -0,0 +1,80 @@
+// ========================================================================
+// Copyright 2008 Mort Bay Consulting Pty. Ltd.
+// ------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ========================================================================
+
+
+package org.mortbay.jetty.security;
+
+import org.mortbay.component.AbstractLifeCycle;
+import org.mortbay.jetty.AuthenticationManager;
+import org.mortbay.jetty.handler.SecurityHandler;
+
+public abstract class AbstractAuthenticationManager extends AbstractLifeCycle implements AuthenticationManager 
+{
+    private String _method;
+    private boolean _lazy = true; //by default allow lazy authentication
+    private String _errorPage; //only for form auth
+    private String _loginPage; //only for form auth
+    private SecurityHandler _securityHandler;
+    
+    
+    public String getAuthMethod()
+    {
+        return _method;
+    }
+
+    public void setAuthMethod(String method)
+    {
+        _method = method;
+    }
+
+    public boolean getAllowLazyAuth()
+    {
+        return _lazy;
+    }
+
+    public void setAllowLazyAuth(boolean lazy)
+    {
+        _lazy = lazy;
+    }
+
+    public String getErrorPage()
+    {
+        return _errorPage;
+    }
+
+    public String getLoginPage()
+    {
+        return _loginPage;
+    }
+
+    public void setErrorPage(String errorPage)
+    {
+        _errorPage = errorPage;  
+    }
+
+    public void setLoginPage(String loginPage)
+    {
+        _loginPage = loginPage;        
+    }
+    
+    public SecurityHandler getSecurityHandler()
+    {
+       return _securityHandler;
+    }
+
+    public void setSecurityHandler(SecurityHandler handler)
+    {
+        _securityHandler = handler;  
+    } 
+}
---
+++ jetty/branches/jetty-7-jaspi/modules/server/jetty-security/src/main/java/org/mortbay/jetty/security/jaspi/JaspiAuthenticationManager.java
@@ -0,0 +1,111 @@
+package org.mortbay.jetty.security.jaspi;
+
+import javax.security.auth.message.ServerAuth;
+import javax.security.auth.message.config.ServerAuthConfig;
+import javax.security.auth.message.config.ServerAuthContext;
+
+import org.mortbay.jetty.JettyMessageInfo;
+import org.mortbay.jetty.LoginService;
+import org.mortbay.jetty.ServerAuthException;
+import org.mortbay.jetty.ServerAuthResult;
+import org.mortbay.jetty.ServerAuthStatus;
+import org.mortbay.jetty.ServerAuthentication;
+import org.mortbay.jetty.handler.SecurityHandler;
+import org.mortbay.jetty.security.AbstractAuthenticationManager;
+import org.mortbay.jetty.security.Constraint;
+import org.mortbay.jetty.security.ServletCallbackHandler;
+import org.mortbay.jetty.security.jaspi.modules.BasicAuthModule;
+import org.mortbay.jetty.security.jaspi.modules.ClientCertAuthModule;
+import org.mortbay.jetty.security.jaspi.modules.DigestAuthModule;
+import org.mortbay.jetty.security.jaspi.modules.FormAuthModule;
+
+public class JaspiAuthenticationManager extends AbstractAuthenticationManager
+{
+    private ServerAuthentication _serverAuthentication;
+    private ServerAuthContext _serverAuthContext;
+    private ServerAuth _serverAuth;
+    
+    public JaspiAuthenticationManager ()
+    {}
+    
+    public void setServerAuth (ServerAuth serverAuth)
+    {
+        //TODO set a jaspi auth module to use, possibly third party
+        _serverAuth = serverAuth;
+    }
+    
+    
+    protected void doStart() throws Exception
+    {
+        super.doStart();
+
+        if (getAuthMethod() != null && !"".equals(getAuthMethod()))
+        {
+            LoginService loginService = getSecurityHandler().getLoginService();
+
+            ServletCallbackHandler callbackHandler=new ServletCallbackHandler(loginService);
+
+            //allow a jaspi auth module to be plugged in - only use our defaults if we
+            //haven't been given one to use
+            if (_serverAuth == null)
+            {
+                if (Constraint.__FORM_AUTH.equals(getAuthMethod()))
+                {
+                    _serverAuthContext = new FormAuthModule(callbackHandler, getLoginPage(), getErrorPage());
+                } 
+                else if (Constraint.__BASIC_AUTH.equals(getAuthMethod()))
+                {
+                    _serverAuthContext = new BasicAuthModule(callbackHandler, loginService.getName());
+                } 
+                else if (Constraint.__DIGEST_AUTH.equals(getAuthMethod()))
+                {
+                    _serverAuthContext = new DigestAuthModule(callbackHandler, loginService.getName());
+                } 
+                else if (Constraint.__CERT_AUTH.equals(getAuthMethod()) ||
+                        Constraint.__CERT_AUTH2.equals(getAuthMethod()))
+                {
+                    //TODO figure out how to configure max handshake?
+                    _serverAuthContext = new ClientCertAuthModule(callbackHandler);
+                }           
+                else
+                    throw new IllegalStateException ("Unrecognized auth method: "+getAuthMethod());
+            }
+
+            //TODO - how does the ServerAuth module relate to the ServerAuthContext? Can we plug in a
+            //3rd party jaspi module by passing it to our JaspiServerAuthentication?
+
+            String appContext = null;
+            ServerAuthConfig serverAuthConfig = new SimpleAuthConfig(appContext, _serverAuthContext);
+            _serverAuthentication = new JaspiServerAuthentication(appContext,
+                                                                  serverAuthConfig,
+                                                                  null,
+                                                                  callbackHandler,
+                                                                  //TODO??
+                                                                  null,
+                                                                  getAllowLazyAuth());
+        }
+    }
+
+    protected void doStop() throws Exception
+    {
+        super.doStop();
+    }
+
+    public ServerAuthStatus secureResponse(JettyMessageInfo messageInfo, ServerAuthResult validatedUser) 
+    throws ServerAuthException
+    {
+        if (_serverAuthentication == null)
+            return null;
+        
+        return _serverAuthentication.secureResponse(messageInfo, validatedUser);
+    }
+
+    public ServerAuthResult validateRequest(JettyMessageInfo messageInfo) 
+    throws ServerAuthException
+    {
+        if (_serverAuthentication == null)
+            return null;
+        
+       return _serverAuthentication.validateRequest(messageInfo);
+    }
+}
