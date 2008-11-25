49976b55db2e3a7d677e66aa5dcc1fa76e4b4ea1
dyu <dyu@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2008-11-25 10:13:35 +0000

    JETTY-808 cometd client demo run.sh

--- trunk/modules/extra/cometd/client/bin/run.sh
+++ trunk/modules/extra/cometd/client/bin/run.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 cd `dirname $0`/..
-java -Xmx2048M -cp \
-target/classes:target/test-classes/:../bayeux/target/classes/:../../../../lib/cometd/cometd-api-1.0.beta4.jar:../../../../lib/ext/jetty-client-7.0-SNAPSHOT.jar:../../../../lib/jetty-util-7.0-SNAPSHOT.jar:../../../../lib/jetty-7.0-SNAPSHOT.jar:../../../../lib/jetty-security-7.0-SNAPSHOT.jar:../../../../lib/jetty-ssl-7.0-SNAPSHOT.jar:../../../../lib/jetty-webapp-7.0-SNAPSHOT.jar:../../../../lib/jetty-xml-7.0-SNAPSHOT.jar:../../../../lib/servlet-api-3.0.pre4.jar \
-org.mortbay.cometd.client.BayeuxLoadGenerator
+java -Xmx2048M -Djetty.home=../../../../ -DSTOP.PORT=-1 -DSTART=bin/start.config -DOPTIONS=jetty,ext,cometd -jar ../../../../start.jar
