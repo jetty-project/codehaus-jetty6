3bfe68468fbecb25726255bbfbb1bad10ce60156
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-01-19 23:23:33 +0000

    JETTY-862

--- modules/server/jetty/src/test/java/org/mortbay/jetty/EncodedHttpURITest.java
+++ modules/server/jetty/src/test/java/org/mortbay/jetty/EncodedHttpURITest.java
@@ -11,8 +11,10 @@ public class EncodedHttpURITest extends TestCase
     public void testNonURIAscii ()
     throws Exception
     {
-        String url = "http://www.foo.com/mañana";
-     
+        String url = "http://www.foo.com/ma\u00F1ana";
+        byte[] asISO = url.getBytes("ISO-8859-1");
+        String str = new String(asISO, "ISO-8859-1");
+        
         //use a non UTF-8 charset as the encoding and url-escape as per
         //http://www.w3.org/TR/html40/appendix/notes.html#non-ascii-chars
         String s = URLEncoder.encode(url, "ISO-8859-1");     
@@ -24,6 +26,5 @@ public class EncodedHttpURITest extends TestCase
         //decode the url encoding
         String d = URLDecoder.decode(uri.getCompletePath(), "ISO-8859-1");
         assertEquals(url, d);
-
     }
 }
