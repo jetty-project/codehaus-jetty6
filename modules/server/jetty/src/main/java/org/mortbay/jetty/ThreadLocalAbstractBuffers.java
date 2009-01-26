9e80874f8765b615b2efd54808e9280bb5f2f486
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-01-26 11:56:55 +0000

    JETTY-861 Improved threadlocal and test harness

--- modules/server/jetty/src/main/java/org/mortbay/jetty/ThreadLocalAbstractBuffers.java
+++ modules/server/jetty/src/main/java/org/mortbay/jetty/ThreadLocalAbstractBuffers.java
@@ -27,103 +27,115 @@ import org.mortbay.io.ByteArrayBuffer;
 public abstract class ThreadLocalAbstractBuffers
     extends AbstractBuffers
 {
+    final static private int __HEADER=0;
+    final static private int __REQUEST=1;
+    final static private int __RESPONSE=2;
+    final static private int __OTHER=3;
+    final private int[] _pool={2,1,1,2};
 
+    private final ThreadLocal<ThreadBuffers> _buffers=new ThreadLocal<ThreadBuffers>()
+    {
+        protected ThreadBuffers initialValue()
+        {
+            return new ThreadBuffers(_pool[__HEADER],_pool[__REQUEST],_pool[__RESPONSE],_pool[__OTHER]);
+        }
+    };
+    
     public ThreadLocalAbstractBuffers()
     {
         super();
     }
 
-    private ThreadLocal tlHeader;
-    private ThreadLocal tlRequest;
-    private ThreadLocal tlResponse;
 
 
-    public Buffer getBuffer( int size )
+    public Buffer getBuffer(final int size )
     {
+        final int set = (size==_headerBufferSize)?__HEADER
+                :(size==_responseBufferSize)?__RESPONSE
+                        :(size==_requestBufferSize)?__REQUEST:__OTHER;
 
-        if ( size == _headerBufferSize )
+        final ThreadBuffers thread_buffers = _buffers.get();
+        
+        final Buffer[] buffers=thread_buffers._buffers[set];
+        for (int i=0;i<buffers.length;i++)
         {
-           Buffer b = (Buffer) tlHeader.get();
-	   if (b!=null)
-	   {
-	       tlHeader.set(null);
-	       return b;
-	   }
+            final Buffer b=buffers[i];
+            if (b!=null && b.capacity()==size)
+            {
+                buffers[i]=null;
+                return b;
+            }
         }
-        else if ( size == _responseBufferSize )
-        {
-           Buffer b = (Buffer) tlResponse.get();
-	   if (b!=null)
-	   {
-	       tlResponse.set(null);
-	       return b;
-	   }
-        }
-        else if ( size == _requestBufferSize )
-        {
-           Buffer b = (Buffer) tlRequest.get();
-	   if (b!=null)
-	   {
-	       tlRequest.set(null);
-	       return b;
-	   }
-        }
-
-        return newBuffer( size );
+        
+        return newBuffer(size);
     }
 
     public void returnBuffer( Buffer buffer )
     {
         buffer.clear();
-        if (!buffer.isVolatile() && !buffer.isImmutable())
+        if (buffer.isVolatile() || buffer.isImmutable())
+            return;
+        
+        int size=buffer.capacity();
+        final int set = (size==_headerBufferSize)?__HEADER
+                :(size==_responseBufferSize)?__RESPONSE
+                        :(size==_requestBufferSize)?__REQUEST:__OTHER;
+
+        final ThreadBuffers thread_buffers = _buffers.get();
+        final Buffer[] buffers=thread_buffers._buffers[set];
+        for (int i=0;i<buffers.length;i++)
         {
-            int size=buffer.capacity();
-	    if ( size == _headerBufferSize )
-	    {
-	       tlHeader.set(buffer);
-	    }
-	    else if ( size == _responseBufferSize )
-	    {
-	       tlResponse.set(buffer);
-	    }
-	    else if ( size == _requestBufferSize )
-	    {
-	       tlRequest.set(buffer);
-	    }
-	}
+            if (buffers[i]==null)
+            {
+                buffers[i]=buffer;
+                return;
+            }
+        }
+            
     }
     
 
     protected void doStart()
         throws Exception
     {
-        tlHeader = new ThreadLocal()
-        {
-            protected synchronized Object initialValue()
-            {
-                return newBuffer( _headerBufferSize );
-            }
-        };
-        
-        tlRequest = new ThreadLocal()
-        {
-            protected synchronized Object initialValue()
-            {
-                return newBuffer( _requestBufferSize );
-            }
-        };
-        
-        tlResponse = new ThreadLocal()
-        {
-            protected synchronized Object initialValue()
-            {
-                return newBuffer( _responseBufferSize );
-            }
-        };
-        
         super.doStart();
+        if (_headerBufferSize==_requestBufferSize && _headerBufferSize==_responseBufferSize)
+        {
+            _pool[__HEADER]+=_pool[__REQUEST]+_pool[__RESPONSE];
+            _pool[__REQUEST]=0;
+            _pool[__RESPONSE]=0;
+        }
+        else if (_headerBufferSize==_requestBufferSize)
+        {
+            _pool[__HEADER]+=_pool[__REQUEST];
+            _pool[__REQUEST]=0;
+        }
+        else if (_headerBufferSize==_responseBufferSize)
+        {
+            _pool[__HEADER]+=_pool[__RESPONSE];
+            _pool[__RESPONSE]=0;
+        }
+        else if (_requestBufferSize==_responseBufferSize)
+        {
+            _pool[__RESPONSE]+=_pool[__REQUEST];
+            _pool[__REQUEST]=0;
+        }
+        
     }
 
     public abstract Buffer newBuffer( int size );
 
+    protected static class ThreadBuffers
+    {
+        final Buffer[][] _buffers;
+        ThreadBuffers(int headers,int requests,int responses,int others)
+        {
+            _buffers = new Buffer[4][];
+            _buffers[__HEADER]=new Buffer[headers];
+            _buffers[__REQUEST]=new Buffer[requests];
+            _buffers[__RESPONSE]=new Buffer[responses];
+            _buffers[__OTHER]=new Buffer[others];
+            
+        }
+    }
 }
