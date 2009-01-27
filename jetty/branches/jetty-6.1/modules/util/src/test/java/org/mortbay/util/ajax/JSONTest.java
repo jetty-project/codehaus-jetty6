266942ffd727b3a8dc5b9a21ac56a90d94617dd0
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-01-27 16:25:08 +0000

    double guessing myself now, think we might be allowing 1.5 code in test cases?

--- jetty/branches/jetty-6.1/modules/util/src/test/java/org/mortbay/util/ajax/JSONTest.java
+++ jetty/branches/jetty-6.1/modules/util/src/test/java/org/mortbay/util/ajax/JSONTest.java
@@ -183,7 +183,7 @@ public class JSONTest extends TestCase
     {
         Object obj = JSON.parse("1.0E7");
         assertTrue(obj instanceof Double);
-        BigDecimal bd = new BigDecimal(10000000d);
+        BigDecimal bd = BigDecimal.valueOf(10000000d);
         String string = JSON.toString(new Object[]{bd}); 
         obj = Array.get(JSON.parse(string),0);
         assertTrue(obj instanceof Double);
