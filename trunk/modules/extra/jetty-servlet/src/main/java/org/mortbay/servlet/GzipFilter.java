8a3d9949e7f715dd91295ec06e03c41c35f4faee
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-02-10 00:11:46 +0000

    [JETTY-897] Remove swing dependency in GzipFilter

--- trunk/modules/extra/jetty-servlet/src/main/java/org/mortbay/servlet/GzipFilter.java
+++ trunk/modules/extra/jetty-servlet/src/main/java/org/mortbay/servlet/GzipFilter.java
@@ -22,7 +22,6 @@ import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.zip.GZIPOutputStream;
 
-import javax.servlet.AsyncContext;
 import javax.servlet.AsyncEvent;
 import javax.servlet.AsyncListener;
 import javax.servlet.FilterChain;
@@ -31,11 +30,9 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.ServletResponseWrapper;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
-import javax.swing.text.WrappedPlainView;
 
 import org.mortbay.util.ByteArrayOutputStream2;
 import org.mortbay.util.StringUtil;
