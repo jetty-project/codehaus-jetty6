35237ea55bc2b3381c9291bdadddeec3b43ec462
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-02-12 19:02:10 +0000

    [JETTY-883] conversion of jetty-start

--- jetty/trunk/modules/server/jetty-start/src/main/java/org/mortbay/jetty/start/Version.java
+++ jetty/trunk/modules/server/jetty-start/src/main/java/org/mortbay/jetty/start/Version.java
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // ========================================================================
-package org.mortbay.start;
+package org.mortbay.jetty.start;
 
 /**
  * Utility class for parsing and comparing version strings.
