02fbf940e662e794c059b712f316d0f067cee59f
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-02-13 09:50:40 +0000

    JETTY-873

--- modules/jetty/src/test/java/org/mortbay/jetty/HttpParserTest.java
+++ modules/jetty/src/test/java/org/mortbay/jetty/HttpParserTest.java
@@ -151,6 +151,24 @@ public class HttpParserTest extends TestCase
         assertEquals(-1, h);
     }
 
+    public void testConnect()
+        throws Exception
+    {
+        StringEndPoint io=new StringEndPoint();
+        io.setInput("CONNECT 192.168.1.2:80 HTTP/1.1\015\012" + "\015\012");
+        ByteArrayBuffer buffer= new ByteArrayBuffer(4096);
+        SimpleBuffers buffers=new SimpleBuffers(new Buffer[]{buffer});
+
+        Handler handler = new Handler();
+        HttpParser parser= new HttpParser(buffers,io, handler, buffer.capacity(), 0);
+        parser.parse();
+        assertTrue(handler.request);
+        assertEquals("CONNECT", f0);
+        assertEquals("192.168.1.2:80", f1);
+        assertEquals("HTTP/1.1", f2);
+        assertEquals(-1, h);
+    }
+    
     public void testHeaderParse()
 	throws Exception
     {
@@ -456,6 +474,7 @@ public class HttpParserTest extends TestCase
 
     class Handler extends HttpParser.EventHandler
     {   
+        boolean request;
         HttpFields fields;
         
         public void content(Buffer ref)
@@ -470,6 +489,7 @@ public class HttpParserTest extends TestCase
         {
             try
             {
+                request=true;
                 h= -1;
                 hdr= new String[9];
                 val= new String[9];
@@ -522,6 +542,7 @@ public class HttpParserTest extends TestCase
 
         public void startResponse(Buffer version, int status, Buffer reason)
         {
+            request=false;
             f0 = version.toString();
 	    f1 = Integer.toString(status);
 	    f2 = reason.toString();
