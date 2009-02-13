8c236c07092cfb05c14a879fee088d22e8366e2f
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-02-13 18:23:50 +0000

    adjust dep

--- dep-list.sh
+++ dep-list.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-mvn dependency:list | grep -E "\[INFO\]    [a-z]" | sed -e "s/\[INFO\]    //;/^none$/d;/task-segment:/d;/org.mortbay.jetty:jetty/d" | sort -u > EXTERNAL-DEPENDENCIES.txt
+mvn dependency:list | grep -E "\[INFO\]    [a-z]" | sed -e "s/\[INFO\]    //;/^none$/d;/task-segment:/d;/org.mortbay.jetty:jetty/d;s/:compile//;s/:provided//;s/:test//" | sort -u > EXTERNAL-DEPENDENCIES.txt
 
 svn commit -m "dependency list update" EXTERNAL-DEPENDENCIES.txt
