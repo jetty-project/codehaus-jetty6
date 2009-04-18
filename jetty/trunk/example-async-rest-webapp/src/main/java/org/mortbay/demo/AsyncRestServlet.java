a484c484fcc604c0367886d344605a329e9f623f
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-04-18 05:08:36 +0000

    nicer UI

--- jetty/trunk/example-async-rest-webapp/src/main/java/org/mortbay/demo/AsyncRestServlet.java
+++ jetty/trunk/example-async-rest-webapp/src/main/java/org/mortbay/demo/AsyncRestServlet.java
@@ -16,25 +16,30 @@ package org.mortbay.demo;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import javax.servlet.AsyncContext;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.mortbay.jetty.client.ContentExchange;
-import org.mortbay.jetty.client.HttpClient;
-import org.mortbay.jetty.util.ajax.JSON;
+import org.eclipse.jetty.client.ContentExchange;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.continuation.Continuation;
+import org.eclipse.jetty.continuation.ContinuationSupport;
+import org.eclipse.jetty.util.ajax.JSON;
 
 /**
  * Servlet implementation class AsyncRESTServlet.
@@ -46,27 +51,18 @@ import org.mortbay.jetty.util.ajax.JSON;
  * <dt>items</dt><dd>The keyword to search for</dd>
  * </dl>
  */
-public class AsyncRestServlet extends HttpServlet
+public class AsyncRestServlet extends AbstractRestServlet
 {
-    private final static String __DEFAULT_APPID = "Webtide81-adf4-4f0a-ad58-d91e41bbe85";
-
-    final static String ITEMS_PARAM = "items";
-    final static String APPID_PARAM = "appid";
-
-    final static String CLIENT_ATTR = "org.mortbay.demo.client";
+    final static String RESULTS_ATTR = "org.mortbay.demo.client";
     final static String DURATION_ATTR = "org.mortbay.demo.duration";
     final static String START_ATTR = "org.mortbay.demo.start";
 
-    private HttpClient _client;
-    private String _appid;
+    HttpClient _client;
 
     public void init(ServletConfig servletConfig) throws ServletException
     {
-        if (servletConfig.getInitParameter(APPID_PARAM) == null)
-            _appid = __DEFAULT_APPID;
-        else
-            _appid = servletConfig.getInitParameter(APPID_PARAM);
-
+        super.init(servletConfig);
+        
         _client = new HttpClient();
         _client.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL);
 
@@ -82,92 +78,138 @@ public class AsyncRestServlet extends HttpServlet
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        Long start=System.currentTimeMillis();
-
-        if (!request.isAsyncStarted() || request.getAttribute(CLIENT_ATTR)==null)
+        Long start=System.nanoTime();
+        
+        // resumed request: either we got all the results, or we timed out
+        Queue<Map<String, String>> results = (Queue<Map<String, String>>) request.getAttribute(RESULTS_ATTR);
+        
+        if (results==null)
         {
-            // extract keywords to search for
-            String[] keywords=request.getParameter(ITEMS_PARAM).split(",");
-            
             // define results data structures
-            final List<Map<String, String>> results = 
-                Collections.synchronizedList(new ArrayList<Map<String, String>>());
-            final AtomicInteger count=new AtomicInteger(keywords.length);
+            results = new ConcurrentLinkedQueue<Map<String,String>>();
+            request.setAttribute(RESULTS_ATTR, results);
             
             // suspend the request
-            final AsyncContext asyncContext = request.startAsync();
-            request.setAttribute(CLIENT_ATTR, results);
+            // This is done before scheduling async handling to avoid race of 
+            // resume before suspend!
+            final Continuation continuation = ContinuationSupport.getContinuation(request);
+            continuation.setTimeout(10000);
+            continuation.suspend();
 
-            // For each keyword
+            // extract keywords to search for
+            String[] keywords=request.getParameter(ITEMS_PARAM).split(",");
+            final AtomicInteger count=new AtomicInteger(keywords.length);
+            
+            // Send request each keyword
             for (final String item:keywords)
             {
-                // create an asynchronous HTTP exchange
-                ContentExchange exchange = new ContentExchange()
-                {
-                    protected void onResponseComplete() throws IOException
-                    {
-                        // extract auctions from the results
-                        Map query = (Map) JSON.parse(this.getResponseContent());
-                        Object[] auctions = (Object[]) query.get("Item");
-                        if (auctions != null)
-                        {
-                            for (Object o : auctions)
-                                results.add((Map) o);
-                        }
-
-                        // if that was all, resume the request
-                        if (count.decrementAndGet()<=0)
-                        	asyncContext.dispatch();
-                    }
-                };
-
-                // send the exchange
-                exchange.setMethod("GET");
-                exchange.setURL("http://open.api.ebay.com/shopping?MaxEntries=5&appid=" + 
-                        _appid + 
-                        "&version=573&siteid=0&callname=FindItems&responseencoding=JSON&QueryKeywords=" + 
-                        URLEncoder.encode(item,"UTF-8"));
-                _client.send(exchange);
-                
+                sendAsyncRestRequest(item,results,count,continuation/*,debug*/);
             }
-
+            
             // save timing info and return
             request.setAttribute(START_ATTR, start);
-            request.setAttribute(DURATION_ATTR, new Long(System.currentTimeMillis() - start));
+            request.setAttribute(DURATION_ATTR, new Long(System.nanoTime() - start));
+
             return;
         }
 
-        // resumed request: either we got all the results, or we timed out
-        List<Map<String, String>> results = (List<Map<String, String>>) request.getAttribute(CLIENT_ATTR);
+        // Generate the response
+        String thumbs = generateThumbs(results);
+        
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<html><head><style type='text/css'>img:hover {height:75px}</style></head><body><small>");
+        out.println("<html><head>");
+        out.println(STYLE);
+        out.println("</head><body><small>");
 
-        for (Map<String, String> m : results)
-        {
-            out.print("<a href=\""+m.get("ViewItemURLForNaturalSearch")+"\">");
-            if (m.containsKey("GalleryURL"))
-                out.print("<img border='1px' height='20px' src=\""+m.get("GalleryURL")+"\"/>&nbsp;");
-
-            out.print(m.get("Title"));
-            out.println("</a><br/>");
-        }
-
-        out.println("<hr />");
-        long duration = (Long) request.getAttribute(DURATION_ATTR);
+        long initial = (Long) request.getAttribute(DURATION_ATTR);
         long start0 = (Long) request.getAttribute(START_ATTR);
 
-        long now = System.currentTimeMillis();
-        out.print("Total Time: ");
-        out.print(now - start0);
-        out.println("ms<br/>");
-        out.print("Thread held: ");
+        long now = System.nanoTime();
+        long total=now-start0;
+        long generate=now-start;
+        long thread=initial+generate;
+        
+        out.print("<b>Asynchronous: "+request.getParameter(ITEMS_PARAM)+"</b><br/>");
+        out.print("Total Time: "+ms(total)+"ms<br/>");
 
-        long duration2 = now - start;
-        out.print(duration + duration2);
-        out.println("ms (" + duration + " initial + " + duration2 + " resume )");
-        out.println("</small></body></html>");
+        out.print("Thread held (<span class='red'>red</span>): "+ms(thread)+"ms (" + ms(initial) + " initial + " + ms(generate) + " generate )<br/>");
+        out.print("Async wait (<span class='green'>green</span>): "+ms(total-thread)+"ms<br/>");
+        
+        out.println("<img border='0px' src='red.png'   height='20px' width='"+width(initial)+"px'>"+
+                    "<img border='0px' src='green.png' height='20px' width='"+width(total-thread)+"px'>"+
+                    "<img border='0px' src='red.png'   height='20px' width='"+width(generate)+"px'>");
+        
+        out.println("<hr />");
+        out.println(thumbs);
+        out.println("</small>");
+        out.println("</body></html>");
         out.close();
+    }
+
+    private void sendAsyncRestRequest(
+            final String item, 
+            final Queue<Map<String, String>> results, 
+            final AtomicInteger count, 
+            final Continuation continuation /*,
+            final StringBuffer debug*/)
+        throws IOException
+    {
+        // create an asynchronous HTTP exchange
+        ContentExchange exchange = new ContentExchange()
+        {
+            protected void onResponseComplete() throws IOException
+            {
+                // extract auctions from the results
+                Map query = (Map) JSON.parse(this.getResponseContent());
+                Object[] auctions = (Object[]) query.get("Item");
+                if (auctions != null)
+                {
+                    for (Object o : auctions)
+                        results.add((Map) o);
+                }
+
+                doCount();
+            }
+
+            /* ------------------------------------------------------------ */
+            protected void onConnectionFailed(Throwable ex)
+            {
+                doCount();
+            }
+
+            /* ------------------------------------------------------------ */
+            protected void onException(Throwable ex)
+            {
+                doCount();
+            }
+
+            /* ------------------------------------------------------------ */
+            protected void onExpire()
+            {
+                doCount();
+            }
+            
+            /* ------------------------------------------------------------ */
+            private void doCount()
+            {
+                if (count.decrementAndGet()<=0)
+                {
+                    // debug.append("got & resume "+item+"\n");
+                    continuation.resume();
+                }
+                else
+                {
+                    // debug.append("got "+item+"\n");
+                }
+            }
+            
+        };
+
+        // send the exchange
+        exchange.setMethod("GET");
+        exchange.setURL(restURL(item));
+        _client.send(exchange);        
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
