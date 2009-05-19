89fee9e1b34a0aae79253a2c01d7847b61304f89
ayao <ayao@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-05-19 07:28:22 +0000

    JETTY-1018 missing file

---
+++ modules/jetty/src/main/java/org/mortbay/servlet/jetty/IncludableDoSFilter.java
@@ -0,0 +1,47 @@
+// ========================================================================
+// Copyright 2009 Mort Bay Consulting Pty. Ltd.
+// ------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//========================================================================
+
+package org.mortbay.servlet.jetty;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mortbay.jetty.HttpConnection;
+import org.mortbay.jetty.Request;
+import org.mortbay.log.Log;
+import org.mortbay.servlet.DoSFilter;
+
+/* ------------------------------------------------------------ */
+/** Includable DoS Filter.
+ * This is an extension to the {@link DoSFilter} that uses Jetty APIs to allow
+ * connections to be closed cleanly. 
+ */
+
+public class IncludableDoSFilter extends DoSFilter
+{
+    protected void closeConnection(HttpServletRequest request, HttpServletResponse response, Thread thread)
+    {
+        try
+        {
+            Request base_request=(request instanceof Request)?(Request)request:HttpConnection.getCurrentConnection().getRequest();
+            base_request.getConnection().getEndPoint().close();
+        }
+        catch(IOException e)
+        {
+            Log.warn(e);
+        }
+    }
+}
