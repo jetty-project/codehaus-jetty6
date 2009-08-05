9c1029446f60b282703b3fade7096c5e8f3f52fb
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-08-05 01:26:30 +0000

    fileserver example

--- jetty/trunk/example-jetty-spring/src/main/java/org/mortbay/jetty/spring/FileServer.java
+++ jetty/trunk/example-jetty-spring/src/main/java/org/mortbay/jetty/spring/FileServer.java
@@ -7,15 +7,14 @@ import org.springframework.core.io.UrlResource;
 
 
 /* ------------------------------------------------------------ */
-/** Run Jetty from Spring configuration.
- * @see http://svn.codehaus.org/jetty/jetty/trunk/example-jetty-spring/src/main/resources/jetty-spring.xml
+/** Run Jetty File server from Spring configuration.
  * @see http://svn.codehaus.org/jetty/jetty/trunk/example-jetty-spring/src/main/resources/fileserver-spring.xml
  */
-public class Main
+public class FileServer
 {
     public static void main(String[] args) throws Exception
     {
-        Resource config = Resource.newSystemResource(args.length == 1?args[0]:"jetty-spring.xml");
+        Resource config = Resource.newSystemResource(args.length == 1?args[0]:"fileserver-spring.xml");
         XmlBeanFactory bf = new XmlBeanFactory(new UrlResource(config.getURL()));
         Server server = (Server)bf.getBean(args.length == 2?args[1]:"Server");
         server.join();
