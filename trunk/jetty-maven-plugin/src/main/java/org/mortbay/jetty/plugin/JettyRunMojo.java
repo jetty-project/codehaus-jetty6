f6862030f2cf41fa61ec4438bcd719b95e2136c1
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-08-06 12:32:49 +0000

    use the resource collection mechanism for WEB-INF extraction

--- trunk/jetty-maven-plugin/src/main/java/org/mortbay/jetty/plugin/JettyRunMojo.java
+++ trunk/jetty-maven-plugin/src/main/java/org/mortbay/jetty/plugin/JettyRunMojo.java
@@ -454,18 +454,7 @@ public class JettyRunMojo extends AbstractJettyMojo
             {
                 try
                 {
-                    // TODO temp solution until after RC2. Extract overlays
-                    File extract=new File(new File(webAppConfig.getTempDirectory(),"overlays"),artifact.getGroupId()+"-"+artifact.getArtifactId()+"-"+artifact.getVersion());
-                    if (extract.exists() && artifact.isSnapshot())
-                        extract.delete();
-                    if (!extract.exists() || artifact.isSnapshot())
-                    {
-                        extract.mkdirs();
-                        Resource r = Resource.newResource("jar:"+artifact.getFile().toURL().toString()+"!/");
-                        Log.info("Extract "+r+" to "+extract);
-                        JarResource.extract(r,extract,false);
-                    }
-                    Resource r=Resource.newResource(extract.toURL());
+                    Resource r=Resource.newResource("jar:"+artifact.getFile().toURL().toString()+"!/");
                     overlays.add(r);
                     getExtraScanTargets().add(artifact.getFile());
                 }
