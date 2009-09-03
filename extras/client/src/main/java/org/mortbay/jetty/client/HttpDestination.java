2527ae2a766cea77ee1c100610e2fecb79cc7148
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-09-03 19:27:26 +0000

    port a couple of issues from jetty7 client

--- extras/client/src/main/java/org/mortbay/jetty/client/HttpDestination.java
+++ extras/client/src/main/java/org/mortbay/jetty/client/HttpDestination.java
@@ -424,6 +424,7 @@ public class HttpDestination
     public void resend(HttpExchange ex) throws IOException
     {
         ex.getEventListener().onRetry();
+        ex.reset();
         doSend(ex);
     }
 
