2882eb480125a44c9193abc61db7546b40ae7ba8
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-10-01 00:22:56 +0000

    updated z branch

--- modules/util/src/main/java/org/mortbay/servlet/GzipFilter.java
+++ modules/util/src/main/java/org/mortbay/servlet/GzipFilter.java
@@ -42,7 +42,7 @@ import org.mortbay.util.StringUtil;
  * <li>The filter is mapped to a matching path</li>
  * <li>The response status code is >=200 and <300
  * <li>The content length is unknown or more than the <code>minGzipSize</code> initParameter or the minGzipSize is 0(default)</li>
- * <li>The content-type is in the coma separated list of mimeTypes set in the <code>mimeTypes</code> initParameter or
+ * <li>The content-type is in the comma separated list of mimeTypes set in the <code>mimeTypes</code> initParameter or
  * if no mimeTypes are defined the content-type is not "application/gzip"</li>
  * <li>No content-encoding is specified by the resource</li>
  * </ul>
@@ -110,7 +110,9 @@ public class GzipFilter extends UserAgentFilter
         HttpServletResponse response=(HttpServletResponse)res;
 
         String ae = request.getHeader("accept-encoding");
-        if (ae != null && ae.indexOf("gzip")>=0 && !response.containsHeader("Content-Encoding"))
+        Boolean gzip=(Boolean)request.getAttribute("GzipFilter");
+        if (ae != null && ae.indexOf("gzip")>=0 && !response.containsHeader("Content-Encoding") &&
+            (gzip==null || gzip.booleanValue()) && !"HEAD".equalsIgnoreCase(request.getMethod()))
         {
             if (_excluded!=null)
             {
@@ -177,13 +179,17 @@ public class GzipFilter extends UserAgentFilter
         public void setContentType(String ct)
         {
             super.setContentType(ct);
-            int colon=ct.indexOf(";");
-            if (colon>0)
-                ct=ct.substring(0,colon);
+
+            if (ct!=null)
+            {
+                int colon=ct.indexOf(";");
+                if (colon>0)
+                    ct=ct.substring(0,colon);
+            }
 
             if ((_gzStream==null || _gzStream._out==null) && 
                 (_mimeTypes==null && "application/gzip".equalsIgnoreCase(ct) ||
-                 _mimeTypes!=null && !_mimeTypes.contains(StringUtil.asciiToLowerCase(ct))))
+                 _mimeTypes!=null && (ct==null||!_mimeTypes.contains(StringUtil.asciiToLowerCase(ct)))))
             {
                 noGzip();
             }
