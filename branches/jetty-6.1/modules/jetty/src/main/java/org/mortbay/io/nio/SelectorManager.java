ee4123c5cd933d987c61f439c5794f88b20602b7
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-10-16 18:21:34 +0000

    adjusted test parameters

--- branches/jetty-6.1/modules/jetty/src/main/java/org/mortbay/io/nio/SelectorManager.java
+++ branches/jetty-6.1/modules/jetty/src/main/java/org/mortbay/io/nio/SelectorManager.java
@@ -44,7 +44,7 @@ public abstract class SelectorManager extends AbstractLifeCycle
 {
     private static final int __JVMBUG_THRESHHOLD=Integer.getInteger("org.mortbay.io.nio.JVMBUG_THRESHHOLD",512).intValue();
     private static final int __MONITOR_PERIOD=Integer.getInteger("org.mortbay.io.nio.MONITOR_PERIOD",1000).intValue();
-    private static final int __MAX_SELECTS=Integer.getInteger("org.mortbay.io.nio.MAX_SELECTS",15000).intValue();
+    private static final int __MAX_SELECTS=Integer.getInteger("org.mortbay.io.nio.MAX_SELECTS",25000).intValue();
     private static final int __BUSY_PAUSE=Integer.getInteger("org.mortbay.io.nio.BUSY_PAUSE",50).intValue();
     
     private boolean _delaySelectKeyUpdate=true;
