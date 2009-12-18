51b017578333917a655800db8991aabd06663dc0
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2009-12-18 02:58:17 +0000

    cleanup svn

--- jetty/trunk-migrated-to-eclipse/dep-list.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-mvn dependency:list | grep -E "\[INFO\]    [a-z]" | sed -e "s/\[INFO\]    //;/^none$/d;/task-segment:/d;/org.mortbay.jetty:jetty/d;s/:compile//;s/:provided//;s/:test//" | sort -u > EXTERNAL-DEPENDENCIES.txt
-
-svn commit -m "dependency list update" EXTERNAL-DEPENDENCIES.txt
