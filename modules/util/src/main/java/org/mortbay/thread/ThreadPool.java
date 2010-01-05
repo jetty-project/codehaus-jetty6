d59c0c3ca804cbb58a23fd4a02cc4fa2f25819b7
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2010-01-05 05:37:55 +0000

    undo wrong commit for COMETD-62 Delay add listeners until after client construction

--- modules/util/src/main/java/org/mortbay/thread/ThreadPool.java
+++ modules/util/src/main/java/org/mortbay/thread/ThreadPool.java
@@ -15,6 +15,8 @@
 
 package org.mortbay.thread;
 
+import org.mortbay.component.LifeCycle;
+
 /* ------------------------------------------------------------ */
 /** ThreadPool.
  * @author gregw
@@ -27,7 +29,7 @@ public interface ThreadPool
 
     /* ------------------------------------------------------------ */
     /**
-     * Blocks until the thread pool is {@link org.mortbay.component.LifeCycle#stop stopped}.
+     * Blocks until the thread pool is {@link LifeCycle#stop stopped}.
      */
     public void join() throws InterruptedException;
 
