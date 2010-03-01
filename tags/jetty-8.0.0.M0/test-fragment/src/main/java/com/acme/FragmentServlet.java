a3d2ea861c6e46d98b7c177dbbcde5e1117ff911
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2010-03-01 19:51:03 +0000

    [maven-release-plugin]  copy for tag jetty-8.0.0.M0

---
+++ tags/jetty-8.0.0.M0/test-fragment/src/main/java/com/acme/FragmentServlet.java
@@ -0,0 +1,79 @@
+//========================================================================
+//Copyright 2010 Mort Bay Consulting Pty. Ltd.
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+
+/**
+ * 
+ */
+package com.acme;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ */
+
+public class FragmentServlet extends HttpServlet 
+{
+    private ServletConfig config;
+    
+    
+    public void init(ServletConfig config) throws ServletException
+    {
+        super.init(config);
+        this.config = config;
+    }
+
+    
+    
+    /* ------------------------------------------------------------ */
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        doGet(request, response);
+    }
+
+    /* ------------------------------------------------------------ */
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {      
+        try
+        {
+            response.setContentType("text/html");
+            ServletOutputStream out = response.getOutputStream();
+            out.println("<html>");
+            out.println("<h1>Jetty Fragment Servlet</h1>");
+            out.println("<body>");
+            out.println("</body>");            
+            out.println("</html>");
+            out.flush();
+        }
+        catch (Exception e)
+        {
+            throw new ServletException(e);
+        }
+    }
+    
+
+  
+   
+}
