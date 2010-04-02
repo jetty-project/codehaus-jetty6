84c1c002cb28801857877b896a3f7cdf0681bc8c
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2010-04-02 11:12:49 +0000

    JETTY-1202 Use platfrom default algorithm for SecureRandom

--- branches/jetty-6.1/modules/jetty/src/main/java/org/mortbay/jetty/servlet/AbstractSessionIdManager.java
+++ branches/jetty-6.1/modules/jetty/src/main/java/org/mortbay/jetty/servlet/AbstractSessionIdManager.java
@@ -14,7 +14,6 @@
 
 package org.mortbay.jetty.servlet;
 
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -28,8 +27,6 @@ import org.mortbay.log.Log;
 public abstract class AbstractSessionIdManager extends AbstractLifeCycle implements SessionIdManager
 {
     private final static String __NEW_SESSION_ID="org.mortbay.jetty.newSessionId";  
-    protected final static String SESSION_ID_RANDOM_ALGORITHM = "SHA1PRNG";
-    protected final static String SESSION_ID_RANDOM_ALGORITHM_ALT = "IBMSecureRandom";
     
     protected Random _random;
     protected boolean _weakRandom;
@@ -100,19 +97,23 @@ public abstract class AbstractSessionIdManager extends AbstractLifeCycle impleme
             String id=null;
             while (id==null||id.length()==0||idInUse(id))
             {
-                long r=_weakRandom
+                long r0=_weakRandom
                 ?(hashCode()^Runtime.getRuntime().freeMemory()^_random.nextInt()^(((long)request.hashCode())<<32))
                 :_random.nextLong();
-                r^=created;
-                if (request!=null && request.getRemoteAddr()!=null)
-                    r^=request.getRemoteAddr().hashCode();
-                if (r<0)
-                    r=-r;
-                id=Long.toString(r,36);
+                if (r0<0)
+                    r0=-r0;
+                long r1=_weakRandom
+                ?(hashCode()^Runtime.getRuntime().freeMemory()^_random.nextInt()^(((long)request.hashCode())<<32))
+                :_random.nextLong();
+                if (r1<0)
+                    r1=-r1;
+                id=Long.toString(r0,36)+Long.toString(r1,36);
                 
                 //add in the id of the node to ensure unique id across cluster
                 //NOTE this is different to the node suffix which denotes which node the request was received on
-                id=_workerName + id;
+                if (_workerName!=null)
+                    id=_workerName + id;
+                
             }
 
             request.setAttribute(__NEW_SESSION_ID,id);
@@ -120,15 +121,11 @@ public abstract class AbstractSessionIdManager extends AbstractLifeCycle impleme
         }
     }
 
-    
     public void doStart()
     {
        initRandom();
     }
 
-    
-    
-    
     /**
      * Set up a random number generator for the sessionids.
      * 
@@ -140,21 +137,14 @@ public abstract class AbstractSessionIdManager extends AbstractLifeCycle impleme
         {
             try
             {
-                _random=SecureRandom.getInstance(SESSION_ID_RANDOM_ALGORITHM);
+                _random=new SecureRandom();
+                _weakRandom=false;
             }
-            catch (NoSuchAlgorithmException e)
+            catch (Exception e)
             {
-                try
-                {
-                    _random=SecureRandom.getInstance(SESSION_ID_RANDOM_ALGORITHM_ALT);
-                    _weakRandom=false;
-                }
-                catch (NoSuchAlgorithmException e_alt)
-                {
-                    Log.warn("Could not generate SecureRandom for session-id randomness",e);
-                    _random=new Random();
-                    _weakRandom=true;
-                }
+                Log.warn("Could not generate SecureRandom for session-id randomness",e);
+                _random=new Random();
+                _weakRandom=true;
             }
         }
         _random.setSeed(_random.nextLong()^System.currentTimeMillis()^hashCode()^Runtime.getRuntime().freeMemory()); 
