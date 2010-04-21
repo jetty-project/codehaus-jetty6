84031cd4a9adf05a56836894d5bf70c697981c6c
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2010-04-21 17:32:59 +0000

    release 6.1.24

---
+++ examples/test-webapp/src/main/java/com/acme/TestFilter.java
@@ -0,0 +1,113 @@
+//========================================================================
+//$Id: TestFilter.java,v 1.5 2005/11/01 11:42:53 gregwilkins Exp $
+//Copyright 2004-2005 Mort Bay Consulting Pty. Ltd.
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+
+package com.acme;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletRequestWrapper;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+
+/* ------------------------------------------------------------ */
+/** TestFilter.
+ * @author gregw
+ *
+ */
+public class TestFilter implements Filter
+{
+    private boolean _remote;
+    private ServletContext _context;
+    private final Set _allowed = new HashSet();
+    
+    /* ------------------------------------------------------------ */
+    /* 
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+        _context= filterConfig.getServletContext();
+        _remote=Boolean.parseBoolean(_context.getInitParameter("remote"));
+        _allowed.add("/favicon.ico");
+        _allowed.add("/jetty_banner.gif");
+        _allowed.add("/remote.html");
+    }
+
+    /* ------------------------------------------------------------ */
+    /* 
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException
+    {
+        String from = request.getRemoteHost();
+        String to = request.getServerName();
+
+        if ((!_remote&&!from.equals("localhost")&&!from.startsWith("127.0.0.")||
+             !to.equals("localhost")&&!to.startsWith("127.0.0.")) &&
+             !_allowed.contains(((HttpServletRequest)request).getServletPath()))
+        {
+            ((HttpServletResponse)response).sendRedirect("/remote.html");
+            return;
+        }
+
+        Integer old_value=null;
+        ServletRequest r = request;
+        while (r instanceof ServletRequestWrapper)
+            r=((ServletRequestWrapper)r).getRequest();
+        
+        try
+        {
+            old_value=(Integer)request.getAttribute("testFilter");
+            
+            Integer value=(old_value==null)?new Integer(1):new Integer(old_value.intValue()+1);
+                        
+            request.setAttribute("testFilter", value);
+            
+            String qString = ((HttpServletRequest)request).getQueryString();
+            if (qString != null && qString.indexOf("wrap")>0)
+            {
+                request=new HttpServletRequestWrapper((HttpServletRequest)request);
+            }
+            _context.setAttribute("request"+r.hashCode(),value);
+            
+            chain.doFilter(request, response);
+        }
+        finally
+        {
+            request.setAttribute("testFilter", old_value);
+            _context.setAttribute("request"+r.hashCode(),old_value);
+        }
+    }
+
+    /* ------------------------------------------------------------ */
+    /* 
+     * @see javax.servlet.Filter#destroy()
+     */
+    public void destroy()
+    {
+    }
+
+}
