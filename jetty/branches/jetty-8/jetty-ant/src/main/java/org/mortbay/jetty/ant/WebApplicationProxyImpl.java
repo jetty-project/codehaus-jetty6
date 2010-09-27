9f71e5e4632e77b4b91170d54494ac48fd7a78bc
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2010-09-27 07:01:32 +0000

    Merge with jetty-7.2.0 via tag jetty-7-to-jetty-8-base-20100927-1; remove whitespace from some srcs; update poms that aren't part of the main build to the latest version number.

--- jetty/branches/jetty-8/jetty-ant/src/main/java/org/mortbay/jetty/ant/WebApplicationProxyImpl.java
+++ jetty/branches/jetty-8/jetty-ant/src/main/java/org/mortbay/jetty/ant/WebApplicationProxyImpl.java
@@ -31,6 +31,8 @@ import org.eclipse.jetty.webapp.JettyWebXmlConfiguration;
 import org.eclipse.jetty.webapp.TagLibConfiguration;
 import org.eclipse.jetty.webapp.WebAppClassLoader;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.FragmentConfiguration;
+import org.eclipse.jetty.webapp.MetaInfConfiguration;
 import org.eclipse.jetty.webapp.WebInfConfiguration;
 
 /**
@@ -89,10 +91,14 @@ public class WebApplicationProxyImpl implements WebApplicationProxy
     {
         this.name = name;
         TaskLog.log("\nConfiguring Jetty for web application: " + name);
-
-				this.configurations = new Configuration[] { new WebInfConfiguration(),
-		            new EnvConfiguration(), new JettyWebAppConfiguration(), new JettyWebXmlConfiguration(),
-		            new TagLibConfiguration() };
+         this.configurations = new Configuration[] 
+                                                 { new WebInfConfiguration(),
+                                                   new JettyWebAppConfiguration(), 
+                                                   new MetaInfConfiguration(),
+                                                   new FragmentConfiguration(),
+                                                   new JettyWebXmlConfiguration(), 
+                                                   new EnvConfiguration(), 
+                                                   new org.eclipse.jetty.plus.webapp.PlusConfiguration()};
     }
 
     public List getClassPathFiles()
--- jetty/branches/jetty-8/jetty-maven-plugin/src/main/java/org/mortbay/jetty/plugin/JettyWebAppContext.java
+++ jetty/branches/jetty-8/jetty-maven-plugin/src/main/java/org/mortbay/jetty/plugin/JettyWebAppContext.java
@@ -16,11 +16,22 @@
 package org.mortbay.jetty.plugin;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.annotations.ContainerInitializerConfiguration;
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.util.URIUtil;
+import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.FragmentConfiguration;
@@ -41,6 +52,9 @@ import org.eclipse.jetty.webapp.WebXmlConfiguration;
  */
 public class JettyWebAppContext extends WebAppContext
 {
+    private static final String WEB_INF_CLASSES_PREFIX = "/WEB-INF/classes";
+    private static final String WEB_INF_LIB_PREFIX = "/WEB-INF/lib";
+
     private List<File> classpathFiles;
     private String jettyEnvXml;
     private MavenWebInfConfiguration webInfConfig = new MavenWebInfConfiguration();
@@ -57,6 +71,9 @@ public class JettyWebAppContext extends WebAppContext
     private boolean unpackOverlays;
     private String containerIncludeJarPattern = ".*/servlet-api-[^/]*\\.jar$";
     
+    private List<File> webInfClasses = new ArrayList<File>();
+    private List<File> webInfJars = new ArrayList<File>();
+    private Map<String, File> webInfJarMap = new HashMap<String, File>();
 
 
     public JettyWebAppContext ()
@@ -115,12 +132,43 @@ public class JettyWebAppContext extends WebAppContext
         return this.jettyEnvXml;
     }
 
+   
+    public void setWebInfClasses(List<File> dirs)
+    {
+        webInfClasses.addAll(dirs);
+    }
+    
+    public List<File> getWebInfClasses()
+    {
+        return webInfClasses;
+    }
+    
+    public void setWebInfLib (List<File> jars)
+    {
+        webInfJars.addAll(jars);
+    }
+
+    public List<File> getWebInfLib()
+    {
+        return webInfJars;
+    }
 
     public void doStart () throws Exception
     {
         setConfigurations(configs);
         setAttribute(WebInfConfiguration.CONTAINER_JAR_PATTERN, containerIncludeJarPattern);
         
+
+        // Initialize map containing all jars in /WEB-INF/lib
+        webInfJarMap.clear();
+        for (File file : webInfJars)
+        {
+            // Return all jar files from class path
+            String fileName = file.getName();
+            if (fileName.endsWith(".jar"))
+                webInfJarMap.put(fileName, file);
+        }
+
         if (this.jettyEnvXml != null)
             envConfig.setJettyEnvXml(new File(this.jettyEnvXml).toURL());
         setShutdown(false);
@@ -133,5 +181,97 @@ public class JettyWebAppContext extends WebAppContext
         //just wait a little while to ensure no requests are still being processed
         Thread.currentThread().sleep(500L);
         super.doStop();
+    }
+
+    @Override
+    public Resource getResource(String uriInContext) throws MalformedURLException
+    {
+        Resource resource = null;
+        // Try to get regular resource
+        resource = super.getResource(uriInContext);
+
+        // If no regular resource exists check for access to /WEB-INF/lib or /WEB-INF/classes
+        if ((resource == null || !resource.exists()) && uriInContext != null && webInfClasses != null)
+        {
+            String uri = URIUtil.canonicalPath(uriInContext);
+
+            try
+            {
+                // Replace /WEB-INF/classes with real classes directory
+                if (uri.startsWith(WEB_INF_CLASSES_PREFIX))
+                {
+                    Resource res = null;
+                    int i=0;
+                    while (res == null && (i < webInfClasses.size()))
+                    {
+                        String newPath = uri.replace(WEB_INF_CLASSES_PREFIX, webInfClasses.get(i).getPath());
+                        res = Resource.newResource(newPath);
+                        if (!res.exists())
+                        {
+                            res = null; 
+                            i++;
+                        }
+                    }
+                    return res;
+                }
+                // Return the real jar file for all accesses to
+                // /WEB-INF/lib/*.jar
+                else if (uri.startsWith(WEB_INF_LIB_PREFIX))
+                {
+                    String jarName = uri.replace(WEB_INF_LIB_PREFIX, "");
+                    if (jarName.startsWith("/") || jarName.startsWith("\\")) 
+                        jarName = jarName.substring(1);
+                    if (jarName.isEmpty()) return null;
+                    File jarFile = webInfJarMap.get(jarName);
+                    if (jarFile != null)
+                        return Resource.newResource(jarFile.getPath());
+
+                    return null;
+                }
+            }
+            catch (MalformedURLException e)
+            {
+                throw e;
+            }
+            catch (IOException e)
+            {
+                Log.ignore(e);
+            }
+        }
+        return resource;
+    }
+
+    @Override
+    public Set<String> getResourcePaths(String path)
+    {
+        // Try to get regular resource paths
+        Set<String> paths = super.getResourcePaths(path);
+
+        // If no paths are returned check for virtual paths /WEB-INF/classes and /WEB-INF/lib
+        if (paths.isEmpty() && path != null)
+        {
+            path = URIUtil.canonicalPath(path);
+            if (path.startsWith(WEB_INF_LIB_PREFIX))
+            {
+                paths = new TreeSet<String>();
+                for (String fileName : webInfJarMap.keySet())
+                {
+                    // Return all jar files from class path
+                    paths.add(WEB_INF_LIB_PREFIX + "/" + fileName);
+                }
+            }
+            else if (path.startsWith(WEB_INF_CLASSES_PREFIX))
+            {
+                int i=0;
+               
+                while (paths.isEmpty() && (i < webInfClasses.size()))
+                {
+                    String newPath = path.replace(WEB_INF_CLASSES_PREFIX, webInfClasses.get(i).getPath());
+                    paths = super.getResourcePaths(newPath);
+                    i++;
+                }
+            }
+        }
+        return paths;
     }
 }
