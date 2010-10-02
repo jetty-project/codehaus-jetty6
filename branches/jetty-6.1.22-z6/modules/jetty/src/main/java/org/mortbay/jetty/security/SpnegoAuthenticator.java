ebd279b485af0ce76e81e3b7bc554fbc32addb96
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2010-10-02 04:16:31 +0000

    pull over the Spengo authenticator and user realm from other branch

--- branches/jetty-6.1.22-z6/modules/jetty/src/main/java/org/mortbay/jetty/security/SpnegoAuthenticator.java
+++ branches/jetty-6.1.22-z6/modules/jetty/src/main/java/org/mortbay/jetty/security/SpnegoAuthenticator.java
@@ -1,4 +1,18 @@
 package org.mortbay.jetty.security;
+//========================================================================
+//Copyright 1998-2010 Mort Bay Consulting Pty. Ltd.
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+
 
 import java.io.IOException;
 import java.security.Principal;
@@ -8,55 +22,167 @@ import javax.servlet.http.HttpServletResponse;
 import org.mortbay.jetty.HttpHeaders;
 import org.mortbay.jetty.Request;
 import org.mortbay.jetty.Response;
+import org.mortbay.jetty.security.SpnegoUserRealm.SpnegoUser;
+import org.mortbay.log.Log;
+import org.mortbay.util.StringUtil;
+import org.mortbay.util.URIUtil;
 
+/**
+ * SPNEGO Authentication Authenticator
+ * 
+ */
 public class SpnegoAuthenticator implements Authenticator
 {
-
-    public Principal authenticate(UserRealm realm, String pathInContext, Request request, Response response) throws IOException
+    private String _errorPage;
+    private String _errorPath;
+    
+    /* ------------------------------------------------------------ */
+    public void setErrorPage(String path)
     {
-        System.out.println("Trying to authenticate");
-        
+        if (path==null || path.trim().length()==0)
+        {
+            _errorPath=null;
+            _errorPage=null;
+        }
+        else
+        {
+            if (!path.startsWith("/"))
+            {
+                Log.warn("error-page must start with /");
+                path="/"+path;
+            }
+            _errorPage=path;
+            _errorPath=path;
+
+            if (_errorPath!=null && _errorPath.indexOf('?')>0)
+                _errorPath=_errorPath.substring(0,_errorPath.indexOf('?'));
+        }
+    }    
+
+    /* ------------------------------------------------------------ */
+    public String getErrorPage()
+    {
+        return _errorPage;
+    }
+    
+
+    public boolean isErrorPage(String pathInContext)
+    {
+        return pathInContext!=null && (pathInContext.equals(_errorPath));
+    }
+    
+    public Principal authenticate(UserRealm realm, String pathInContext, Request request, Response response) throws IOException
+    {   
+    	/*
+    	 * if the request is for the error page, return nobody and it should present
+    	 */
+    	if ( isErrorPage(pathInContext) )
+    	{
+    		return SecurityHandler.__NOBODY;
+    	}
+    	
         Principal user = null;
         
         String header = request.getHeader(HttpHeaders.AUTHORIZATION);
         
+        /*
+         * if the header is null then we need to challenge...this is after the error page check
+         */
         if (header == null)
         {
             sendChallenge(realm,request,response);
-            return user;
+            return null;
         } 
         else if (header != null && header.startsWith(HttpHeaders.NEGOTIATE)) 
-        {
-            Principal principal = request.getUserPrincipal();
-            
+        {        	
+            /*
+             * we have gotten a negotiate header to try and authenticate
+             */
+        	
             String username = header.substring(10);
-            String password = null;
             
-            // doublecheck this password object 'credentials'
-            principal = realm.authenticate(username, password, request);
+            user = realm.authenticate(username, null, request);
             
-            if (principal != null)
+            if (user != null)
             {
+                Log.debug("SpengoAuthenticator: obtained principal: " + user.getName());
+
                 request.setAuthType(Constraint.__SPNEGO_AUTH);
-                request.setUserPrincipal(principal);
+                request.setUserPrincipal(user);
+                
+                response.addHeader(HttpHeaders.WWW_AUTHENTICATE, HttpHeaders.NEGOTIATE + " " + ((SpnegoUser)user).getToken());
+                
                 return user;
             }
             else
             {
-                return user;
-                //request.getMimeHeaders().removeHeader("authorization");
+            	/*
+            	 * no user was returned from the authentication which means something failed
+            	 * so process error logic
+            	 */
+                if(Log.isDebugEnabled())
+                {
+                    Log.debug("SpengoAuthenticator: authentication failed");
+                }
+                
+                if (_errorPage==null)
+                {
+                    if (response != null)
+                    {
+                        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                    }
+                }
+                else
+                {
+                    if (response != null)
+                    {
+                        response.setContentLength(0);
+                        
+                        response.sendRedirect(response.encodeRedirectURL
+                                          (URIUtil.addPaths(request.getContextPath(),
+                                                        _errorPage)));                  
+                    }
+                }
+                
+                return null;
             }
         }
+        /*
+         * the header was not null, but we didn't get a negotiate so process error logic
+         */
         else
         {
+            if(Log.isDebugEnabled())
+            {
+                Log.debug("SpengoAuthenticator: authentication failed, unknown header (browser is likely misconfigured for SPNEGO)");
+            }
+            
+            if (_errorPage==null)
+            {
+                if (response != null)
+                {
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                }
+            }
+            else
+            {
+                if (response != null)
+                {
+                    response.setContentLength(0);
+                    
+                    response.sendRedirect(response.encodeRedirectURL
+                                      (URIUtil.addPaths(request.getContextPath(),
+                                                    _errorPage)));
+                }
+            }
+                     
             return null;
-        }
-       
+        }        
     }
-
+    
     public void sendChallenge(UserRealm realm, Request request, Response response) throws IOException
     {
-        System.out.println("sending challenge");
+        Log.debug("SpengoAuthenticator: sending challenge");
         response.setHeader(HttpHeaders.WWW_AUTHENTICATE, HttpHeaders.NEGOTIATE);
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
--- branches/jetty-6.1.22-z6/modules/jetty/src/main/java/org/mortbay/jetty/security/SpnegoUserRealm.java
+++ branches/jetty-6.1.22-z6/modules/jetty/src/main/java/org/mortbay/jetty/security/SpnegoUserRealm.java
@@ -1,22 +1,26 @@
 package org.mortbay.jetty.security;
 
+//========================================================================
+//Copyright 1998-2010 Mort Bay Consulting Pty. Ltd.
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+
 import java.io.IOException;
-import java.math.BigInteger;
 import java.security.Principal;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-
-import sun.misc.BASE64Decoder;
-
-import javax.security.auth.callback.Callback;
-import javax.security.auth.callback.CallbackHandler;
-import javax.security.auth.callback.NameCallback;
-import javax.security.auth.callback.UnsupportedCallbackException;
-import javax.security.auth.login.FailedLoginException;
-import javax.security.auth.login.LoginException;
 
 import org.ietf.jgss.GSSContext;
 import org.ietf.jgss.GSSCredential;
@@ -26,96 +30,270 @@ import org.ietf.jgss.GSSName;
 import org.ietf.jgss.Oid;
 import org.mortbay.jetty.Request;
 import org.mortbay.log.Log;
-import org.mortbay.util.Loader;
+import org.mortbay.resource.Resource;
 
+/*
+ * Implementation of a jetty user realm for spnego support. 
+ *	
+ * See the README.spengo file in the etc directory for information on configuring spnego
+ *
+ * Based on some of the work for supporting spnego in Apache Geronimo by Ashish Jain
+ */
 @SuppressWarnings("restriction")
-public class SpnegoUserRealm extends HashUserRealm implements UserRealm
+public class SpnegoUserRealm implements UserRealm
 {
-    private GSSName srcName;
-    private CallbackHandler callbackHandler;
- 
-    private BASE64Decoder base64Decoder = new BASE64Decoder();
-    
+    private String _realmName;
+    private String _config;
+    private Resource _configResource;
+    private String _targetName;
+
+   // private Base64 base64 = new Base64();
+
+    // private BASE64Decoder base64Decoder = new BASE64Decoder();
+    // private BASE64Encoder base64Encoder = new BASE64Encoder();
+
     public SpnegoUserRealm()
     {
-        
+        super();
     }
-    
-    
-    
+
     public SpnegoUserRealm(String name, String config) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException
     {
-        super(name);
+        super();
+
+        setName(name);
         setConfig(config);
+
     }
-    
-    
+
+    public Resource getConfigResource()
+    {
+        return _configResource;
+    }
+
+    /* ------------------------------------------------------------ */
+    /**
+     * Load realm users from properties file. The property file maps usernames to password specs followed by an optional comma separated list of role names.
+     * 
+     * @param config
+     *            Filename or url of user properties file.
+     * @exception IOException
+     */
+    public void setConfig(String config) throws IOException
+    {
+        _config = config;
+        _configResource = Resource.newResource(_config);
+        loadConfig();
+
+    }
+
+    public void setName(String name)
+    {
+        _realmName = name;
+    }
+
     protected void loadConfig() throws IOException
     {
         Properties properties = new Properties();
 
         properties.load(getConfigResource().getInputStream());
-      
+
+        _targetName = properties.getProperty("targetName");
+
+        dumpConfig(); // display configuration if debug enabled
     }
 
     public Principal authenticate(String username, Object credentials, Request request)
     {
-               
-        try {
-            
-            Callback[] callbacks = new Callback[1];
-            callbacks[0] = new NameCallback("User name");
-            callbackHandler.handle(callbacks);
-            username = ((NameCallback) callbacks[0]).getName();
+        try
+        {
+            byte[] token = B64Code.decode(username);
 
-            if (username == null || username.equals("")) {
-                username = null;
-                return null;
-            }
             
-            byte[] token = base64Decoder.decodeBuffer(username);
-
             GSSManager manager = GSSManager.getInstance();
-           
-            System.out.println("Got GSSManager");
-            
+
             Oid krb5Oid = new Oid("1.3.6.1.5.5.2"); // http://java.sun.com/javase/6/docs/technotes/guides/security/jgss/jgss-features.html
-            GSSName gssName = manager.createName(username, GSSName.NT_USER_NAME);
-            GSSCredential serverCreds = manager.createCredential(gssName, GSSCredential.INDEFINITE_LIFETIME, krb5Oid, GSSCredential.ACCEPT_ONLY);
+            GSSName gssName = manager.createName(_targetName,null);// GSSName.NT_USER_NAME);
+            GSSCredential serverCreds = manager.createCredential(gssName,GSSCredential.INDEFINITE_LIFETIME,krb5Oid,GSSCredential.ACCEPT_ONLY);
             GSSContext gContext = manager.createContext(serverCreds);
-            if (gContext == null) {
-                Log.info("Failed to create a GSSContext");
-            } else {
-                while (!gContext.isEstablished()) {
-                    token = gContext.acceptSecContext(token, 0, token.length);
+
+            if (gContext == null)
+            {
+                Log.debug("SpnegoUserRealm: failed to establish GSSContext");
+            }
+            else
+            {
+                while (!gContext.isEstablished())
+                {
+                    token = gContext.acceptSecContext(token,0,token.length);
                 }
-                if (gContext.isEstablished()) {
-                    srcName = gContext.getSrcName();
-                    Log.info("A security context is successfully established " + gContext);
-                    
-                    // from the hash manager
-                    put(srcName, serverCreds);
-                    
-                    return null;
-                } else {
-                    Log.info("Failed to establish a security context");
-                    //throw new LoginException("Failed to establish a security context");
+                if (gContext.isEstablished())
+                {
+                    Log.debug("SpnegoUserRealm: established a security context");
+                    Log.debug("Client Principal is: " + gContext.getSrcName());
+                    Log.debug("Server Principal is: " + gContext.getTargName());
+
+                    GSSName srcName = gContext.getSrcName();
+                    String encodedToken = new String(B64Code.encode(token));
+
+                    SpnegoUser user = new SpnegoUser(srcName.toString(),encodedToken);
+
+                    // set the role to the logged in domain
+                    String clientPrincipal = gContext.getSrcName().toString();
+                    String role = clientPrincipal.substring(clientPrincipal.indexOf('@') + 1);
+                    user.addRole(role);
+                    Log.debug("Client Role: " + role);
+
+                    return user;
+                }
+                else
+                {
+                    Log.debug("SpnegoUserRealm: failed to establish a security context");
                 }
             }
-        } catch (GSSException e) {
-            e.printStackTrace();
-            //throw (LoginException) new LoginException().initCause(e);
         }
-        catch (IOException e)
+        catch (GSSException e)
         {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        	Log.debug("error processing token for spnego authentication", e.getMessage());
+        	
+        	if ( Log.isDebugEnabled() )
+        	{
+        		e.printStackTrace();
+        	}
         }
-        catch (UnsupportedCallbackException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        
         return null;
+    }
+
+    public class SpnegoUser implements Principal
+    {
+        String _name;
+        String _token;
+        List<String> _roles;
+
+        public SpnegoUser(String name, String token)
+        {
+            _name = name;
+            _token = token;
+        }
+
+        /* ------------------------------------------------------------ */
+        private UserRealm getUserRealm()
+        {
+            return SpnegoUserRealm.this;
+        }
+
+        public String getName()
+        {
+            return _name;
+        }
+
+        public boolean isAuthenticated()
+        {
+            return true;
+        }
+
+        public String toString()
+        {
+            return getName();
+        }
+
+        public String getToken()
+        {
+            return _token;
+        }
+
+        public void addRole(String role)
+        {
+            if (_roles == null)
+            {
+                _roles = new ArrayList(1);
+            }
+            _roles.add(role);
+        }
+
+        public List getRoles()
+        {
+            if (_roles != null)
+            {
+                return _roles;
+            }
+            else
+            {
+                return Collections.EMPTY_LIST;
+            }
+        }
+
+    }
+
+    @Override
+    public String getName()
+    {
+        return _realmName;
+    }
+
+    @Override
+    public Principal getPrincipal(String token)
+    {
+        return null;
+    }
+
+    @Override
+    public boolean reauthenticate(Principal user)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isUserInRole(Principal user, String role)
+    {
+        if (user instanceof SpnegoUser)
+        {
+            List roles = ((SpnegoUser)user).getRoles();
+
+            for (Iterator i = roles.iterator(); i.hasNext();)
+            {
+                if (role.equals((String)i.next()))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void disassociate(Principal user)
+    {
+
+    }
+
+    @Override
+    public Principal pushRole(Principal user, String role)
+    {
+        if (user instanceof SpnegoUser)
+        {
+            ((SpnegoUser)user).addRole(role);
+        }
+        return user;
+    }
+
+    @Override
+    public Principal popRole(Principal user)
+    {
+        return null;
+    }
+
+    @Override
+    public void logout(Principal user)
+    {
+
+    }
+
+    private void dumpConfig()
+    {
+        Log.debug("SpnegoUserRealm");
+        Log.debug(" - targetName = " + _targetName);
     }
 }
