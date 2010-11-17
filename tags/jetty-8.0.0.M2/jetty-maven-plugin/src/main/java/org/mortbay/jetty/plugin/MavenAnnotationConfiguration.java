8771b111f18ab2d7866c8b1a455e36ee051dc20b
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2010-11-17 19:52:22 +0000

    [maven-release-plugin]  copy for tag jetty-8.0.0.M2

---
+++ tags/jetty-8.0.0.M2/jetty-maven-plugin/src/main/java/org/mortbay/jetty/plugin/MavenAnnotationConfiguration.java
@@ -0,0 +1,67 @@
+package org.mortbay.jetty.plugin;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jetty.annotations.AnnotationParser;
+import org.eclipse.jetty.annotations.ClassNameResolver;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.DiscoveredAnnotation;
+import org.eclipse.jetty.webapp.MetaData;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class MavenAnnotationConfiguration extends AnnotationConfiguration
+{
+
+    /* ------------------------------------------------------------ */
+    @Override
+    public void parseWebInfClasses(final WebAppContext context, final AnnotationParser parser) throws Exception
+    {
+        JettyWebAppContext jwac = (JettyWebAppContext)context;
+        if (jwac.getClassPathFiles() == null)
+            super.parseWebInfClasses (context, parser);
+        else
+        {
+            Log.debug("Scanning classes ");
+            //Look for directories on the classpath and process each one of those
+            
+            MetaData metaData = context.getMetaData();
+            if (metaData == null)
+               throw new IllegalStateException ("No metadata");
+
+            clearAnnotationList(parser.getAnnotationHandlers());    
+
+            for (File f:jwac.getClassPathFiles())
+            {
+                if (f.isDirectory() && f.exists())
+                {
+                    parser.parse(Resource.newResource(f.toURL()), 
+                                new ClassNameResolver()
+                    {
+                        public boolean isExcluded (String name)
+                        {
+                            if (context.isSystemClass(name)) return true;
+                            if (context.isServerClass(name)) return false;
+                            return false;
+                        }
+
+                        public boolean shouldOverride (String name)
+                        {
+                            //looking at webapp classpath, found already-parsed class of same name - did it come from system or duplicate in webapp?
+                            if (context.isParentLoaderPriority())
+                                return false;
+                            return true;
+                        }
+                    });
+                }
+            }
+            //gather together all annotations discovered
+            List<DiscoveredAnnotation> annotations = new ArrayList<DiscoveredAnnotation>();
+            gatherAnnotations(annotations, parser.getAnnotationHandlers());
+            metaData.addDiscoveredAnnotations (annotations);
+        }
+    }
+}
