3acdffa76e352cccffd1ba0c3af886ed07a549bf
joakime <joakime@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2011-03-18 18:54:42 +0000

    Initial boilerplate for integration testing idea discussed with jesse

---
+++ jetty/branches/jetty-7/test-jetty-policy-integration/src/test/java/org/mortbay/jetty/tests/policy/integration/JettyProcess.java
@@ -0,0 +1,196 @@
+package org.mortbay.jetty.tests.policy.integration;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.URI;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.jetty.toolchain.test.FS;
+import org.eclipse.jetty.toolchain.test.IO;
+import org.eclipse.jetty.toolchain.test.MavenTestingUtils;
+import org.eclipse.jetty.toolchain.test.OS;
+import org.eclipse.jetty.toolchain.test.PathAssert;
+import org.eclipse.jetty.toolchain.test.TestingDir;
+
+/**
+ * Basic executor for the testable Jetty Distribution.
+ * <p>
+ * Allows for a test specific directory, that is a copied jetty-distribution, and then modified for the test specific
+ * testing required.
+ */
+public class JettyProcess
+{
+    private File jettyHomeDir;
+
+    /**
+     * Setup the JettyHome as belonging in a testing directory associated with a testing clazz.
+     * 
+     * @param clazz
+     *            the testing class using this JettyProcess
+     * @throws IOException
+     *             if unable to copy unpacked distribution into place for the provided testing directory
+     */
+    public JettyProcess(Class<?> clazz) throws IOException
+    {
+        this.jettyHomeDir = MavenTestingUtils.getTargetTestingDir(clazz,"jettyHome");
+        copyBaseDistro();
+    }
+
+    /**
+     * Setup the JettyHome as belonging to a specific testing method directory
+     * 
+     * @param testdir
+     *            the testing directory to use as the JettyHome for this JettyProcess
+     * @throws IOException
+     *             if unable to copy unpacked distribution into place for the provided testing directory
+     */
+    public JettyProcess(TestingDir testdir) throws IOException
+    {
+        this.jettyHomeDir = testdir.getDir();
+        copyBaseDistro();
+    }
+
+    /**
+     * 
+     * @throws IOException
+     *             if unable to copy unpacked distribution into place for the provided testing directory
+     */
+    private void copyBaseDistro() throws IOException
+    {
+        // The outputDirectory for the maven side dependency:unpack goal.
+        File distroUnpackDir = MavenTestingUtils.getTargetFile("jetty-distro");
+        PathAssert.assertDirExists("jetty-distribution dependency:unpack",distroUnpackDir);
+
+        // The actual jetty-distribution-${version} directory is under this directory.
+        // Lets find it.
+        File subdirs[] = distroUnpackDir.listFiles(new FileFilter()
+        {
+            Pattern pat = Pattern.compile("jetty-distribution-[0-9]+\\.[0-9A-Z.-]*");
+
+            public boolean accept(File path)
+            {
+                if (!path.isDirectory())
+                {
+                    return false;
+                }
+
+                Matcher mat = pat.matcher(path.getName());
+                return mat.matches();
+            }
+        });
+
+        if (subdirs.length == 0)
+        {
+            // No jetty-distribution found.
+            StringBuilder err = new StringBuilder();
+            err.append("No target/jetty-distro/jetty-distribution-${version} directory found.");
+            err.append("\n  To fix this, run 'mvn process-test-resources' to create the directory.");
+            throw new IOException(err.toString());
+        }
+
+        if (subdirs.length != 1)
+        {
+            // Too many jetty-distributions found.
+            StringBuilder err = new StringBuilder();
+            err.append("Too many target/jetty-distro/jetty-distribution-${version} directories found.");
+            for (File dir : subdirs)
+            {
+                err.append("\n  ").append(dir.getAbsolutePath());
+            }
+            err.append("\n  To fix this, run 'mvn clean process-test-resources' to recreate the target/jetty-distro directory.");
+            throw new IOException(err.toString());
+        }
+
+        File distroSrcDir = subdirs[0];
+        FS.ensureEmpty(jettyHomeDir);
+        System.out.printf("Copying Jetty Distribution: %s%n",distroSrcDir.getAbsolutePath());
+        System.out.printf("            To Testing Dir: %s%n",jettyHomeDir.getAbsolutePath());
+        IO.copyDir(distroSrcDir,jettyHomeDir);
+    }
+
+    /**
+     * Return the $(jetty.home) directory being used for this JettyProcess
+     * 
+     * @return the jetty.home directory being used
+     */
+    public File getJettyHomeDir()
+    {
+        return this.jettyHomeDir;
+    }
+
+    /**
+     * Copy a war file from ${project.basedir}/target/test-wars/${testWarFilename} into the ${jetty.home}/webapps/
+     * directory
+     * 
+     * @param testWarFilename
+     *            the war file to copy (must exist)
+     * @throws IOException
+     *             if unable to copy the war file.
+     */
+    public void copyTestWar(String testWarFilename) throws IOException
+    {
+        File srcWar = MavenTestingUtils.getTargetFile("test-wars/" + testWarFilename);
+        File destWar = new File(jettyHomeDir,OS.separators("webapps/" + testWarFilename));
+        IO.copyFile(srcWar,destWar);
+    }
+
+    /**
+     * Delete a File or Directory found in the ${jetty.home} directory.
+     * 
+     * @param path
+     *            the path to delete. (can be a file or directory)
+     */
+    public void delete(String path)
+    {
+        File jettyPath = new File(jettyHomeDir,OS.separators(path));
+        FS.delete(jettyPath);
+    }
+
+    /**
+     * Return the baseUri being used for this Jetty Process Instance.
+     * 
+     * @return the base URI for this Jetty Process Instance.
+     */
+    public URI getBaseUri()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Take the directory contents from ${project.basedir}/src/test/resources/${testConfigName}/ and copy it over
+     * whatever happens to be at ${jetty.home}
+     * 
+     * @param testConfigName
+     *            the src/test/resources/ directory name to use as the source diretory for the configuration we are
+     *            interested in.
+     * @throws IOException
+     *             if unable to copy directory.
+     */
+    public void overlayConfig(String testConfigName) throws IOException
+    {
+        File srcDir = MavenTestingUtils.getTestResourceDir(testConfigName);
+        IO.copyDir(srcDir,jettyHomeDir);
+    }
+
+    /**
+     * Start the jetty server
+     */
+    public void start()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * Stop the jetty server
+     */
+    public void stop()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
