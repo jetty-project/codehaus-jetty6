28a27fc7f4b8482c5552bd0e8be4c14c3b82b1b4
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2011-03-22 15:18:21 +0000

    supporting bits for jetty secure testing

---
+++ branches/jetty-7/test-jetty-policy-integration/src/test/java/org/mortbay/jetty/tests/policy/integration/SecuredTest.java
@@ -0,0 +1,99 @@
+package org.mortbay.jetty.tests.policy.integration;
+
+import static org.hamcrest.Matchers.*;
+
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.eclipse.jetty.toolchain.test.SimpleRequest;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Test Jetty with 2 webapps, with jetty-policy or java security in place.
+ */
+public class SecuredTest
+{
+    private static JettyProcess jetty;
+
+    @BeforeClass
+    public static void initJetty() throws Exception
+    {
+        jetty = new JettyProcess(SecuredTest.class);
+
+        jetty.copyTestWar("test-war-java_util_logging.war");
+        jetty.copyTestWar("test-war-policy.war");
+
+        jetty.delete("webapps/test.war");
+        jetty.delete("contexts/test.d");
+        jetty.delete("contexts/javadoc.xml");
+        jetty.delete("contexts/test.xml");
+
+        jetty.overlayConfig("secured");
+
+        jetty.start();
+    }
+
+    @AfterClass
+    public static void shutdownJetty() throws Exception
+    {
+        if (jetty != null)
+        {
+            jetty.stop();
+        }
+    }
+
+    @Test
+    public void testFilesystem() throws Exception
+    {
+        assertCheckerSuccess("processFilesystemChecks");
+    }
+
+    @Test
+    public void testJettyLog() throws Exception
+    {
+        assertCheckerSuccess("processJettyLogChecks");
+    }
+
+    @Test
+    public void testServletContext() throws Exception
+    {
+        assertCheckerSuccess("processFooWebappContextChecks");
+    }
+
+    @Test
+    public void testRequestDispatcher() throws Exception
+    {
+        assertCheckerSuccess("processFooWebappRequestDispatcherChecks");
+    }
+
+    @Test
+    @Ignore("need to fix loadLibrary to actually load a real library")
+    public void testLib() throws Exception
+    {
+        assertCheckerSuccess("processLibChecks");
+    }
+
+    @Test
+    public void testSystemProperty() throws Exception
+    {
+        assertCheckerSuccess("processSystemPropertyChecks");
+    }
+
+    private void assertCheckerSuccess(String testname) throws Exception
+    {
+        SimpleRequest request = new SimpleRequest(jetty.getBaseUri());
+        Properties props = request.getProperties("/policytests/checker/" + testname);
+        @SuppressWarnings("unchecked")
+        Enumeration<String> names = (Enumeration<String>)props.propertyNames();
+        while (names.hasMoreElements())
+        {
+            String name = names.nextElement();
+            String value = props.getProperty(name);
+            Assert.assertThat("[" + testname + "] " + name,value,startsWith("Success"));
+        }
+    }
+}
