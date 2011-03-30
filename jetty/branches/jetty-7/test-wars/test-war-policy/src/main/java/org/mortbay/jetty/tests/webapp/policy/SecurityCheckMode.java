ef4736fbda8b7da11337aa2ac50989d6f3183d81
joakime <joakime@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2011-03-30 20:20:34 +0000

    Adding some javadoc to help figure out what is going on

--- jetty/branches/jetty-7/test-wars/test-war-policy/src/main/java/org/mortbay/jetty/tests/webapp/policy/SecurityCheckMode.java
+++ jetty/branches/jetty-7/test-wars/test-war-policy/src/main/java/org/mortbay/jetty/tests/webapp/policy/SecurityCheckMode.java
@@ -2,7 +2,22 @@ package org.mortbay.jetty.tests.webapp.policy;
 
 public enum SecurityCheckMode
 {
-    NO_SECURITY, PRACTICAL, PARANOID;
+    /** 
+     * Expectation that there no java security or <code>jetty-policy</code> present in jetty 
+     */
+    NO_SECURITY,
+
+    /**
+     * Expectation that java security and <code>jetty-policy</code> is enabled, but specific useful grants have been
+     * given, simulating a practical configuration of <code>jetty-policy</code> and the various java security
+     * <code>*.policy</code> files
+     */
+    PRACTICAL,
+
+    /** 
+     * Expectation that java security and <code>jetty-policy</code> is enabled, but no grants have been given 
+     */
+    PARANOID;
 
     public static SecurityCheckMode getMode(String securityMode)
     {
