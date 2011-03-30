69e3d2b8a15513762a3a44e2674398ab7cc1fe47
joakime <joakime@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2011-03-30 23:34:28 +0000

    Updating test-jetty-policy-integration testing to use new SecurityTestServlet structure.

--- branches/jetty-7/test-jetty-policy-integration/src/test/java/org/mortbay/jetty/tests/policy/integration/security/PracticalSecurityTest.java
+++ branches/jetty-7/test-jetty-policy-integration/src/test/java/org/mortbay/jetty/tests/policy/integration/security/PracticalSecurityTest.java
@@ -1,4 +1,4 @@
-package org.mortbay.jetty.tests.policy.integration;
+package org.mortbay.jetty.tests.policy.integration.security;
 
 import static org.hamcrest.Matchers.*;
 
@@ -9,19 +9,21 @@ import org.eclipse.jetty.toolchain.test.SimpleRequest;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mortbay.jetty.tests.policy.integration.JettyProcess;
 
 /**
- * Test Jetty with 2 webapps, with jetty-policy or java security in place.
+ * Test Jetty with 2 webapps, and no jetty-policy or java security in place.
  */
-public class PolicyTest
+public class PracticalSecurityTest
 {
     private static JettyProcess jetty;
 
     @BeforeClass
     public static void initJetty() throws Exception
     {
-        jetty = new JettyProcess(PolicyTest.class);
+        jetty = new JettyProcess(PracticalSecurityTest.class);
 
         jetty.copyTestWar("test-war-java_util_logging.war");
         jetty.copyTestWar("test-war-policy.war");
@@ -30,16 +32,14 @@ public class PolicyTest
         jetty.delete("contexts/test.d");
         jetty.delete("contexts/javadoc.xml");
         jetty.delete("contexts/test.xml");
-        
-        // Remove standard servlet-api jar
+
         jetty.delete("lib/servlet-api-2.5.jar");
         // Use AspectJ + Security enhanced servlet-api jar
         jetty.copyLib("jetty-aspect-servlet-api-2.5.jar","lib-secure/servlet-api-2.5.jar");
         jetty.copyLib("aspectjrt.jar","lib-secure/aspectjrt.jar");
 
-        jetty.overlayConfig("policy");
 
-        jetty.setDebug(true);
+        jetty.overlayConfig("practical_security");
         
         jetty.start();
     }
@@ -56,40 +56,41 @@ public class PolicyTest
     @Test
     public void testFilesystem() throws Exception
     {
-        assertCheckerFailure("processFilesystemChecks");
+        assertCheckerSuccess("processFilesystemChecks");
     }
 
     @Test
     public void testJettyLog() throws Exception
     {
-        assertCheckerFailure("processJettyLogChecks");
+        assertCheckerSuccess("processJettyLogChecks");
     }
 
     @Test
     public void testServletContext() throws Exception
     {
-        assertCheckerFailure("processFooWebappContextChecks");
+        assertCheckerSuccess("processFooWebappContextChecks");
     }
 
     @Test
     public void testRequestDispatcher() throws Exception
     {
-        assertCheckerFailure("processFooWebappRequestDispatcherChecks");
+        assertCheckerSuccess("processFooWebappRequestDispatcherChecks");
     }
 
     @Test
+    @Ignore("need to fix loadLibrary to actually load a real library")
     public void testLib() throws Exception
     {
-        assertCheckerFailure("processLibChecks");
+        assertCheckerSuccess("processLibChecks");
     }
 
     @Test
     public void testSystemProperty() throws Exception
     {
-        assertCheckerFailure("processSystemPropertyChecks");
+        assertCheckerSuccess("processSystemPropertyChecks");
     }
 
-    private void assertCheckerFailure(String testname) throws Exception
+    private void assertCheckerSuccess(String testname) throws Exception
     {
         SimpleRequest request = new SimpleRequest(jetty.getBaseUri());
         Properties props = request.getProperties("/policytests/checker/" + testname);
@@ -99,7 +100,7 @@ public class PolicyTest
         {
             String name = names.nextElement();
             String value = props.getProperty(name);
-            Assert.assertThat("[" + testname + "] " + name,value,not(startsWith("Success")));
+            Assert.assertThat("[" + testname + "] " + name,value,startsWith("Success"));
         }
     }
 }
