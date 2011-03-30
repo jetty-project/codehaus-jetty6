397f64a1da2984c7a452dfc4b5da36a5b2b17bad
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2011-03-30 03:43:24 +0000

    JETTY-1354 Added jetty-nested

---
+++ jetty/branches/jetty-7/jetty-nested/src/main/java/org/eclipse/jetty/nested/NestedEndPoint.java
@@ -0,0 +1,20 @@
+package org.eclipse.jetty.nested;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.io.bio.StreamEndPoint;
+
+public class NestedEndPoint extends StreamEndPoint
+{
+    public NestedEndPoint(HttpServletRequest request, HttpServletResponse response)
+        throws IOException
+    {
+        super(request.getInputStream(),response.getOutputStream());
+    }
+
+}
