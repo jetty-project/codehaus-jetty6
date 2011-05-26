4a822b793d708758ae8755f0b1e62645a6225bfb
janb <janb@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2011-05-26 00:15:26 +0000

    JETTY-1372

--- jetty/branches/jetty-7/jetty-jspc-maven-plugin/src/main/java/org/mortbay/jetty/jspc/plugin/JspcMojo.java
+++ jetty/branches/jetty-7/jetty-jspc-maven-plugin/src/main/java/org/mortbay/jetty/jspc/plugin/JspcMojo.java
@@ -214,6 +214,14 @@ public class JspcMojo extends AbstractMojo
      * @parameter
      */
     private String schemaResourcePrefix;
+
+
+
+    /**
+     * Should white spaces in template text between actions or directives be trimmed? Defaults to false.
+     * @parameter
+     */
+    private boolean trimSpaces = false;
     
 
     public void execute() throws MojoExecutionException, MojoFailureException
@@ -234,6 +242,7 @@ public class JspcMojo extends AbstractMojo
             getLog().info("suppressSmap=" + suppressSmap);
             getLog().info("ignoreJspFragmentErrors=" + ignoreJspFragmentErrors);
             getLog().info("schemaResourcePrefix=" + schemaResourcePrefix);
+            getLog().info("trimSpaces=" + trimSpaces);
         }
         try
         {
@@ -282,6 +291,7 @@ public class JspcMojo extends AbstractMojo
         jspc.setSmapSuppressed(suppressSmap);
         jspc.setSmapDumped(!suppressSmap);
         jspc.setJavaEncoding(javaEncoding);
+        jspc.setTrimSpaces(trimSpaces);
 
         // JspC#setExtensions() does not exist, so 
         // always set concrete list of files that will be processed.
