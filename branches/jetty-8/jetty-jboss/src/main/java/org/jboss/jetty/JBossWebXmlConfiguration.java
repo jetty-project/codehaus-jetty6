d542b446a59413af3e929035f56edc78216ce9b2
gregw <gregw@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2011-05-30 05:09:40 +0000

    ready for release

--- branches/jetty-8/jetty-jboss/src/main/java/org/jboss/jetty/JBossWebXmlConfiguration.java
+++ branches/jetty-8/jetty-jboss/src/main/java/org/jboss/jetty/JBossWebXmlConfiguration.java
@@ -16,12 +16,11 @@
 package org.jboss.jetty;
 
 import org.eclipse.jetty.security.SecurityHandler;
+import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.Descriptor;
 import org.eclipse.jetty.webapp.IterativeDescriptorProcessor;
 import org.eclipse.jetty.webapp.MetaData;
-import org.eclipse.jetty.webapp.StandardDescriptorProcessor;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.eclipse.jetty.webapp.WebXmlConfiguration;
 import org.eclipse.jetty.xml.XmlParser;
 import org.jboss.logging.Logger;
 import org.jboss.metadata.WebMetaData;
@@ -32,44 +31,18 @@ import org.jboss.metadata.WebMetaData;
  * Extends the jetty WebXmlConfiguration to provide jboss
  * handling of various elements in the web.xml
  */
-public class JBossWebXmlConfiguration extends WebXmlConfiguration
+public class JBossWebXmlConfiguration implements Configuration
 {
     protected static Logger __log=Logger.getLogger(JBossWebAppContext.class); 
-
+    protected WebAppContext _context;
+    protected MetaData _metaData;
+    protected SecurityHandler _securityHandler;
   
-<<<<<<< .working
-<<<<<<< .working
-    public static class JBossWebXmlProcessor extends IterativeDescriptorProcessor
-=======
     
-=======
-    
->>>>>>> .merge-right.r6613
     public class JBossWebXmlProcessor extends IterativeDescriptorProcessor
->>>>>>> .merge-right.r6623
     {
-<<<<<<< .working
-<<<<<<< .working
-    	protected MetaData _metaData;
-    	protected WebAppContext _context;
-    	protected SecurityHandler _securityHandler;
-    	
-=======
->>>>>>> .merge-right.r6623
-=======
->>>>>>> .merge-right.r6613
         public JBossWebXmlProcessor() throws ClassNotFoundException
         {
-<<<<<<< .working
-        	   try
-               {
-                   registerVisitor("login-config", this.getClass().getDeclaredMethod("visitLoginConfig", __signature));
-               }
-        	   catch (Exception e)
-        	   {
-        		   throw new IllegalStateException(e);
-        	   }
-=======
             try
             {
                 registerVisitor("login-config",getClass().getDeclaredMethod("visitLoginConfig", __signature));
@@ -78,55 +51,18 @@ public class JBossWebXmlConfiguration extends WebXmlConfiguration
             {
                 throw new IllegalStateException(e);
             }
-<<<<<<< .working
->>>>>>> .merge-right.r6623
-=======
->>>>>>> .merge-right.r6613
         }
         
-<<<<<<< .working
-<<<<<<< .working
-        @Override
-		public void start(Descriptor descriptor) 
-		{
-=======
         @Override
         public void start(WebAppContext context, Descriptor descriptor)
-=======
-        @Override
-        public void start(WebAppContext context, Descriptor descriptor)
->>>>>>> .merge-right.r6613
         {
-<<<<<<< .working
->>>>>>> .merge-right.r6623
             _metaData=context.getMetaData();
             _context=context;
-=======
-            _metaData=context.getMetaData();
-            _context=context;
->>>>>>> .merge-right.r6613
             _securityHandler = (SecurityHandler)_context.getSecurityHandler();
-		}
-		
-		@Override
-		public void end(Descriptor descriptor) 
-		{
-	        _metaData = null;
-	        _context = null;
-		}
+        }
 
-<<<<<<< .working
-<<<<<<< .working
-		
-        public void visitLoginConfig(Descriptor descriptor, XmlParser.Node node) throws Exception
-        {    
-=======
         @Override
         public void end(WebAppContext context, Descriptor descriptor)
-=======
-        @Override
-        public void end(WebAppContext context, Descriptor descriptor)
->>>>>>> .merge-right.r6613
         {
             _metaData = null;
             _context = null;
@@ -134,7 +70,6 @@ public class JBossWebXmlConfiguration extends WebXmlConfiguration
 
         public void visitLoginConfig(WebAppContext context, Descriptor descriptor, XmlParser.Node node) throws Exception
         {
->>>>>>> .merge-right.r6623
             //use a security domain name from jboss-web.xml
             if (null==_securityHandler.getRealmName())
             {
@@ -155,23 +90,14 @@ public class JBossWebXmlConfiguration extends WebXmlConfiguration
                 __log.debug("Realm name is : "+_securityHandler.getRealmName());
         }
     }
-    
 
     public void configure(WebAppContext context) throws Exception
-    {
-        super.configure(context);
-        MetaData metaData = (MetaData)context.getAttribute(MetaData.METADATA); 
-        if (metaData == null)
-           throw new IllegalStateException ("No metadata");
-        
-        metaData.addDescriptorProcessor(new JBossWebXmlProcessor());
-        context.getMetaData().addDescriptorProcessor(new JBossWebXmlProcessor());
+    {     
         context.getMetaData().addDescriptorProcessor(new JBossWebXmlProcessor());
     }
 
     public void deconfigure(WebAppContext context) throws Exception
     {
-        super.deconfigure(context);
     }
 
     public void destroy(WebAppContext context) throws Exception
@@ -180,12 +106,10 @@ public class JBossWebXmlConfiguration extends WebXmlConfiguration
 
     public void postConfigure(WebAppContext context) throws Exception
     {
-        super.postConfigure(context);
     }
 
     public void preConfigure(WebAppContext context) throws Exception
     {
-        super.preConfigure(context);
     }
 
     public void cloneConfigure(WebAppContext template, WebAppContext context) throws Exception
