dfb79c9ba8c454cd2be68af25c33722751411936
joakime <joakime@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2011-05-31 23:10:14 +0000

    Finishing up first pass of RAClient

--- branches/jetty-7/test-wars/jetty-remote-assert/src/test/java/org/mortbay/jetty/test/validation/FrameworkTest.java
+++ branches/jetty-7/test-wars/jetty-remote-assert/src/test/java/org/mortbay/jetty/test/validation/FrameworkTest.java
@@ -15,6 +15,8 @@ import org.junit.Test;
 import org.mortbay.jetty.test.validation.client.RemoteAssertClient;
 import org.mortbay.jetty.test.validation.client.RemoteAssertResults;
 import org.mortbay.jetty.test.validation.client.RemoteAssertResults.TestClass;
+import org.mortbay.jetty.test.validation.client.RemoteAssertResults.TestResult;
+import org.mortbay.jetty.test.validation.fwk.ContextTest;
 import org.mortbay.jetty.test.validation.fwk.SimpleTest;
 
 public class FrameworkTest
@@ -72,8 +74,26 @@ public class FrameworkTest
 
         Assert.assertThat("Class Count",results.getTestClassCount(),is(2));
 
-        TestClass tc = results.getTestClass(SimpleTest.class.getName());
-        Assert.assertNotNull("Should have found TestClass[" + SimpleTest.class.getName() + "]",tc);
+        TestClass tc;
+        TestResult tr;
+
+        tc = results.getTestClass(SimpleTest.class.getName());
+        Assert.assertNotNull("Should have found TestClass[SimpleTest]",tc);
         Assert.assertEquals("SimpleTest.testCount",5,tc.getTestCount());
+
+        tr = tc.getTestResult("testNumberIgnored");
+        Assert.assertTrue("test should be ignore",tr.isIgnored());
+
+        tr = tc.getTestResult("testQuoteEqualsFailure");
+        Assert.assertFalse("test should not be ignore",tr.isIgnored());
+        Assert.assertFalse("test should not be success",tr.isSuccess());
+        Assert.assertFalse("test should not have assumption failure",tr.isAssumptionFailure());
+        Assert.assertEquals("test.failure.header","testQuoteEqualsFailure(org.mortbay.jetty.test.validation.fwk.SimpleTest)",tr.getFailureHeader());
+        Assert.assertEquals("test.failure.message","Quote expected:<[Sweet]> but was:<[Dude]>",tr.getFailureMessage());
+        Assert.assertNotNull("test.failure.trace should not be null",tr.getFailureTrace());
+
+        tc = results.getTestClass(ContextTest.class.getName());
+        Assert.assertNotNull("Should have found TestClass[ContextTest]",tc);
+        Assert.assertEquals("ContextTest.testCount",3,tc.getTestCount());
     }
 }
