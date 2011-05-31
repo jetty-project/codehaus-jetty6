11b4ce05698c812c4bf894d9bf0ec1783c9e02b4
joakime <joakime@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2011-05-31 20:28:18 +0000

    Trying to bring back the unit testing of jetty-remote-assert into a single module, so I can get rid of jetty-remote-assert-tests and test-war-remote-asserts

---
+++ branches/jetty-7/test-wars/jetty-remote-assert/src/test/java/org/mortbay/jetty/test/validation/FrameworkTest.java
@@ -0,0 +1,63 @@
+package org.mortbay.jetty.test.validation;
+
+import java.net.URI;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.bio.SocketConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.toolchain.test.SimpleRequest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FrameworkTest
+{
+    private static Server jetty;
+    private static URI baseUri;
+
+    @BeforeClass
+    public static void initEmbeddedJetty() throws Exception
+    {
+        jetty = new Server();
+        Connector socketConnector = new SocketConnector();
+        socketConnector.setPort(0);
+        Connector connectors[] = new Connector[1];
+        connectors[0] = socketConnector;
+        jetty.setConnectors(connectors);
+
+        ServletHandler handler = new ServletHandler();
+        jetty.setHandler(handler);
+
+        ServletHolder servletHolder = new ServletHolder(BasicTestSuiteServlet.class);
+        handler.addServletWithMapping(servletHolder,"/");
+
+        jetty.start();
+
+        String host = socketConnector.getHost();
+        if (host == null)
+        {
+            host = "localhost";
+        }
+        baseUri = URI.create("http://" + host + ":" + socketConnector.getLocalPort());
+    }
+
+    @AfterClass
+    public static void stopEmbeddedJetty() throws Exception
+    {
+        if (jetty != null)
+        {
+            jetty.stop();
+        }
+    }
+
+    @Test
+    public void testSimpleTest() throws Exception
+    {
+        SimpleRequest request = new SimpleRequest(baseUri);
+
+        String result = request.getString("/tests/");
+        System.out.println("Result:\n" + result);
+    }
+}
