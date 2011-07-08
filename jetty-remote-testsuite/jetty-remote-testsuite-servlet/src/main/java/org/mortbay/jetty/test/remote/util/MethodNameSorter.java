9a3c447e9441f3e54fba688bf33f66708c8b34c2
jesse <jesse@c7fae8f8-2506-0410-b6b2-ad6cf81c2737>
2011-07-08 00:32:21 +0000

    [maven-release-plugin]  copy for tag jetty-7.4.4.v20110707

---
+++ jetty-remote-testsuite/jetty-remote-testsuite-servlet/src/main/java/org/mortbay/jetty/test/remote/util/MethodNameSorter.java
@@ -0,0 +1,12 @@
+package org.mortbay.jetty.test.remote.util;
+
+import java.lang.reflect.Method;
+import java.util.Comparator;
+
+public class MethodNameSorter implements Comparator<Method>
+{
+    public int compare(Method o1, Method o2)
+    {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
